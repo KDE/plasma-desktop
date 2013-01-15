4d67c386567ef10b9b2ece34d6e2bee677866919
Weng Xuetian <wengxt@gmail.com>
2013-01-15 17:05:45 -0500

    blacklist ibus older than 1.4.0

--- applets/kimpanel/cmake/FindIBus.cmake
+++ applets/kimpanel/cmake/FindIBus.cmake
@@ -24,23 +24,26 @@ else (IBUS_LIBRARIES AND IBUS_INCLUDE_DIR)
       # use pkg-config to get the directories and then use these values
       # in the FIND_PATH() and FIND_LIBRARY() calls
       find_package(PkgConfig)
-      pkg_check_modules(PC_IBUS ibus-1.0)
+      pkg_check_modules(PC_IBUS ibus-1.0>=1.4.0)
 
       set(IBUS_DEFINITIONS ${PC_IBUS_CFLAGS_OTHER})
    endif (NOT WIN32)
 
-   find_path(IBUS_INCLUDE_DIR ibus.h
-      PREFIX ibus-1.0
-      PATHS
-      ${PC_IBUS_INCLUDEDIR}
-      ${PC_IBUS_INCLUDE_DIRS}
-      )
+   if (PC_IBUS_FOUND)
 
-   find_library(IBUS_LIBRARIES NAMES ibus-1.0 ibus
-      PATHS
-      ${PC_IBUS_LIBDIR}
-      ${PC_IBUS_LIBRARY_DIRS}
-      )
+       find_path(IBUS_INCLUDE_DIR ibus.h
+          PREFIX ibus-1.0
+          PATHS
+          ${PC_IBUS_INCLUDEDIR}
+          ${PC_IBUS_INCLUDE_DIRS}
+          )
+
+       find_library(IBUS_LIBRARIES NAMES ibus-1.0 ibus
+          PATHS
+          ${PC_IBUS_LIBDIR}
+          ${PC_IBUS_LIBRARY_DIRS}
+          )
+   endif (PC_IBUS_FOUND)
 
    include(FindPackageHandleStandardArgs)
    FIND_PACKAGE_HANDLE_STANDARD_ARGS(IBUS DEFAULT_MSG IBUS_LIBRARIES IBUS_INCLUDE_DIR)
