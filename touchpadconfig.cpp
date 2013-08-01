1882dc5f9b2406741a851903ef6021b4c6e9f0ba
Alexander Mezin <mezin.alexander@gmail.com>
2013-08-02 06:55:48 +0700

    Added about data

--- touchpadconfig.cpp
+++ touchpadconfig.cpp
@@ -22,6 +22,7 @@
 
 #include <QScrollArea>
 
+#include <KAboutData>
 #include <KPluginFactory>
 #include <KLocalizedString>
 #include <KMessageWidget>
@@ -31,9 +32,35 @@
 #include "customslider.h"
 #include "sliderpair.h"
 #include "touchpadbackend.h"
+#include "version.h"
 
 K_PLUGIN_FACTORY(TouchpadConfigFactory, registerPlugin<TouchpadConfig>();)
-K_EXPORT_PLUGIN(TouchpadConfigFactory("kcm_touchpad"))
+
+static KAboutData buildAboutData()
+{
+    KAboutData data("kcm_touchpad",
+                    QByteArray(),
+                    ki18n("Touchpad Settings"),
+                    TOUCHPAD_KCM_VERSION,
+                    ki18n("Touchpad configuration module for KDE"),
+                    KAboutData::License_GPL_V2,
+                    ki18n("Copyright © 2013 Alexander Mezin"),
+                    ki18n("This program incorporates work covered by this "
+                          "copyright notice:\n"
+                          "Copyright © 2002-2005,2007 Peter Osterlund"),
+                    QByteArray(),
+                    "mezin.alexander@gmail.com");
+
+    data.addAuthor(ki18n("Alexander Mezin"),
+                   ki18n("Developer"),
+                   "mezin.alexander@gmail.com");
+    data.addCredit(ki18n("Alex Fiestas"), ki18n("Helped a bit"));
+    data.addCredit(ki18n("Peter Osterlund"), ki18n("Developer of synclient"));
+
+    return data;
+}
+
+K_EXPORT_PLUGIN(TouchpadConfigFactory(buildAboutData()))
 
 extern "C"
 {
@@ -265,6 +292,9 @@ TouchpadConfig::TouchpadConfig(QWidget *parent, const QVariantList &args)
     : KCModule(TouchpadConfigFactory::componentData(), parent, args),
       m_tabOrderSet(false), m_configOutOfSync(false)
 {
+    setAboutData(new KAboutData(
+                     *TouchpadConfigFactory::componentData().aboutData()));
+
     QVBoxLayout *layout = new QVBoxLayout(this);
 
     m_errorMessage = new KMessageWidget(this);
