c65f39b2c4bcd146a2de73f8e61f5ceb41929f2b
Alexander Mezin <mezin.alexander@gmail.com>
2013-09-09 20:40:27 +0700

    Move plugin factory to separate file

---
+++ plugins.cpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2013 Alexander Mezin <mezin.alexander@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "plugins.h"
+
+#include <KAboutData>
+#include <KPluginFactory>
+
+#include "touchpadconfig.h"
+#include "version.h"
+
+static KAboutData buildAboutData()
+{
+    KAboutData data("kcm_touchpad",
+                    QByteArray(),
+                    ki18n("Touchpad Settings"),
+                    TOUCHPAD_KCM_VERSION,
+                    ki18n("Touchpad configuration module for KDE"),
+                    KAboutData::License_GPL_V2,
+                    ki18n("Copyright © 2013 Alexander Mezin"),
+                    ki18n("This program incorporates work covered by this "
+                          "copyright notice:\n"
+                          "Copyright © 2002-2005,2007 Peter Osterlund"),
+                    QByteArray(),
+                    "mezin.alexander@gmail.com");
+
+    data.addAuthor(ki18n("Alexander Mezin"),
+                   ki18n("Developer"),
+                   "mezin.alexander@gmail.com");
+    data.addCredit(ki18n("Alex Fiestas"), ki18n("Helped a bit"));
+    data.addCredit(ki18n("Peter Osterlund"), ki18n("Developer of synclient"));
+
+    return data;
+}
+
+K_PLUGIN_FACTORY_DEFINITION(TouchpadPluginFactory,
+                            registerPlugin<TouchpadConfig>();)
+K_EXPORT_PLUGIN(TouchpadPluginFactory(buildAboutData()))
--- touchpadconfig.cpp
+++ touchpadconfig.cpp
@@ -23,7 +23,6 @@
 #include <QScrollArea>
 
 #include <KAboutData>
-#include <KPluginFactory>
 #include <KLocalizedString>
 #include <KMessageWidget>
 #include <KConfigDialogManager>
@@ -32,35 +31,7 @@
 #include "customslider.h"
 #include "sliderpair.h"
 #include "touchpadbackend.h"
-#include "version.h"
-
-K_PLUGIN_FACTORY(TouchpadConfigFactory, registerPlugin<TouchpadConfig>();)
-
-static KAboutData buildAboutData()
-{
-    KAboutData data("kcm_touchpad",
-                    QByteArray(),
-                    ki18n("Touchpad Settings"),
-                    TOUCHPAD_KCM_VERSION,
-                    ki18n("Touchpad configuration module for KDE"),
-                    KAboutData::License_GPL_V2,
-                    ki18n("Copyright © 2013 Alexander Mezin"),
-                    ki18n("This program incorporates work covered by this "
-                          "copyright notice:\n"
-                          "Copyright © 2002-2005,2007 Peter Osterlund"),
-                    QByteArray(),
-                    "mezin.alexander@gmail.com");
-
-    data.addAuthor(ki18n("Alexander Mezin"),
-                   ki18n("Developer"),
-                   "mezin.alexander@gmail.com");
-    data.addCredit(ki18n("Alex Fiestas"), ki18n("Helped a bit"));
-    data.addCredit(ki18n("Peter Osterlund"), ki18n("Developer of synclient"));
-
-    return data;
-}
-
-K_EXPORT_PLUGIN(TouchpadConfigFactory(buildAboutData()))
+#include "plugins.h"
 
 static KSharedConfig::Ptr savedDefaults()
 {
@@ -320,11 +291,10 @@ static void enableSliders(QObject *w)
 }
 
 TouchpadConfig::TouchpadConfig(QWidget *parent, const QVariantList &args)
-    : KCModule(TouchpadConfigFactory::componentData(), parent, args),
+    : KCModule(TouchpadPluginFactory::componentData(), parent, args),
       m_tabOrderSet(false), m_configOutOfSync(false)
 {
-    setAboutData(new KAboutData(
-                     *TouchpadConfigFactory::componentData().aboutData()));
+    setAboutData(new KAboutData(*componentData().aboutData()));
 
     QVBoxLayout *layout = new QVBoxLayout(this);
 
