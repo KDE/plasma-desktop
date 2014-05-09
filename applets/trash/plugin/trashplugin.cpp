d27c0559bae079d054c039181c211b8ae2dece7d
Bhushan Shah <bhush94@gmail.com>
2014-05-10 03:34:27 +0530

    Unbreak trash applet by adding KDirModel as private import
    CCBUG: 334560

---
+++ applets/trash/plugin/trashplugin.cpp
@@ -0,0 +1,33 @@
+/*
+ *   Copyright 2012 by Marco Martin <mart@kde.org>
+
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Library General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "trashplugin.h"
+#include "dirmodel.h"
+
+#include <QtQml>
+
+
+void TrashPrivatePlugin::registerTypes(const char *uri)
+{
+    Q_ASSERT(uri == QLatin1String("org.kde.plasma.private.trash"));
+    qmlRegisterType<DirModel>(uri, 1,0, "DirModel");
+}
+
+#include "moc_dirmodelplugin.cpp"
+
