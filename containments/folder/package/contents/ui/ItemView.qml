KDE/plasma-desktop

commit 04bf5d09ef8a2b467afd22da666503e1bd444783

move the hoveredItem sync in the tooltip area
Unfortunately, QQuickItem::setFiltersChildMOuseEvents() doesn't ever filter hover events,
so this is the only way, since the only place possible to catch it is the tooltip area itself

containments/folder/package/contents/ui/ItemView.qml
@@ -102,11 +102,6 @@ MouseEventListener {
         }
     }
 
-    onPositionChanged: {
-        var pos = mapToItem(gridView.contentItem, mouse.x, mouse.y);
-        gridView.hoveredItem = gridView.itemAt(pos.x, pos.y);
-    }
-
     onHoveredItemChanged: {
         doubleClickInProgress = false;
     }

containments/folder/package/contents/ui/ItemDelegate.qml
@@ -93,6 +93,7 @@ Item {
                     toolTip.icon = model.decoration;
                     toolTip.mainText = model.display;
                     toolTip.subText = model.type + "\n" + model.size + i18n(" bytes") // TODO: Format by locale.
+                    main.GridView.view.hoveredItem = main;
                 }
             }
 

commit 532fc47a9d591b0d9668cedbaec3a48dc3034f74

Make sure tracked hovered item is reset to null when leaving delegates.
Fixes a regression from 04bf5d0.

BUG:334738

containments/folder/package/contents/ui/ItemView.qml
@@ -102,6 +102,15 @@ MouseEventListener {
         }
     }
 
+    onPositionChanged: {
+        var pos = mapToItem(gridView.contentItem, mouse.x, mouse.y);
+        var item = gridView.itemAt(pos.x, pos.y);
+
+        if (!item) {
+            gridView.hoveredItem = null;
+        }
+    }
+
     onHoveredItemChanged: {
         doubleClickInProgress = false;
     }