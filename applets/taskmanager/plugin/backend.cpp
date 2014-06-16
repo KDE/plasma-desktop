74562fef561eaa111df44a9f570142efae0f0d73
Eike Hein <hein@kde.org>
2014-06-16 11:35:25 +0200

    Drop an ugly manual XFlush() from task click handling.
    Obviated by 651901d and cd509f6 in kwindowsystem.

--- applets/taskmanager/plugin/backend.cpp
+++ applets/taskmanager/plugin/backend.cpp
@@ -31,10 +31,6 @@
 #include <QQuickWindow>
 #include <QScreen>
 
-//FIXME TODO HACK See comment in Backend::activateItem().
-#include <QX11Info>
-#include <X11/Xlib.h>
-
 #include <KAuthorized>
 #include <kwindoweffects.h>
 
@@ -175,10 +171,6 @@ void Backend::activateItem(int id, bool toggle)
         } else {
             taskItem->task()->activate();
         }
-
-        //FIXME TODO HACK This papers over XSendEvent() calls lacking an explicit flush in
-        //KWindowSystem; not addressed there due to pending xcb port.
-        XFlush(QX11Info::display());
     } else if (item->itemType() == TaskManager::LauncherItemType) {
         static_cast<TaskManager::LauncherItem*>(item)->launch();
     }
