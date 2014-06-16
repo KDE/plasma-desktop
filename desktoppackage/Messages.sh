b550f338980033ac605ce3aad7b027a8b8de47d4
Lukáš Tinkl <lukas@kde.org>
2014-06-16 19:01:31 +0200

    i18n fixes

--- desktoppackage/Messages.sh
+++ desktoppackage/Messages.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
-$XGETTEXT `find . -name \*.qml` -L Java -o $podir/plasma_shell_org.kde.desktop.pot
+$XGETTEXT `find . -name \*.qml` -L Java -o $podir/plasma_shell_org.kde.plasma.desktop.pot
 rm -f rc.cpp
