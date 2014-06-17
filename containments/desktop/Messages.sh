d57abcee221b3292251f4873faef28f740f5016c
Lukáš Tinkl <lukas@kde.org>
2014-06-17 12:56:39 +0200

    rename to plasma_applet_foo
    to get the catalog loading work

--- containments/desktop/Messages.sh
+++ containments/desktop/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.qml` -L Java -o $podir/plasma_containment_org.kde.desktopcontainment.pot
+$XGETTEXT `find . -name \*.qml` -L Java -o $podir/plasma_applet_org.kde.desktopcontainment.pot
 rm -f rc.cpp
