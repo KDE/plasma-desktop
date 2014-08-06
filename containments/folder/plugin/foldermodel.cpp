KDE/plasma-desktop

commit a9c56140bd5f1fb227b528dd38d418358da7e497

Do a sanity check and reject drops on KFileItems that can't handle them.
Ideally the view would reject these already during move based on the
QAbstractItemModel item flags for the destination model index, however
the DeclarativeDropArea component is currently incapable of rejecting
drag events. Accordingly this also adds a TODO to the code so we don't
forget in the future.

BUG:338021

containments/folder/plugin/foldermodel.cpp
@@ -701,6 +701,10 @@ void FolderModel::drop(QQuickItem *target, QObject* dropEvent, int row)
         return;
     }
 
+    if (!item.acceptsDrops()) {
+        return;
+    }
+
     QPoint pos;
     pos.setX(dropEvent->property("x").toInt());
     pos.setY(dropEvent->property("y").toInt());

containments/folder/package/contents/ui/main.qml
@@ -153,6 +153,10 @@ DragDrop.DropArea {
     }
 
     onDragMove: {
+        // TODO: We should reject drag moves onto file items that don't accept drops
+        // (cf. QAbstractItemModel::flags() here, but DeclarativeDropArea currently
+        // is currently incapable of rejecting drag events.
+
         // Trigger autoscroll.
         if (event.mimeData.urls.length) {
             itemView.scrollLeft = (event.x < (units.largeSpacing * 3));
@@ -458,7 +462,7 @@ DragDrop.DropArea {
             return;
         } else {
             //WORKAROUND: that's the only place where we can inject a sensible size.
-            //if root has width defined, it will override the value we set before 
+            //if root has width defined, it will override the value we set before
             //the component completes
             root.width = plasmoid.width
         }

commit c6b7840db81221839d928e7c239b1bb4f916bb99

Fix crash regression from a9c5614.
BUG:338045

Also port check away from deprecated KFileItem API.

containments/folder/plugin/foldermodel.cpp
@@ -106,6 +106,7 @@ FolderModel::FolderModel(QObject *parent) : QSortFilterProxyModel(parent),
 
     m_dirModel = new KDirModel(this);
     m_dirModel->setDirLister(dirLister);
+    m_dirModel->setDropsAllowed(KDirModel::DropOnDirectory | KDirModel::DropOnLocalExecutable);
 
     m_selectionModel = new QItemSelectionModel(this, this);
     connect(m_selectionModel, SIGNAL(selectionChanged(QItemSelection,QItemSelection)),
@@ -678,10 +679,12 @@ void FolderModel::drop(QQuickItem *target, QObject* dropEvent, int row)
         return;
     }
 
+    QModelIndex idx;
     KFileItem item;
 
     if (row > -1 && row < rowCount()) {
-         item = itemForIndex(index(row, 0));
+         idx = index(row, 0);
+         item = itemForIndex(idx);
     }
 
     if (item.isNull() &&
@@ -701,7 +704,7 @@ void FolderModel::drop(QQuickItem *target, QObject* dropEvent, int row)
         return;
     }
 
-    if (!item.acceptsDrops()) {
+    if (idx.isValid() && !(flags(idx) & Qt::ItemIsDropEnabled)) {
         return;
     }
 