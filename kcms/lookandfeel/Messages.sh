b50ac561b08b0409c821f21b1be10e51c4633358
Lasse Liehu <lasse.liehu@gmail.com>
2014-10-16 18:29:43 +0300

    Extract messages from qml files in look and feel kcm
    This adds one previously untranslatable string to kcm_lookandfeel.pot.
    
    CCMAIL: kde-i18n-doc@kde.org

--- kcms/lookandfeel/Messages.sh
+++ kcms/lookandfeel/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name "*.ui"` >> rc.cpp || exit 11
-$XGETTEXT `find . -name "*.cpp"` -o $podir/kcm_lookandfeel.pot
+$XGETTEXT `find . -name "*.cpp" -o -name "*.qml"` -o $podir/kcm_lookandfeel.pot
 rm -f rc.cpp
