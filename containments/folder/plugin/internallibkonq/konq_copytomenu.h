KDE/plasma-desktop

commit b29f85dfd0b7576359b0d540c135aea10f98ac05

Update from the real libkonq, to make sure nothing that was removed creates a problem here.
Everything seems fine.

containments/folder/plugin/internallibkonq/konq_copytomenu.h
@@ -21,8 +21,10 @@
 #ifndef KONQ_COPYTOMENU_H
 #define KONQ_COPYTOMENU_H
 
-#include <kurl.h>
+#include <QUrl>
+
 #include <kfileitem.h>
+#include <libkonq_export.h>
 
 class QMenu;
 class KonqCopyToMenuPrivate;
@@ -33,15 +35,6 @@ class KonqCopyToMenuPrivate;
 class KonqCopyToMenu
 {
 public:
-    /**
-     * Creates a KonqCopyToMenu instance.
-     * Note that this instance must stay alive for at least as long as the popupmenu;
-     * it has the slots for the actions created by addActionsTo.
-     * @deprecated
-     * KDE5: remove, so that passing a parent widget is mandatory
-     */
-    KonqCopyToMenu();
-
     /**
      * Creates a KonqCopyToMenu instance, with a parent widget for the file dialog
      * and message boxes.
@@ -67,7 +60,7 @@ public:
      * Sets the URLs which the actions apply to.
      * Either call this or setItems.
      */
-    void setUrls(const KUrl::List& urls);
+    void setUrls(const QList<QUrl>& urls);
 
     /**
      * If setReadOnly(true) is called, the "Move To" submenu will not appear.

containments/folder/plugin/internallibkonq/konq_dndpopupmenuplugin.h
@@ -18,12 +18,13 @@
 #ifndef _KONQ_DNDPOPUPMENUPLUGIN_H_
 #define _KONQ_DNDPOPUPMENUPLUGIN_H_
 
+#include "libkonq_export.h"
 #include <QObject>
 
 class QMenu;
 class KActionCollection;
 class KFileItemListProperties;
-class KUrl;
+class QUrl;
 class QAction;
 
 /**
@@ -58,7 +59,7 @@ public:
      * the menu.
      */
     virtual void setup(const KFileItemListProperties& popupMenuInfo,
-            KUrl destination,
+            QUrl destination,
             QList<QAction*>& pluginActions) = 0;
 };
 

containments/folder/plugin/internallibkonq/konq_operations.h
@@ -23,10 +23,11 @@
 #ifndef __konq_operations_h__
 #define __konq_operations_h__
 
-#include <kurl.h>
+#include <libkonq_export.h>
 
 #include <QObject>
 #include <QDropEvent>
+#include <QUrl>
 
 class KJob;
 namespace KIO { class Job; class SimpleJob; }
@@ -46,44 +47,6 @@ protected:
     virtual ~KonqOperations();
 
 public:
-    /**
-     * Pop up properties dialog for mimetype @p mimeType.
-     * @param parent parent widget (for dialogs)
-     */
-    static void editMimeType( const QString & mimeType, QWidget* parent );
-
-    enum Operation { TRASH, DEL, COPY, MOVE, LINK, EMPTYTRASH, STAT, MKDIR, RESTORE, UNKNOWN, PUT, RENAME };
-    /**
-     * Delete the @p selectedUrls if possible.
-     *
-     * @param parent parent widget (for error dialog box if any)
-     * @param method should be TRASH or DEL
-     * @param selectedUrls the URLs to be deleted
-     */
-    static void del( QWidget * parent, Operation method, const KUrl::List & selectedUrls );
-
-    /**
-     * Copy the @p selectedUrls to the destination @p destUrl.
-     *
-     * @param parent parent widget (for error dialog box if any)
-     * @param method should be COPY, MOVE or LINK
-     * @param selectedUrls the URLs to copy
-     * @param destUrl destination of the copy
-     *
-     * @todo document restrictions on the kind of destination
-     */
-    static void copy( QWidget * parent, Operation method, const KUrl::List & selectedUrls, const KUrl& destUrl );
-    /**
-     * Drop
-     * @param destItem destination KFileItem for the drop (background or item)
-     * @param destUrl destination URL for the drop.
-     * @param ev the drop event
-     * @param parent parent widget (for error dialog box if any)
-     *
-     * If destItem is 0L, doDrop will stat the URL to determine it.
-     */
-    static void doDrop( const KFileItem & destItem, const KUrl & destUrl, QDropEvent * ev, QWidget * parent );
-
     /**
      * Drop
      * @param destItem destination KFileItem for the drop (background or item)
@@ -109,172 +72,28 @@ public:
      * @return The KonqOperations object
      * @since 4.3
      */
-    static KonqOperations *doDrop( const KFileItem & destItem, const KUrl & destUrl, QDropEvent * ev, QWidget * parent,
-                                   const QList<QAction*> &userActions );
-
-    /**
-     * Paste the clipboard contents
-     */
-    static void doPaste( QWidget * parent, const KUrl & destUrl, const QPoint &pos = QPoint() );
-
-    /**
-     * Paste the clipboard contents
-     *
-     * @return The KonqOperations object
-     * @since 4.10
-     *
-     * @todo TODO KDE 5,0 - Merge doPaste and doPasteV2
-     */
-    static KonqOperations *doPasteV2(QWidget * parent, const KUrl & destUrl, const QPoint &pos = QPoint());
-
-    /**
-     * Returns the state of the paste action:
-     * first is whether the action should be enabled
-     * second is the text for the action
-     * @since 4.3
-     */
-    static QPair<bool, QString> pasteInfo(const KUrl& targetUrl);
-
-    /**
-     * Empty the trash
-     */
-    static void emptyTrash( QWidget* parent );
-    /**
-     * Restore trashed items
-     */
-    static void restoreTrashedItems( const KUrl::List& urls, QWidget* parent );
-
-    /**
-     * Create a directory. Same as KIO::mkdir but records job into KonqFileUndoManager for undo/redo purposes.
-     */
-    static KIO::SimpleJob* mkdir( QWidget *parent, const KUrl & url );
-
-    /**
-     * Ask for the name of a new directory and create it.
-     * Calls KonqOperations::mkdir.
-     *
-     * @param parent the parent widget
-     * @param baseUrl the directory to create the new directory in
-     * @return the job used to create the directory or 0 if the creation was cancelled by the user
-     */
-    static KIO::SimpleJob* newDir( QWidget * parent, const KUrl & baseUrl );
-    enum NewDirFlag { ViewShowsHiddenFile = 1 };
-    Q_DECLARE_FLAGS(NewDirFlags, NewDirFlag)
-    /**
-     * Ask for the name of a new directory and create it.
-     * Calls KonqOperations::mkdir.
-     *
-     * @param parent the parent widget
-     * @param baseUrl the directory to create the new directory in
-     * @param flags see NewDirFlags
-     * @return the job used to create the directory or 0 if the creation was cancelled by the user
-     */
-    static KIO::SimpleJob* newDir( QWidget * parent, const KUrl & baseUrl, NewDirFlags flags );
-
-    /**
-     * Get info about a given URL, and when that's done (it's asynchronous!),
-     * call a given slot with a const KFileItem& as argument.
-     * The KFileItem will be deleted by statUrl after calling the slot. Make a copy
-     * if you need one !
-     */
-    static void statUrl( const KUrl & url, const QObject *receiver, const char *member, QWidget* parent );
-
-    /**
-     * Do a renaming.
-     * @param parent the parent widget, passed to KonqOperations ctor
-     * @param oldurl the current url of the file to be renamed
-     * @param name the new name for the file. Shouldn't include '/'.
-     */
-    static void rename( QWidget * parent, const KUrl & oldurl, const QString & name );
-
-    /**
-     * Do a renaming.
-     * @param parent the parent widget, passed to KonqOperations ctor
-     * @param oldurl the current url of the file to be renamed
-     * @param name the new name for the file. Shouldn't include '/'.
-     *
-     * @return The KonqOperations object
-     * @since 4.11
-     *
-     * @todo TODO KDE 5.0 - Merge rename and renameV2
-     */
-    static KonqOperations *renameV2( QWidget * parent, const KUrl & oldurl, const QString & name );
-
-    /**
-     * Do a renaming.
-     * @param parent the parent widget, passed to KonqOperations ctor
-     * @param oldurl the current url of the file to be renamed
-     * @param newurl the new url for the file
-     * Use this version if the other one wouldn't work :)  (e.g. because name could
-     * be a relative path, including a '/').
-     */
-    static void rename( QWidget * parent, const KUrl & oldurl, const KUrl & newurl );
-
-    /**
-     * Do a renaming.
-     * @param parent the parent widget, passed to KonqOperations ctor
-     * @param oldurl the current url of the file to be renamed
-     * @param newurl the new url for the file
-     * Use this version if the other one wouldn't work :)  (e.g. because name could
-     * be a relative path, including a '/').
-     *
-     * @return The KonqOperations object
-     * @since 4.11
-     *
-     * @todo TODO KDE 5.0 - Merge rename and renameV2
-     */
-    static KonqOperations *renameV2( QWidget * parent, const KUrl & oldurl, const KUrl & newurl );
-
-    enum ConfirmationType { DEFAULT_CONFIRMATION, SKIP_CONFIRMATION, FORCE_CONFIRMATION };
-    /**
-     * Ask for confirmation before deleting/trashing @p selectedUrls.
-     * @param selectedUrls the urls about to be deleted
-     * @param method the type of deletion (DEL for real deletion, anything else for trash)
-     * @param confirmation default (based on config file), skip (no confirmation) or force (always confirm)
-     * @param widget parent widget for message boxes
-     * @return true if confirmed
-     */
-    static bool askDeleteConfirmation( const KUrl::List & selectedUrls, int method, ConfirmationType confirmation, QWidget* widget );
-
-    /**
-     * Returns the list of dropped URL's.
-     *
-     * You can call this method on the object returned by KonqOperations::doDrop(),
-     * to obtain the list of URL's this object handles.
-     *
-     * @since 4.3
-     */
-    KUrl::List droppedUrls() const;
-
-    /**
-     * Returns the position where the drop occurred.
-     * @since 4.3
-     */
-    QPoint dropPosition() const;
+    static KonqOperations *doDrop(const KFileItem & destItem, const QUrl &destUrl, QDropEvent * ev, QWidget * parent,
+                                   const QList<QAction*> &userActions = QList<QAction *>() );
 
 Q_SIGNALS:
-    void statFinished( const KFileItem & item );
-    void aboutToCreate(const KUrl::List &urls);
-    void renamingFailed(const KUrl &oldUrl, const KUrl &newUrl);
+    void aboutToCreate(const QList<QUrl> &urls);
 
 private:
     QWidget* parentWidget() const;
-    void _del( Operation method, const KUrl::List & selectedUrls, ConfirmationType confirmation );
-    void _restoreTrashedItems( const KUrl::List& urls );
-    void _statUrl( const KUrl & url, const QObject *receiver, const char *member );
-    void _addPluginActions(QList<QAction*>& pluginActions, const KUrl& destination, const KFileItemListProperties& info);
+    void _addPluginActions(QList<QAction*>& pluginActions, const QUrl& destination, const KFileItemListProperties& info);
 
     // internal, for COPY/MOVE/LINK/MKDIR
-    void setOperation( KIO::Job * job, Operation method, const KUrl & dest );
+    enum Operation { TRASH, COPY, MOVE, LINK, UNKNOWN, PUT };
+    void setOperation( KIO::Job * job, Operation method, const QUrl & dest );
 
     struct DropInfo
     {
-        DropInfo( Qt::KeyboardModifiers k, const KUrl::List & u, const QMap<QString,QString> &m,
+        DropInfo( Qt::KeyboardModifiers k, const QList<QUrl> & u, const QMap<QString,QString> &m,
                   const QPoint& pos, Qt::DropAction a, const QList<QAction *> &actions) :
             keyboardModifiers(k), urls(u), metaData(m), mousePos(pos), action(a), userActions(actions)
         {}
         Qt::KeyboardModifiers keyboardModifiers;
-        KUrl::List urls;
+        QList<QUrl> urls;
         QMap<QString,QString> metaData;
         QPoint mousePos;
         Qt::DropAction action;
@@ -283,50 +102,21 @@ private:
     // internal, for doDrop
     void setDropInfo( DropInfo * info ) { m_info = info; }
 
-    struct KIOPasteInfo // KDE4: remove and use DropInfo instead or a QPoint member
-    {
-        // Used to position the files at the position where RMB/Paste was used [mostly on the desktop]
-        QPoint mousePos;
-    };
-    void setPasteInfo( KIOPasteInfo * info ) { m_pasteInfo = info; }
-
 protected Q_SLOTS:
 
     void slotResult( KJob * job );
     void slotStatResult( KJob * job );
     void asyncDrop( const KFileItem & item );
     void doDropFileCopy();
-    void slotCopyingDone(KIO::Job *job, const KUrl &from, const KUrl &to);
-    void slotCopyingLinkDone(KIO::Job *job, const KUrl &from, const QString &target, const KUrl &to);
+    void slotCopyingDone(KIO::Job *job, const QUrl &from, const QUrl &to);
+    void slotCopyingLinkDone(KIO::Job *job, const QUrl &from, const QString &target, const QUrl &to);
 
 private:
     Operation m_method;
-    //KUrl::List m_srcUrls;
-    KUrl::List m_createdUrls;
-    KUrl m_destUrl;
+    QList<QUrl> m_createdUrls;
+    QUrl m_destUrl;
     // for doDrop
     DropInfo * m_info;
-    KIOPasteInfo * m_pasteInfo;
-};
-
-#include <kio/job.h>
-
-/// Restore multiple trashed files
-class KonqMultiRestoreJob : public KIO::Job
-{
-    Q_OBJECT
-
-public:
-    KonqMultiRestoreJob( const KUrl::List& urls );
-
-protected Q_SLOTS:
-    virtual void slotStart();
-    virtual void slotResult( KJob *job );
-
-private:
-    const KUrl::List m_urls;
-    KUrl::List::const_iterator m_urlsIterator;
-    int m_progress;
 };
 
 #endif

containments/folder/plugin/internallibkonq/konq_popupmenu.h
@@ -26,18 +26,20 @@
 #include <QMap>
 #include <QMenu>
 
-#include <kaction.h>
+#include <QAction>
 #include <kactioncollection.h>
 #include <kfileitem.h>
 #include <kparts/browserextension.h>
 #include <kservice.h>
+
+#include <libkonq_export.h>
 
 class KNewFileMenu;
 class KFileItemActions;
 
 class KBookmarkManager;
 class KonqPopupMenuPrivate;
-class KUrl;
+class QUrl;
 
 /**
  * This class implements the popup menu for URLs in konqueror and kdesktop
@@ -82,7 +84,7 @@ public:
    * @todo that list is probably not be up-to-date
    */
   KonqPopupMenu( const KFileItemList &items,
-                 const KUrl& viewURL,
+                 const QUrl& viewURL,
                  KActionCollection & actions,
                  KNewFileMenu * newMenu,
                  Flags appFlags,

containments/folder/plugin/internallibkonq/konq_popupmenuplugin.h
@@ -21,6 +21,7 @@
 #ifndef KONQ_POPUPMENUPLUGIN_H
 #define KONQ_POPUPMENUPLUGIN_H
 
+#include "libkonq_export.h"
 #include <QObject>
 
 class QMenu;

containments/folder/plugin/internallibkonq/konq_copytomenu.cpp
@@ -21,16 +21,19 @@
 
 #include "konq_copytomenu.h"
 #include "konq_copytomenu_p.h"
-#include "konq_operations.h"
-#include <kaction.h>
+#include <QAction>
 #include <kdebug.h>
-#include <kicon.h>
+#include <QIcon>
 #include <kglobal.h>
 #include <kfiledialog.h>
 #include <klocale.h>
 #include <kmenu.h>
 #include <kmimetype.h>
 #include <kstringhandler.h>
+#include <KJobWidgets>
+#include <KIO/FileUndoManager>
+#include <KIO/CopyJob>
+#include <KIO/JobUiDelegate>
 #include <QDir>
 
 #ifdef Q_OS_WIN
@@ -43,11 +46,6 @@ KonqCopyToMenuPrivate::KonqCopyToMenuPrivate(QWidget* parentWidget)
 }
 
 ////
-
-KonqCopyToMenu::KonqCopyToMenu()
-    : d(new KonqCopyToMenuPrivate())
-{
-}
 
 KonqCopyToMenu::KonqCopyToMenu(QWidget* parentWidget)
     : d(new KonqCopyToMenuPrivate(parentWidget))
@@ -68,7 +66,7 @@ void KonqCopyToMenu::setItems(const KFileItemList& items)
         d->m_urls.append(item.url());
 }
 
-void KonqCopyToMenu::setUrls(const KUrl::List& urls)
+void KonqCopyToMenu::setUrls(const QList<QUrl> &urls)
 {
     d->m_urls = urls;
 }
@@ -99,7 +97,7 @@ KonqCopyToMainMenu::KonqCopyToMainMenu(QMenu* parent, KonqCopyToMenuPrivate* _d,
     : KMenu(parent), m_menuType(menuType),
       m_actionGroup(static_cast<QWidget *>(0)),
       d(_d),
-      m_recentDirsGroup(KGlobal::config(), m_menuType == Copy ? "kuick-copy" : "kuick-move")
+      m_recentDirsGroup(KSharedConfig::openConfig(), m_menuType == Copy ? "kuick-copy" : "kuick-move")
 {
     connect(this, &KonqCopyToMainMenu::aboutToShow, this, &KonqCopyToMainMenu::slotAboutToShow);
     connect(&m_actionGroup, &QActionGroup::triggered, this, &KonqCopyToMainMenu::slotTriggered);
@@ -112,14 +110,14 @@ void KonqCopyToMainMenu::slotAboutToShow()
     // Home Folder
     subMenu = new KonqCopyToDirectoryMenu(this, this, QDir::homePath());
     subMenu->setTitle(i18nc("@title:menu", "Home Folder"));
-    subMenu->setIcon(KIcon("go-home"));
+    subMenu->setIcon(QIcon::fromTheme("go-home"));
     addMenu(subMenu);
 
     // Root Folder
 #ifndef Q_OS_WIN
     subMenu = new KonqCopyToDirectoryMenu(this, this, QDir::rootPath());
     subMenu->setTitle(i18nc("@title:menu", "Root Folder"));
-    subMenu->setIcon(KIcon("folder-red"));
+    subMenu->setIcon(QIcon::fromTheme("folder-red"));
     addMenu(subMenu);
 #else
     foreach ( const QFileInfo& info, QDir::drives() ) {
@@ -148,14 +146,14 @@ void KonqCopyToMainMenu::slotAboutToShow()
         }
         subMenu = new KonqCopyToDirectoryMenu(this, this, info.absoluteFilePath());
         subMenu->setTitle(info.absoluteFilePath());
-        subMenu->setIcon(KIcon(driveIcon));
+        subMenu->setIcon(QIcon::fromTheme(driveIcon));
         addMenu(subMenu);
     }
 #endif
 
     // Browse... action, shows a KFileDialog
-    KAction* browseAction = new KAction(i18nc("@title:menu in Copy To or Move To submenu", "Browse..."), this);
-    connect(browseAction, &KAction::triggered, this, &KonqCopyToMainMenu::slotBrowse);
+    QAction * browseAction = new QAction(i18nc("@title:menu in Copy To or Move To submenu", "Browse..."), this);
+    connect(browseAction, &QAction::triggered, this, &KonqCopyToMainMenu::slotBrowse);
     addAction(browseAction);
 
     addSeparator(); // looks like Qt4 handles removing it automatically if it's last in the menu, nice.
@@ -163,9 +161,9 @@ void KonqCopyToMainMenu::slotAboutToShow()
     // Recent Destinations
     const QStringList recentDirs = m_recentDirsGroup.readPathEntry("Paths", QStringList());
     Q_FOREACH(const QString& recentDir, recentDirs) {
-        const KUrl url(recentDir);
-        const QString text = KStringHandler::csqueeze(url.pathOrUrl(), 60); // shorten very long paths (#61386)
-        KAction* act = new KAction(text, this);
+        const QUrl url(recentDir);
+        const QString text = KStringHandler::csqueeze(url.toDisplayString(), 60); // shorten very long paths (#61386)
+        QAction * act = new QAction(text, this);
         act->setData(url);
         m_actionGroup.addAction(act);
         addAction(act);
@@ -174,8 +172,8 @@ void KonqCopyToMainMenu::slotAboutToShow()
 
 void KonqCopyToMainMenu::slotBrowse()
 {
-    const KUrl dest = KFileDialog::getExistingDirectoryUrl(KUrl("kfiledialog:///copyto"),
-                                             d->m_parentWidget ? d->m_parentWidget : this);
+    const QUrl dest = KFileDialog::getExistingDirectoryUrl(QUrl("kfiledialog:///copyto"), // FIXME
+                                                           d->m_parentWidget ? d->m_parentWidget : this);
     if (!dest.isEmpty()) {
         copyOrMoveTo(dest);
     }
@@ -183,16 +181,16 @@ void KonqCopyToMainMenu::slotBrowse()
 
 void KonqCopyToMainMenu::slotTriggered(QAction* action)
 {
-    const KUrl url = action->data().value<KUrl>();
+    const QUrl url = action->data().value<QUrl>();
     Q_ASSERT(!url.isEmpty());
     copyOrMoveTo(url);
 }
 
-void KonqCopyToMainMenu::copyOrMoveTo(const KUrl& dest)
+void KonqCopyToMainMenu::copyOrMoveTo(const QUrl& dest)
 {
     // Insert into the recent destinations list
     QStringList recentDirs = m_recentDirsGroup.readPathEntry("Paths", QStringList());
-    const QString niceDest = dest.pathOrUrl();
+    const QString niceDest = dest.toDisplayString();
     if (!recentDirs.contains(niceDest)) { // don't change position if already there, moving stuff is bad usability
         recentDirs.prepend(niceDest);
         while (recentDirs.size() > 10) { // hardcoded max size
@@ -204,13 +202,15 @@ void KonqCopyToMainMenu::copyOrMoveTo(const KUrl& dest)
     // #199549: add a trailing slash to avoid unexpected results when the
     // dest doesn't exist anymore: it was creating a file with the name of
     // the now non-existing dest.
-    KUrl dirDest = dest;
-    dirDest.adjustPath(KUrl::AddTrailingSlash);
+    QUrl dirDest = dest;
+    if (!dirDest.path().endsWith('/'))
+        dirDest.setPath(dirDest.path() + '/');
 
     // And now let's do the copy or move -- with undo/redo support.
-    KonqOperations::copy(d->m_parentWidget ? d->m_parentWidget : this,
-                         m_menuType == Copy ? KonqOperations::COPY : KonqOperations::MOVE,
-                         d->m_urls, dirDest);
+    KIO::CopyJob* job = m_menuType == Copy ? KIO::copy(d->m_urls, dirDest) : KIO::move(d->m_urls, dirDest);
+    KIO::FileUndoManager::self()->recordCopyJob(job);
+    KJobWidgets::setWindow(job, d->m_parentWidget ? d->m_parentWidget : this);
+    job->ui()->setAutoErrorHandlingEnabled(true); // or connect to the result signal
 }
 
 ////
@@ -224,10 +224,10 @@ KonqCopyToDirectoryMenu::KonqCopyToDirectoryMenu(QMenu* parent, KonqCopyToMainMe
 void KonqCopyToDirectoryMenu::slotAboutToShow()
 {
     clear();
-    KAction* act = new KAction(m_mainMenu->menuType() == Copy
+    QAction * act = new QAction(m_mainMenu->menuType() == Copy
                                ? i18nc("@title:menu", "Copy Here")
                                : i18nc("@title:menu", "Move Here"), this);
-    act->setData(KUrl(m_path));
+    act->setData(QUrl::fromLocalFile(m_path));
     act->setEnabled(QFileInfo(m_path).isWritable());
     m_mainMenu->actionGroup().addAction(act);
     addAction(act);
@@ -252,7 +252,7 @@ void KonqCopyToDirectoryMenu::slotAboutToShow()
         // correctly and not misinterpreted as an indicator for a keyboard shortcut
         subMenu->setTitle(menuTitle.replace('&', "&&"));
         const QString iconName = dirMime->iconName();
-        subMenu->setIcon(KIcon(iconName));
+        subMenu->setIcon(QIcon::fromTheme(iconName));
         if (QFileInfo(subPath).isSymLink()) { // I hope this isn't too slow...
             QFont font = subMenu->menuAction()->font();
             font.setItalic(true);

containments/folder/plugin/internallibkonq/konq_copytomenu_p.h
@@ -23,14 +23,14 @@
 #include <kmenu.h>
 #include <QActionGroup>
 #include <QObject>
-#include <kurl.h>
+#include <QUrl>
 
 class KonqCopyToMenuPrivate
 {
 public:
-    KonqCopyToMenuPrivate(QWidget* parentWidget = 0);
+    KonqCopyToMenuPrivate(QWidget* parentWidget);
 
-    KUrl::List m_urls;
+    QList<QUrl> m_urls;
     bool m_readOnly;
     QWidget* m_parentWidget;
 };
@@ -54,7 +54,7 @@ private Q_SLOTS:
     void slotTriggered(QAction* action);
 
 private:
-    void copyOrMoveTo(const KUrl& dest);
+    void copyOrMoveTo(const QUrl &dest);
 
 private:
     MenuType m_menuType;

commit 16c8b2d182bf2d7bb110f131622fb4512e416f0f

drop libkonq_export for internal libkonq copy
as suggested by David Faure to fix the build fail introduced in b29f85d
since the internal libkonq does not have an export header

REVIEW: 120954

containments/folder/plugin/internallibkonq/konq_copytomenu.h
@@ -24,7 +24,6 @@
 #include <QUrl>
 
 #include <kfileitem.h>
-#include <libkonq_export.h>
 
 class QMenu;
 class KonqCopyToMenuPrivate;

containments/folder/plugin/internallibkonq/konq_dndpopupmenuplugin.h
@@ -18,7 +18,6 @@
 #ifndef _KONQ_DNDPOPUPMENUPLUGIN_H_
 #define _KONQ_DNDPOPUPMENUPLUGIN_H_
 
-#include "libkonq_export.h"
 #include <QObject>
 
 class QMenu;

containments/folder/plugin/internallibkonq/konq_operations.h
@@ -23,8 +23,6 @@
 #ifndef __konq_operations_h__
 #define __konq_operations_h__
 
-#include <libkonq_export.h>
-
 #include <QObject>
 #include <QDropEvent>
 #include <QUrl>

containments/folder/plugin/internallibkonq/konq_popupmenu.h
@@ -32,8 +32,6 @@
 #include <kparts/browserextension.h>
 #include <kservice.h>
 
-#include <libkonq_export.h>
-
 class KNewFileMenu;
 class KFileItemActions;
 

containments/folder/plugin/internallibkonq/konq_popupmenuplugin.h
@@ -21,7 +21,6 @@
 #ifndef KONQ_POPUPMENUPLUGIN_H
 #define KONQ_POPUPMENUPLUGIN_H
 
-#include "libkonq_export.h"
 #include <QObject>
 
 class QMenu;