91786b418e718639fd044a8f2feae80acd8eb267
Lukáš Tinkl <lukas@kde.org>
2015-01-30 00:12:31 +0100

    Set global shortcuts for touchpad enable/disable/toggle
    Patch by: Rajeesh K Nambiar
    with minor modifications to let it compile :)
    
    REVIEW: 122319
    BUG: 342629

--- src/kded/kdedactions.cpp
+++ src/kded/kdedactions.cpp
@@ -33,17 +33,29 @@ TouchpadGlobalActions::TouchpadGlobalActions(bool isConfiguration, QObject *pare
     QAction *enable = addAction("Enable Touchpad");
     enable->setText(i18n("Enable Touchpad"));
     connect(enable, SIGNAL(triggered()), SIGNAL(enableTriggered()));
+    bool okEnable = KGlobalAccel::setGlobalShortcut(enable, QKeySequence(Qt::Key_TouchpadOn));
+	if (!okEnable) {
+		qDebug() << "Couldn't set global shortcut to Qt::Key_TouchpadOn. There's another program using it, otherwise file a bug against kcm_touchpad";
+    }
 
     QAction *disable = addAction("Disable Touchpad");
     disable->setText(i18n("Disable Touchpad"));
     connect(disable, SIGNAL(triggered()), SIGNAL(disableTriggered()));
+    bool okDisable = KGlobalAccel::setGlobalShortcut(disable, QKeySequence(Qt::Key_TouchpadOff));
+	if (!okDisable) {
+		qDebug() << "Couldn't set global shortcut to Qt::Key_TouchpadOff. There's another program using it, otherwise file a bug against kcm_touchpad";
+	}
 
     QAction *toggle = addAction("Toggle Touchpad");
     toggle->setText(i18n("Toggle Touchpad"));
     connect(toggle, SIGNAL(triggered()), SIGNAL(toggleTriggered()));
+    bool okToggle = KGlobalAccel::setGlobalShortcut(toggle, QKeySequence(Qt::Key_TouchpadToggle));
+	if (!okToggle) {
+		qDebug() << "Couldn't set global shortcut to Qt::Key_TouchpadToggle. There's another program using it, otherwise file a bug against kcm_touchpad";
+	}
 
     Q_FOREACH (QAction *act, actions()) {
-        KGlobalAccel::setGlobalShortcut(act,QKeySequence());
+        //KGlobalAccel::setGlobalShortcut(act,QKeySequence());
         KActionCollection::setShortcutsConfigurable(act,true);
         if (isConfiguration) {
             act->setProperty("isConfigurationAction", true);
