12f750d497c5f2def14d89ad669057e13197b6f8
Lukáš Tinkl <lukas@kde.org>
2015-03-19 15:41:14 +0100

    extract UI messages

--- kcms/access/Messages.sh
+++ kcms/access/Messages.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
 ### TODO: why do we need 2 POT files for a single directory?
+$EXTRACTRC *.ui >> rc.cpp
 $XGETTEXT kaccess.cpp main.cpp rc.cpp -o $podir/kaccess.pot
 $XGETTEXT kcmaccess.cpp rc.cpp -o $podir/kcmaccess.pot
