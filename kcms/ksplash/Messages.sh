478c2ee06691d3d04c452d618a19cb476df1926f
Lukáš Tinkl <lukas@kde.org>
2015-03-23 14:24:41 +0100

    really extract all messages

--- kcms/ksplash/Messages.sh
+++ kcms/ksplash/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name "*.ui"` >> rc.cpp || exit 11
-$XGETTEXT `find . -name "*.cpp" -name "*.qml"` -o $podir/kcm_splashscreen.pot
+$XGETTEXT `find . -name "*.cpp" -o -name "*.qml"` -o $podir/kcm_splashscreen.pot
 rm -f rc.cpp
