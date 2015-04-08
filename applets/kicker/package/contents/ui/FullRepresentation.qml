KDE/plasma-desktop

commit 33f33fb8590db3737aee6be73ae8253e6043ea7e

Support for separator menu items + show a separator below the Recent* categories.

applets/kicker/package/contents/ui/FullRepresentation.qml
@@ -30,12 +30,14 @@ FocusScope {
     Layout.minimumWidth: sideBar.width + mainRow.spacing + Math.max(rootList.width, runnerColumns.width)
     Layout.maximumWidth: sideBar.width + mainRow.spacing + Math.max(rootList.width, runnerColumns.width)
 
-    Layout.minimumHeight: Math.max((rootModel.count * rootList.itemHeight) + searchField.height + (2 * units.smallSpacing),
-        sideBar.margins.top + sideBar.margins.bottom + favoriteApps.contentHeight + favoriteSystemActions.contentHeight
-        + sidebarSeparator.height + (4 * units.smallSpacing))
-    Layout.maximumHeight: Math.max((rootModel.count * rootList.itemHeight) + searchField.height + (2 * units.smallSpacing),
-        sideBar.margins.top + sideBar.margins.bottom + favoriteApps.contentHeight + favoriteSystemActions.contentHeight
-        + sidebarSeparator.height + (4 * units.smallSpacing))
+    Layout.minimumHeight: Math.max(((rootModel.count - (rootList.hasDivider ? 1 : 0)) * rootList.itemHeight)
+        + searchField.height + (2 * units.smallSpacing), sideBar.margins.top + sideBar.margins.bottom
+        + favoriteApps.contentHeight + favoriteSystemActions.contentHeight + sidebarSeparator.height
+        + (4 * units.smallSpacing)) + (rootList.hasDivider ? rootList.dividerHeight : 0)
+    Layout.maximumHeight: Math.max(((rootModel.count - (rootList.hasDivider ? 1 : 0)) * rootList.itemHeight)
+        + searchField.height + (2 * units.smallSpacing), sideBar.margins.top + sideBar.margins.bottom
+        + favoriteApps.contentHeight + favoriteSystemActions.contentHeight + sidebarSeparator.height
+        + (4 * units.smallSpacing)) + (rootList.hasDivider ? rootList.dividerHeight : 0)
 
     signal appendSearchText(string text)
 
@@ -165,6 +167,9 @@ FocusScope {
             height: (rootModel.count * rootList.itemHeight)
 
             visible: (searchField.text == "")
+
+            property bool hasDivider: (rootModel.showRecentApps
+                || rootModel.showRecentDocs || rootModel.showRecentContacts)
 
             iconsEnabled: false
 

applets/kicker/package/contents/ui/ItemListDelegate.qml
@@ -28,17 +28,23 @@ import "../code/tools.js" as Tools
 Item {
     id: item
 
-    height: itemHeight
+    height: isDivider ? dividerHeight : itemHeight
     width: ListView.view.width
+
+    enabled: !isDivider
 
     signal actionTriggered(string actionId, variant actionArgument)
     signal aboutToShowActionMenu(variant actionMenu)
 
+    property bool isDivider: (model.isDivider == true)
     property bool hasChildren: (model.hasChildren == true)
     property bool hasActionList: ((model.favoriteId != null)
         || (("hasActionList" in model) && (model.hasActionList == true)))
     property QtObject childDialog: null
     property Item menu: actionMenu
+
+    Accessible.role: isDivider ? Accessible.Separator: Accessible.MenuItem
+    Accessible.name: label.text
 
     onAboutToShowActionMenu: {
         var actionList = hasActionList ? model.actionList : [];
@@ -72,7 +78,7 @@ Item {
             verticalCenter: parent.verticalCenter
         }
 
-        height: itemHeight
+        height: parent.height
 
         property int mouseCol
         property bool pressed: false
@@ -206,7 +212,7 @@ Item {
             text: model.display
         }
 
-         PlasmaCore.SvgItem {
+        PlasmaCore.SvgItem {
             id: arrow
 
             anchors.verticalCenter: parent.verticalCenter
@@ -221,6 +227,33 @@ Item {
         }
     }
 
+    Component {
+        id: dividerComponent
+
+        PlasmaCore.SvgItem {
+            width: parent.width
+            height: lineSvg.horLineHeight
+
+            svg: lineSvg
+            elementId: "horizontal-line"
+        }
+    }
+
+    Loader {
+        id: dividerLoader
+
+        anchors.left: parent.left
+        anchors.leftMargin: highlightItemSvg.margins.left
+        anchors.right: parent.right
+        anchors.rightMargin: highlightItemSvg.margins.right
+        anchors.verticalCenter: parent.verticalCenter
+
+        active: isDivider
+
+        asynchronous: false
+        sourceComponent: dividerComponent
+    }
+
     Keys.onPressed: {
         if (event.key == Qt.Key_Menu && hasActionList) {
             event.accepted = true;
@@ -233,6 +266,4 @@ Item {
             }
         }
     }
-    Accessible.role: Accessible.MenuItem
-    Accessible.name: label.text
 }

applets/kicker/package/contents/ui/ItemListView.qml
@@ -41,6 +41,7 @@ FocusScope {
     property int itemHeight: Math.ceil((Math.max(theme.mSize(theme.defaultFont).height, units.iconSizes.small)
         + Math.max(highlightItemSvg.margins.top + highlightItemSvg.margins.bottom,
         listItemSvg.margins.top + listItemSvg.margins.bottom)) / 2) * 2
+    property int dividerHeight: lineSvg.horLineHeight + (2 * units.smallSpacing)
 
     property alias currentIndex: listView.currentIndex
     property alias keyNavigationWraps: listView.keyNavigationWraps
@@ -146,7 +147,11 @@ FocusScope {
 
                 delegate: ItemListDelegate {}
 
-                highlight: PlasmaComponents.Highlight { anchors.fill: listView.currentItem }
+                highlight: PlasmaComponents.Highlight {
+                    anchors.fill: listView.currentItem;
+
+                    visible: listView.currentItem && !listView.currentItem.isDivider
+                }
 
                 onCountChanged: {
                     currentIndex = -1;
@@ -197,6 +202,11 @@ FocusScope {
 
                         showChildDialogs = false;
                         decrementCurrentIndex();
+
+                        if (currentItem.isDivider) {
+                            decrementCurrentIndex();
+                        }
+
                         showChildDialogs = true;
                     } else if (event.key == Qt.Key_Down) {
                         event.accepted = true;
@@ -209,6 +219,11 @@ FocusScope {
 
                         showChildDialogs = false;
                         incrementCurrentIndex();
+
+                        if (currentItem.isDivider) {
+                            incrementCurrentIndex();
+                        }
+
                         showChildDialogs = true;
                     } else if (event.key == Qt.Key_Right && childDialog != null) {
                         windowSystem.forceActive(childDialog.mainItem);

applets/kicker/plugin/abstractentry.h
@@ -56,4 +56,10 @@ class AbstractGroupEntry : public AbstractEntry
         QPointer<AbstractModel> m_model;
 };
 
+class DividerEntry : public AbstractEntry
+{
+    public:
+        EntryType type() const { return DividerType; }
+};
+
 #endif

applets/kicker/plugin/abstractmodel.cpp
@@ -26,6 +26,7 @@ AbstractModel::AbstractModel(QObject *parent) : QAbstractListModel(parent)
     roles.insert(Qt::DisplayRole, "display");
     roles.insert(Qt::DecorationRole, "decoration");
     roles.insert(Kicker::IsParentRole, "isParent");
+    roles.insert(Kicker::IsDividerRole, "isDivider");
     roles.insert(Kicker::HasChildrenRole, "hasChildren");
     roles.insert(Kicker::FavoriteIdRole, "favoriteId");
     roles.insert(Kicker::HasActionListRole, "hasActionList");

applets/kicker/plugin/actionlist.h
@@ -29,6 +29,7 @@ namespace Kicker
 
 enum {
     IsParentRole = Qt::UserRole + 1,
+    IsDividerRole,
     HasChildrenRole,
     FavoriteIdRole,
     HasActionListRole,

applets/kicker/plugin/appsmodel.cpp
@@ -102,6 +102,8 @@ QVariant AppsModel::data(const QModelIndex &index, int role) const
         return entry->icon();
     } else if (role == Kicker::IsParentRole) {
         return (entry->type() == AbstractEntry::GroupType);
+    } else if (role == Kicker::IsDividerRole) {
+        return (entry->type() == AbstractEntry::DividerType);
     } else if (role == Kicker::HasChildrenRole) {
         if (entry->type() == AbstractEntry::GroupType) {
             const AbstractGroupEntry *groupEntry = static_cast<const AbstractGroupEntry *>(entry);

applets/kicker/plugin/rootmodel.cpp
@@ -207,9 +207,11 @@ void RootModel::extendEntryList()
     if (m_recentContactsModel) ++insertCount;
 
     if (insertCount) {
-        beginInsertRows(QModelIndex(), 0, insertCount - 1);
+        beginInsertRows(QModelIndex(), 0, insertCount);
 
         GroupEntry *entry = 0;
+
+        m_entryList.prepend(new DividerEntry());
 
         if (m_recentContactsModel) {
             entry = new GroupEntry(i18n("Recent Contacts"), QString(), m_recentContactsModel, this);

commit 2c135b48d5421cfbcfe3b12a96fb06679a5b59b6

Fix alignment regression for top panels from 33f33fb.
BUG:345986

applets/kicker/package/contents/ui/FullRepresentation.qml
@@ -164,7 +164,7 @@ FocusScope {
 
             anchors.top: parent.top
 
-            height: (rootModel.count * rootList.itemHeight)
+            height: ((rootModel.count - (hasDivider ? 1 : 0)) * itemHeight) + (hasDivider ? dividerHeight : 0)
 
             visible: (searchField.text == "")
 