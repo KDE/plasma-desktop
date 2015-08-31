ebd8e972a77fe701586ab638ede3184eb4eece19
Luc Menut <lmenut@free.fr>
2015-08-31 21:55:45 +0100

    Save default file manager in newer file location
    REVIEW: 120564

--- kcms/componentchooser/componentchooserfilemanager.cpp
+++ kcms/componentchooser/componentchooserfilemanager.cpp
@@ -87,7 +87,7 @@ void CfgFileManager::save(KConfig *)
     kDebug() << storageId;
     if (!storageId.isEmpty()) {
         // This is taken from filetypes/mimetypedata.cpp
-        KSharedConfig::Ptr profile = KSharedConfig::openConfig("mimeapps.list", KConfig::NoGlobals, QStandardPaths::ApplicationsLocation);
+        KSharedConfig::Ptr profile = KSharedConfig::openConfig("mimeapps.list", KConfig::NoGlobals, QStandardPaths::GenericConfigLocation);
         if (!profile->isConfigWritable(true)) // warn user if mimeapps.list is root-owned (#155126/#94504)
             return;
         KConfigGroup addedApps(profile, "Added Associations");
