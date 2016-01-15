919262448b1644c9c86bb40fefee3f6968656818
Eike Hein <hein@kde.org>
2016-01-15 14:10:01 +0100

    Auto-add Input Method Panel widget to Default Panel depending on locale's language.
    REVIEW:126752

--- layout-templates/org.kde.plasma.desktop.defaultPanel/contents/layout.js
+++ layout-templates/org.kde.plasma.desktop.defaultPanel/contents/layout.js
@@ -34,6 +34,50 @@ kickoff.writeConfig("global", "Alt+F1")
 //panel.addWidget("org.kde.plasma.showActivityManager")
 panel.addWidget("org.kde.plasma.pager")
 panel.addWidget("org.kde.plasma.taskmanager")
+
+/* Next up is determining whether to add the Input Method Panel
+ * widget to the panel or not. This is done based on whether
+ * the system locale's language id is a member of the following
+ * white list of languages which are known to pull in one of
+ * our supported IME backends when chosen during installation
+ * of common distributions. */
+
+var langIds = ["as",    // Assamese
+               "bn",    // Bengali
+               "bo",    // Tibetan
+               "brx",   // Bodo
+               "doi",   // Dogri
+               "gu",    // Gujarati
+               "hi",    // Hindi
+               "ja",    // Japanese
+               "kn",    // Kannada
+               "ko",    // Korean
+               "kok",   // Konkani
+               "ks",    // Kashmiri
+               "lep",   // Lepcha
+               "mai",   // Maithili
+               "ml",    // Malayalam
+               "mni",   // Manipuri
+               "mr",    // Marathi
+               "ne",    // Nepali
+               "or",    // Odia
+               "pa",    // Punjabi
+               "sa",    // Sanskrit
+               "sat",   // Santali
+               "sd",    // Sindhi
+               "si",    // Sinhala
+               "ta",    // Tamil
+               "te",    // Telugu
+               "th",    // Thai
+               "ur",    // Urdu
+               "vi",    // Vietnamese
+               "zh_CN", // Simplified Chinese
+               "zh_TW"] // Traditional Chinese
+
+if (langIds.indexOf(languageId) != -1) {
+    panel.addWidget("org.kde.plasma.kimpanel");
+}
+
 panel.addWidget("org.kde.plasma.systemtray")
 panel.addWidget("org.kde.plasma.digitalclock")
 
