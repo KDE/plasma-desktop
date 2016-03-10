271719d4b38baac70bf46e22f3fa57effe1cccf9
Ivan Čukić <ivan.cukic@kde.org>
2016-03-10 17:35:32 +0100

    Removed support for old Qt (pre 5.4) from activities kcm

--- kcms/activities/imports/dialog.cpp
+++ kcms/activities/imports/dialog.cpp
@@ -76,12 +76,7 @@ public:
 
         view->setResizeMode(QQuickWidget::SizeRootObjectToView);
 
-// TODO: Remove this once we start requiring Qt 5.4
-#if QT_VERSION < QT_VERSION_CHECK(5, 4, 0)
-        #warning "The activity configuration dialogue will not fully follow the system colours. Update to Qt 5.5."
-#else
         view->setClearColor(QGuiApplication::palette().window().color());
-#endif
 
         view->rootContext()->setContextProperty("dialog", q);
 
