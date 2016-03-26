4f52285b8ba71f0130ae42ce01e72783996372e9
Alexander Potashev <aspotashev@gmail.com>
2016-03-26 15:12:09 +0300

    Make kcm_activities translatable
    Summary: This is part of a patch by Victor Ryzhykh <victorr2007@yandex.ru>
    
    Reviewers: ivan
    
    Reviewed By: ivan
    
    Subscribers: plasma-devel
    
    Projects: #plasma
    
    Differential Revision: https://phabricator.kde.org/D1224

---
+++ kcms/activities/Messages.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
+$XGETTEXT `find . -name \*.qml -o -name \*.h -o -name \*.js -o -name \*.cpp` -o $podir/kcm_activities.pot
+rm -f rc.cpp
