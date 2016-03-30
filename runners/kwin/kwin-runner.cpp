ffe16922c0bf8e060669b4359be7b645ff38ea95
Martin Gräßlin <mgraesslin@kde.org>
2016-03-30 11:36:45 +0200

    [runners] Add a KWin runner to open KWin's debug console
    Summary:
    Based on the plasma-desktop runner. It launches KWin's new debug
    console if one uses the keyword "KWin".
    
    Reviewers: #plasma
    
    Subscribers: plasma-devel
    
    Projects: #plasma
    
    Differential Revision: https://phabricator.kde.org/D1262

---
+++ runners/kwin/kwin-runner.cpp
@@ -0,0 +1,99 @@
+/*
+ *   Copyright (C) 2009 Aaron Seigo <aseigo@kde.org>
+ *   Copyright (C) 2016 Martin Gräßlin <mgraesslin@kde.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License version 2 as
+ *   published by the Free Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "kwin-runner.h"
+
+#include <QDBusConnection>
+#include <QDBusServiceWatcher>
+#include <QDBusConnectionInterface>
+
+#include <QIcon>
+
+#include <KLocalizedString>
+
+K_EXPORT_PLASMA_RUNNER(kwin, KWinRunner)
+
+static const QString s_kwinService = QStringLiteral("org.kde.KWin");
+static const QString s_keyword = QStringLiteral("KWin");
+
+KWinRunner::KWinRunner(QObject *parent, const QVariantList &args)
+    : Plasma::AbstractRunner(parent, args),
+      m_enabled(false)
+{
+    setObjectName(s_keyword);
+    setIgnoredTypes(Plasma::RunnerContext::FileSystem |
+                    Plasma::RunnerContext::NetworkLocation |
+                    Plasma::RunnerContext::Help);
+    QDBusServiceWatcher *watcher = new QDBusServiceWatcher(s_kwinService, QDBusConnection::sessionBus(),
+                                                           QDBusServiceWatcher::WatchForOwnerChange, this);
+    connect(watcher, &QDBusServiceWatcher::serviceOwnerChanged, this, &KWinRunner::checkAvailability);
+    checkAvailability(QString(), QString(), QString());
+}
+
+KWinRunner::~KWinRunner()
+{
+}
+
+void KWinRunner::match(Plasma::RunnerContext &context)
+{
+    if (m_enabled && context.query().compare(s_keyword, Qt::CaseInsensitive) == 0) {
+        Plasma::QueryMatch match(this);
+        match.setId("kwin");
+        match.setType(Plasma::QueryMatch::ExactMatch);
+        match.setIcon(QIcon::fromTheme("kwin"));
+        match.setText(i18n("Open KWin debug console"));
+        match.setRelevance(1.0);
+        context.addMatch(match);
+    }
+}
+
+void KWinRunner::run(const Plasma::RunnerContext &context, const Plasma::QueryMatch &match)
+{
+    Q_UNUSED(match)
+
+    if (m_enabled && context.query().compare(s_keyword, Qt::CaseInsensitive) == 0) {
+        QDBusMessage message = QDBusMessage::createMethodCall(s_kwinService, QStringLiteral("/KWin"), s_kwinService, QStringLiteral("showDebugConsole"));
+        QDBusConnection::sessionBus().asyncCall(message);
+    }
+}
+
+void KWinRunner::checkAvailability(const QString &name, const QString &oldOwner, const QString &newOwner)
+{
+    Q_UNUSED(oldOwner)
+
+    bool enabled = false;
+    if (name.isEmpty()) {
+        enabled = QDBusConnection::sessionBus().interface()->isServiceRegistered(s_kwinService).value();
+    } else {
+        enabled = !newOwner.isEmpty();
+    }
+
+    if (m_enabled != enabled) {
+        m_enabled = enabled;
+
+        if (m_enabled) {
+            addSyntax(Plasma::RunnerSyntax(s_keyword,
+                                           i18n("Opens the KWin (Plasma Window Manager) debug console.")));
+        } else {
+            setSyntaxes(QList<Plasma::RunnerSyntax>());
+        }
+    }
+}
+
+#include "kwin-runner.moc"
---
+++ runners/kwin/kwin-runner.h
@@ -0,0 +1,43 @@
+/*
+ *   Copyright (C) 2009 Aaron Seigo <aseigo@kde.org>
+ *   Copyright (C) 2016 Martin Gräßlin <mgraesslin@kde.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License version 2 as
+ *   published by the Free Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef KWINRUNNER_H
+#define KWINRUNNER_H
+
+#include <krunner/abstractrunner.h>
+
+class KWinRunner : public Plasma::AbstractRunner
+{
+    Q_OBJECT
+
+    public:
+        explicit KWinRunner(QObject *parent, const QVariantList &args);
+        ~KWinRunner();
+
+        void match(Plasma::RunnerContext &context);
+        void run(const Plasma::RunnerContext &context, const Plasma::QueryMatch &action);
+
+    private:
+      void checkAvailability(const QString &name, const QString &oldOwner, const QString &newOwner);
+
+    private:
+        bool m_enabled;
+};
+
+#endif
