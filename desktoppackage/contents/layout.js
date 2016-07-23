88e31e49967e56319f0737d3d6d2ab46d2073f51
Marco Martin <notmart@gmail.com>
2016-07-23 19:40:58 +0200

    loop trough screens only once
    Summary:
    we are already looping trough screens, creating an activity
    per screen is wrong
    
    Reviewers: ivan, davidedmundson, #plasma
    
    Reviewed By: ivan, davidedmundson, #plasma
    
    Subscribers: davidedmundson, plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D2275

--- desktoppackage/contents/layout.js
+++ desktoppackage/contents/layout.js
@@ -1,12 +1,11 @@
 
 loadTemplate("org.kde.plasma.desktop.defaultPanel")
 
-for (var i = 0; i < screenCount; ++i) {
-    var id = createActivity("Desktop");
-    var desktopsArray = desktopsForActivity(id);
-    print(desktopsArray.length);
-    for( var j = 0; j < desktopsArray.length; j++) {
-        desktopsArray[j].wallpaperPlugin = 'org.kde.image';
-        //var clock = desktopsArray[j].addWidget("org.kde.plasma.analogclock");
-    }
+var id = createActivity("Desktop");
+var desktopsArray = desktopsForActivity(id);
+print(desktopsArray.length);
+for( var j = 0; j < desktopsArray.length; j++) {
+    desktopsArray[j].wallpaperPlugin = 'org.kde.image';
+    //var clock = desktopsArray[j].addWidget("org.kde.plasma.analogclock");
 }
+
