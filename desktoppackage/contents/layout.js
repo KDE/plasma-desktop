3b123057a01bff0ae296db7214ee98eb79c22c6f
Marco Martin <notmart@gmail.com>
2016-07-29 13:29:59 +0200

    use desktopsForActivty(currentActivity());
    more future proof

--- desktoppackage/contents/layout.js
+++ desktoppackage/contents/layout.js
@@ -1,7 +1,7 @@
 
 loadTemplate("org.kde.plasma.desktop.defaultPanel")
 
-var desktopsArray = desktops();
+var desktopsArray = desktopsForActivty(currentActivity());
 for( var j = 0; j < desktopsArray.length; j++) {
     desktopsArray[j].wallpaperPlugin = 'org.kde.image';
     //var clock = desktopsArray[j].addWidget("org.kde.plasma.analogclock");
