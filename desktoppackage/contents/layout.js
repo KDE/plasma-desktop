89cb478eb205e5586751311594f2dcf4ec447199
Marco Martin <notmart@gmail.com>
2016-07-29 12:39:53 +0200

    don't create activities by ourselves

--- desktoppackage/contents/layout.js
+++ desktoppackage/contents/layout.js
@@ -1,9 +1,7 @@
 
 loadTemplate("org.kde.plasma.desktop.defaultPanel")
 
-var id = createActivity("Desktop");
-var desktopsArray = desktopsForActivity(id);
-print(desktopsArray.length);
+var desktopsArray = desktops();
 for( var j = 0; j < desktopsArray.length; j++) {
     desktopsArray[j].wallpaperPlugin = 'org.kde.image';
     //var clock = desktopsArray[j].addWidget("org.kde.plasma.analogclock");
