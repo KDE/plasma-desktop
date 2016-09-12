d449b068f887e32cac9ab6b50e3be16e5d6f927b
David Edmundson <kde@davidedmundson.co.uk>
2016-09-12 09:44:06 +0100

    Create empty panel layout template
    Summary:
    Previously we could create an empty panel by creating just a panel
    object. After fa128c722d50 we do not list the panel containment.
    
    This creates a visible layout-template which creates an empty panel.
    
    Reviewers: #plasma
    
    Subscribers: plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D2746

---
+++ layout-templates/org.kde.plasma.desktop.emptyPanel/contents/layout.js
@@ -0,0 +1,28 @@
+var panel = new Panel
+var panelScreen = panel.screen
+var freeEdges = {"bottom": true, "top": true, "left": true, "right": true}
+
+for (i = 0; i < panelIds.length; ++i) {
+    var tmpPanel = panelById(panelIds[i])
+    if (tmpPanel.screen == panelScreen) {
+        // Ignore the new panel
+        if (tmpPanel.id != panel.id) {
+            freeEdges[tmpPanel.location] = false;
+        }
+    }
+}
+
+if (freeEdges["bottom"] == true) {
+    panel.location = "bottom";
+} else if (freeEdges["top"] == true) {
+    panel.location = "top";
+} else if (freeEdges["left"] == true) {
+    panel.location = "left";
+} else if (freeEdges["right"] == true) {
+    panel.location = "right";
+} else {
+    // There is no free edge, so leave the default value
+    panel.location = "top";
+}
+
+panel.height = gridUnit * 2
