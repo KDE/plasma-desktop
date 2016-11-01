KDE/plasma-desktop

commit 90d9a5198a9aa2405ce76d3d8aa12f817000162e

Don't instanciate twice.

applets/kicker/package/contents/ui/DashboardRepresentation.qml
@@ -98,7 +98,7 @@ Kicker.DashboardWindow {
     }
 
     function updateWidgetExplorer() {
-        if (tabBar.activeTab == 1 /* Widgets */ || tabBar.hoveredTab == 1) {
+        if (!root.widgetExplorer && (tabBar.activeTab == 1 /* Widgets */ || tabBar.hoveredTab == 1)) {
             root.widgetExplorer = widgetExplorerComponent.createObject(root);
             root.widgetExplorer.containment = containmentInterface.screenContainment(plasmoid);
         } else if (root.widgetExplorer) {

commit 8181f5ef6cc7cee2f771bf7fc84e2ff3914be432

Remove SortFilterModel, removing the special filters can be done with a prop now.
Also fix a logic bug introduced in 90d9a519.

applets/kicker/package/contents/ui/DashboardRepresentation.qml
@@ -94,13 +94,15 @@ Kicker.DashboardWindow {
         mainGrid.model = (tabBar.activeTab == 0) ? funnelModel : root.widgetExplorer.widgetsModel;
         mainGrid.currentIndex = -1;
         filterListScrollArea.focus = true;
-        filterList.model = (tabBar.activeTab == 0) ? rootModel : widgetsFilterModel;
+        filterList.model = (tabBar.activeTab == 0) ? rootModel : root.widgetExplorer.filterModel;
     }
 
     function updateWidgetExplorer() {
-        if (!root.widgetExplorer && (tabBar.activeTab == 1 /* Widgets */ || tabBar.hoveredTab == 1)) {
-            root.widgetExplorer = widgetExplorerComponent.createObject(root);
-            root.widgetExplorer.containment = containmentInterface.screenContainment(plasmoid);
+        if (tabBar.activeTab == 1 /* Widgets */ || tabBar.hoveredTab == 1) {
+            if (!root.widgetExplorer) {
+                root.widgetExplorer = widgetExplorerComponent.createObject(root);
+                root.widgetExplorer.containment = containmentInterface.screenContainment(plasmoid);
+            }
         } else if (root.widgetExplorer) {
             root.widgetExplorer.destroy();
             root.widgetExplorer = null;
@@ -147,21 +149,7 @@ Kicker.DashboardWindow {
         Component {
             id: widgetExplorerComponent
 
-            WidgetExplorer { }
-        }
-
-        PlasmaCore.SortFilterModel {
-            id: widgetsFilterModel
-
-            sourceModel: root.widgetExplorer ? root.widgetExplorer.filterModel : null
-            filterCallback: function(row, value) {
-                // Filter out "Running", "Uninstallable" and "Categories:".
-                if (row > 0 && row < 4) {
-                    return false;
-                }
-
-                return true;
-            }
+            WidgetExplorer { showSpecialFilters: false }
         }
 
         Connections {