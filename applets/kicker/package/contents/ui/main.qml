KDE/plasma-desktop

commit 3e88ac63f25d526f8f7d4d58266fd303b722a1d0

Kicker backend changes for Simple Menu.
Summary:
This does three changes needed to support the Simple Menu UI:

* Support for pagination with configurable page size and related
  API naming changes.

* Make showing the Power / Session top-level category optional.

* Emit a signal on wheel move events in WheelInterceptor.

As well as adding inheritance from QQmlParserStatus to RootModel
to avoid startup churn.

Reviewers: #plasma, mart

Subscribers: plasma-devel

Tags: #plasma

Differential Revision: https://phabricator.kde.org/D3655

applets/kicker/package/contents/ui/main.qml
@@ -82,7 +82,7 @@ Item {
         showSeparators: !isDash
         appletInterface: plasmoid
 
-        showAllSubtree: isDash
+        showAllApps: isDash
         showRecentApps: plasmoid.configuration.showRecentApps
         showRecentDocs: plasmoid.configuration.showRecentDocs
         showRecentContacts: plasmoid.configuration.showRecentContacts

applets/kicker/plugin/rootmodel.cpp
@@ -26,7 +26,9 @@
 
 #include <KLocalizedString>
 
-GroupEntry::GroupEntry(RootModel *parentModel, const QString &name,
+#include <QCollator>
+
+GroupEntry::GroupEntry(AppsModel *parentModel, const QString &name,
     const QString &iconName, AbstractModel *childModel)
 : AbstractGroupEntry(parentModel)
 , m_name(name)
@@ -61,12 +63,14 @@ AbstractModel *GroupEntry::childModel() const
 }
 
 RootModel::RootModel(QObject *parent) : AppsModel(QString(), parent)
+, m_complete(false)
 , m_favorites(new FavoritesModel(this))
 , m_systemModel(nullptr)
-, m_showAllSubtree(false)
+, m_showAllApps(false)
 , m_showRecentApps(true)
 , m_showRecentDocs(true)
 , m_showRecentContacts(false)
+, m_showPowerSession(true)
 , m_recentAppsModel(0)
 , m_recentDocsModel(0)
 , m_recentContactsModel(0)
@@ -139,6 +143,23 @@ bool RootModel::trigger(int row, const QString& actionId, const QVariant& argume
     return AppsModel::trigger(row, actionId, argument);
 }
 
+
+bool RootModel::showAllApps() const
+{
+    return m_showAllApps;
+}
+
+void RootModel::setShowAllApps(bool show)
+{
+    if (m_showAllApps != show) {
+        m_showAllApps = show;
+
+        refresh();
+
+        emit showAllAppsChanged();
+    }
+}
+
 bool RootModel::showRecentApps() const
 {
     return m_showRecentApps;
@@ -152,22 +173,6 @@ void RootModel::setShowRecentApps(bool show)
         refresh();
 
         emit showRecentAppsChanged();
-    }
-}
-
-bool RootModel::showAllSubtree() const
-{
-    return m_showAllSubtree;
-}
-
-void RootModel::setShowAllSubtree(bool show)
-{
-    if (m_showAllSubtree != show) {
-        m_showAllSubtree = show;
-
-        refresh();
-
-        emit showAllSubtreeChanged();
     }
 }
 
@@ -203,6 +208,22 @@ void RootModel::setShowRecentContacts(bool show)
     }
 }
 
+bool RootModel::showPowerSession() const
+{
+    return m_showPowerSession;
+}
+
+void RootModel::setShowPowerSession(bool show)
+{
+    if (show != m_showPowerSession) {
+        m_showPowerSession = show;
+
+        refresh();
+
+        emit showPowerSessionChanged();
+    }
+}
+
 AbstractModel* RootModel::favoritesModel()
 {
     return m_favorites;
@@ -217,9 +238,20 @@ AbstractModel* RootModel::systemFavoritesModel()
     return nullptr;
 }
 
+void RootModel::classBegin()
+{
+}
+
+void RootModel::componentComplete()
+{
+    m_complete = true;
+
+    refresh();
+}
+
 void RootModel::refresh()
 {
-    if (!m_appletInterface) {
+    if (!m_complete) {
         return;
     }
 
@@ -232,32 +264,96 @@ void RootModel::refresh()
     m_recentDocsModel = nullptr;
     m_recentContactsModel = nullptr;
 
-    if (m_showAllSubtree) {
-        QHash<QString, QList<AbstractEntry *>> m_categoryHash;
-
-        foreach (const AbstractEntry *groupEntry, m_entryList) {
-            AbstractModel *model = groupEntry->childModel();
-
-            for (int i = 0; i < model->count(); ++i) {
-                AbstractEntry *appEntry = static_cast<AbstractEntry *>(model->index(i, 0).internalPointer());
-
-                if (appEntry->name().isEmpty()) {
-                    continue;
-                }
-
-                const QChar &first = appEntry->name().at(0).toUpper();
-                m_categoryHash[first.isDigit() ? QStringLiteral("0-9") : first].append(appEntry);
-            }
-        }
-
+    if (m_showAllApps) {
         QList<AbstractEntry *> groups;
-        QHashIterator<QString, QList<AbstractEntry *>> i(m_categoryHash);
 
-        while (i.hasNext()) {
-            i.next();
-            AppsModel *model = new AppsModel(i.value(), false, this);
-            model->setDescription(i.key());
-            groups.append(new GroupEntry(this, i.key(), QString(), model));
+        if (m_paginate) {
+            QHash<QString, AppEntry *> appsHash;
+            QList<AppEntry *> apps;
+
+            foreach (const AbstractEntry *groupEntry, m_entryList) {
+                AbstractModel *model = groupEntry->childModel();
+
+                for (int i = 0; i < model->count(); ++i) {
+                    GroupEntry *subGroupEntry = static_cast<GroupEntry*>(model->index(i, 0).internalPointer());
+                    AbstractModel *subModel = subGroupEntry->childModel();
+
+                    for (int j = 0; j < subModel->count(); ++j) {
+                        AppEntry *appEntry = static_cast<AppEntry*>(subModel->index(i, 0).internalPointer());
+
+                        if (appEntry->name().isEmpty()) {
+                            continue;
+                        }
+
+                        appsHash.insert(appEntry->service()->menuId(), appEntry);
+                    }
+                }
+            }
+
+            apps = appsHash.values();
+
+            QCollator c;
+
+            std::sort(apps.begin(), apps.end(),
+                [&c](AbstractEntry* a, AbstractEntry* b) {
+                    if (a->type() != b->type()) {
+                        return a->type() > b->type();
+                    } else {
+                        return c.compare(a->name(), b->name()) < 0;
+                    }
+                });
+
+
+            int at = 0;
+            QList<AbstractEntry *> page;
+            page.reserve(24);
+
+            foreach(AppEntry *app, apps) {
+                page.append(app);
+
+                if (at == (m_pageSize - 1)) {
+                    at = 0;
+                    AppsModel *model = new AppsModel(page, false, this);
+                    groups.append(new GroupEntry(this, QString(), QString(), model));
+                    page.clear();
+                } else {
+                    ++at;
+                }
+            }
+
+            if (!page.isEmpty()) {
+                AppsModel *model = new AppsModel(page, false, this);
+                groups.append(new GroupEntry(this, QString(), QString(), model));
+            }
+
+            groups.prepend(new GroupEntry(this, QString(), QString(), m_favorites));
+        } else {
+            QHash<QString, QList<AbstractEntry *>> m_categoryHash;
+
+            foreach (const AbstractEntry *groupEntry, m_entryList) {
+                AbstractModel *model = groupEntry->childModel();
+
+                for (int i = 0; i < model->count(); ++i) {
+                    AbstractEntry *appEntry = static_cast<AbstractEntry *>(model->index(i, 0).internalPointer());
+
+                    if (appEntry->name().isEmpty()) {
+                        continue;
+                    }
+
+                    const QChar &first = appEntry->name().at(0).toUpper();
+                    m_categoryHash[first.isDigit() ? QStringLiteral("0-9") : first].append(appEntry);
+                }
+            }
+
+            QList<AbstractEntry *> groups;
+            QHashIterator<QString, QList<AbstractEntry *>> i(m_categoryHash);
+
+            while (i.hasNext()) {
+                i.next();
+                AppsModel *model = new AppsModel(i.value(), false, this);
+                model->setDescription(i.key());
+                groups.append(new GroupEntry(this, i.key(), QString(), model));
+            }
         }
 
         allModel = new AppsModel(groups, true, this);
@@ -295,7 +391,10 @@ void RootModel::refresh()
     }
 
     m_systemModel = new SystemModel(this);
-    m_entryList << new GroupEntry(this, i18n("Power / Session"), QString(), m_systemModel);
+
+    if (m_showPowerSession) {
+        m_entryList << new GroupEntry(this, i18n("Power / Session"), QString(), m_systemModel);
+    }
 
     endResetModel();
 
@@ -306,9 +405,4 @@ void RootModel::refresh()
     emit separatorCountChanged();
 
     emit refreshed();
-}
-
-void RootModel::extendEntryList()
-{
-
 }

applets/kicker/plugin/rootmodel.h
@@ -22,6 +22,8 @@
 
 #include "appsmodel.h"
 
+#include <QQmlParserStatus>
+
 class FavoritesModel;
 class RecentContactsModel;
 class RecentUsageModel;
@@ -32,7 +34,7 @@ class RootModel;
 class GroupEntry : public AbstractGroupEntry
 {
     public:
-        GroupEntry(RootModel *parentModel, const QString &name,
+        GroupEntry(AppsModel *parentModel, const QString &name,
             const QString &iconName, AbstractModel *childModel);
 
         QIcon icon() const;
@@ -47,16 +49,17 @@ class GroupEntry : public AbstractGroupEntry
         QPointer<AbstractModel> m_childModel;
 };
 
-class RootModel : public AppsModel
+class RootModel : public AppsModel, public QQmlParserStatus
 {
     Q_OBJECT
+    Q_INTERFACES(QQmlParserStatus)
 
     Q_PROPERTY(QObject* systemFavoritesModel READ systemFavoritesModel NOTIFY systemFavoritesModelChanged)
-
-    Q_PROPERTY(bool showAllSubtree READ showAllSubtree WRITE setShowAllSubtree NOTIFY showAllSubtreeChanged)
+    Q_PROPERTY(bool showAllApps READ showAllApps WRITE setShowAllApps NOTIFY showAllAppsChanged)
     Q_PROPERTY(bool showRecentApps READ showRecentApps WRITE setShowRecentApps NOTIFY showRecentAppsChanged)
     Q_PROPERTY(bool showRecentDocs READ showRecentDocs WRITE setShowRecentDocs NOTIFY showRecentDocsChanged)
     Q_PROPERTY(bool showRecentContacts READ showRecentContacts WRITE setShowRecentContacts NOTIFY showRecentContactsChanged)
+    Q_PROPERTY(bool showPowerSession READ showPowerSession WRITE setShowPowerSession NOTIFY showPowerSessionChanged)
 
     public:
         explicit RootModel(QObject *parent = 0);
@@ -66,8 +69,8 @@ class RootModel : public AppsModel
 
         Q_INVOKABLE virtual bool trigger(int row, const QString &actionId, const QVariant &argument);
 
-        bool showAllSubtree() const;
-        void setShowAllSubtree(bool show);
+        bool showAllApps() const;
+        void setShowAllApps(bool show);
 
         bool showRecentApps() const;
         void setShowRecentApps(bool show);
@@ -78,31 +81,39 @@ class RootModel : public AppsModel
         bool showRecentContacts() const;
         void setShowRecentContacts(bool show);
 
+        bool showPowerSession() const;
+        void setShowPowerSession(bool show);
+
         AbstractModel* favoritesModel();
         AbstractModel* systemFavoritesModel();
+
+        void classBegin() override;
+        void componentComplete() override;
 
     Q_SIGNALS:
         void refreshed() const;
         void systemFavoritesModelChanged() const;
-        void showAllSubtreeChanged() const;
+        void showAllAppsChanged() const;
         void showRecentAppsChanged() const;
         void showRecentDocsChanged() const;
         void showRecentContactsChanged() const;
+        void showPowerSessionChanged() const;
         void recentAppsModelChanged() const;
 
     protected Q_SLOTS:
         void refresh();
 
     private:
-        void extendEntryList();
+        bool m_complete;
 
         FavoritesModel *m_favorites;
         SystemModel *m_systemModel;
 
-        bool m_showAllSubtree;
+        bool m_showAllApps;
         bool m_showRecentApps;
         bool m_showRecentDocs;
         bool m_showRecentContacts;
+        bool m_showPowerSession;
 
         RecentUsageModel *m_recentAppsModel;
         RecentUsageModel *m_recentDocsModel;

applets/kicker/plugin/appentry.cpp
@@ -296,10 +296,11 @@ KService::Ptr AppEntry::defaultAppByName(const QString& name)
 }
 
 AppGroupEntry::AppGroupEntry(AppsModel *parentModel, KServiceGroup::Ptr group,
-    bool flat, bool sorted, bool separators, int appNameFormat) : AbstractGroupEntry(parentModel),
+    bool paginate, int pageSize, bool flat, bool sorted, bool separators, int appNameFormat) : AbstractGroupEntry(parentModel),
     m_group(group)
 {
-    AppsModel* model = new AppsModel(group->entryPath(), flat, sorted, separators, parentModel);
+    AppsModel* model = new AppsModel(group->entryPath(), paginate, pageSize, flat,
+        sorted, separators, parentModel);
     model->setAppNameFormat(appNameFormat);
     m_childModel = model;
 

applets/kicker/plugin/appentry.h
@@ -76,7 +76,7 @@ class AppGroupEntry : public AbstractGroupEntry
 {
     public:
         AppGroupEntry(AppsModel *parentModel, KServiceGroup::Ptr group,
-            bool flat, bool sorted, bool separators, int appNameFormat);
+            bool paginate, int pageSize, bool flat, bool sorted, bool separators, int appNameFormat);
 
         QIcon icon() const;
         QString name() const;

applets/kicker/plugin/appsmodel.cpp
@@ -20,6 +20,7 @@
 
 #include "appsmodel.h"
 #include "actionlist.h"
+#include "rootmodel.h"
 
 #include <QCollator>
 #include <QDebug>
@@ -29,8 +30,11 @@
 #include <KLocalizedString>
 #include <KSycoca>
 
-AppsModel::AppsModel(const QString &entryPath, bool flat, bool sorted, bool separators, QObject *parent)
+AppsModel::AppsModel(const QString &entryPath, bool paginate, int pageSize, bool flat,
+    bool sorted, bool separators, QObject *parent)
 : AbstractModel(parent)
+, m_paginate(paginate)
+, m_pageSize(pageSize)
 , m_deleteEntriesOnDestruction(true)
 , m_separatorCount(0)
 , m_showSeparators(separators)
@@ -50,6 +54,8 @@ AppsModel::AppsModel(const QString &entryPath, bool flat, bool sorted, bool sepa
 
 AppsModel::AppsModel(const QList<AbstractEntry *> entryList, bool deleteEntriesOnDestruction, QObject *parent)
 : AbstractModel(parent)
+, m_paginate(false)
+, m_pageSize(24)
 , m_deleteEntriesOnDestruction(deleteEntriesOnDestruction)
 , m_separatorCount(0)
 , m_showSeparators(false)
@@ -277,6 +283,38 @@ int AppsModel::separatorCount() const
     return m_separatorCount;
 }
 
+bool AppsModel::paginate() const
+{
+    return m_paginate;
+}
+
+void AppsModel::setPaginate(bool paginate)
+{
+    if (m_paginate != paginate) {
+        m_paginate = paginate;
+
+        refresh();
+
+        emit paginateChanged();
+    }
+}
+
+int AppsModel::pageSize() const
+{
+    return m_pageSize;
+}
+
+void AppsModel::setPageSize(int size)
+{
+    if (m_pageSize != size) {
+        m_pageSize = size;
+
+        refresh();
+
+        emit pageSizeChanged();
+    }
+}
+
 bool AppsModel::flat() const
 {
     return m_flat;
@@ -418,7 +456,8 @@ void AppsModel::refreshInternal()
                 KServiceGroup::Ptr subGroup(static_cast<KServiceGroup*>(p.data()));
 
                 if (!subGroup->noDisplay() && subGroup->childCount() > 0) {
-                    AppGroupEntry *groupEntry = new AppGroupEntry(this, subGroup, m_flat, m_sorted, m_showSeparators, m_appNameFormat);
+                    AppGroupEntry *groupEntry = new AppGroupEntry(this, subGroup, m_paginate, m_pageSize, m_flat,
+                        m_sorted, m_showSeparators, m_appNameFormat);
                     m_entryList << groupEntry;
                 }
             }
@@ -443,6 +482,33 @@ void AppsModel::refreshInternal()
 
         if (m_sorted) {
             sortEntries();
+        }
+
+        if (m_paginate) {
+            QList<AbstractEntry *> groups;
+
+            int at = 0;
+            QList<AbstractEntry *> page;
+
+            foreach(AbstractEntry *app, m_entryList) {
+                page.append(app);
+
+                if (at == (m_pageSize - 1)) {
+                    at = 0;
+                    AppsModel *model = new AppsModel(page, true, this);
+                    groups.append(new GroupEntry(this, QString(), QString(), model));
+                    page.clear();
+                } else {
+                    ++at;
+                }
+            }
+
+            if (page.count()) {
+                AppsModel *model = new AppsModel(page, true, this);
+                groups.append(new GroupEntry(this, QString(), QString(), model));
+            }
+
+            m_entryList = groups;
         }
     }
 }
@@ -531,7 +597,8 @@ void AppsModel::processServiceGroup(KServiceGroup::Ptr group)
                 const KServiceGroup::Ptr serviceGroup(static_cast<KServiceGroup*>(p.data()));
                 processServiceGroup(serviceGroup);
             } else {
-                AppGroupEntry *groupEntry = new AppGroupEntry(this, subGroup, m_flat, m_sorted, m_showSeparators, m_appNameFormat);
+                AppGroupEntry *groupEntry = new AppGroupEntry(this, subGroup, m_paginate, m_pageSize, m_flat,
+                    m_sorted, m_showSeparators, m_appNameFormat);
                 m_entryList << groupEntry;
             }
         }

applets/kicker/plugin/appsmodel.h
@@ -34,6 +34,8 @@ class AppsModel : public AbstractModel
 {
     Q_OBJECT
 
+    Q_PROPERTY(bool paginate READ paginate WRITE setPaginate NOTIFY paginateChanged)
+    Q_PROPERTY(int pageSize READ pageSize WRITE setPageSize NOTIFY pageSizeChanged)
     Q_PROPERTY(bool flat READ flat WRITE setFlat NOTIFY flatChanged)
     Q_PROPERTY(bool sorted READ sorted WRITE setSorted NOTIFY sortedChanged)
     Q_PROPERTY(bool showSeparators READ showSeparators WRITE setShowSeparators NOTIFY showSeparatorsChanged)
@@ -41,7 +43,8 @@ class AppsModel : public AbstractModel
     Q_PROPERTY(QObject* appletInterface READ appletInterface WRITE setAppletInterface NOTIFY appletInterfaceChanged);
 
     public:
-        explicit AppsModel(const QString &entryPath = QString(), bool flat = false, bool sorted = true, bool separators = true, QObject *parent = 0);
+        explicit AppsModel(const QString &entryPath = QString(), bool paginate = false, int pageSize = 24,
+            bool flat = false, bool sorted = true, bool separators = true, QObject *parent = 0);
         explicit AppsModel(const QList<AbstractEntry *> entryList, bool deleteEntriesOnDestruction, QObject *parent = 0);
         ~AppsModel();
 
@@ -60,6 +63,12 @@ class AppsModel : public AbstractModel
         Q_INVOKABLE int rowForModel(AbstractModel *model);
 
         int separatorCount() const;
+
+        bool paginate() const;
+        void setPaginate(bool paginate);
+
+        int pageSize() const;
+        void setPageSize(int size);
 
         bool flat() const;
         void setFlat(bool flat);
@@ -82,6 +91,8 @@ class AppsModel : public AbstractModel
 
     Q_SIGNALS:
         void cleared() const;
+        void paginateChanged() const;
+        void pageSizeChanged() const;
         void flatChanged() const;
         void sortedChanged() const;
         void showSeparatorsChanged() const;
@@ -94,6 +105,9 @@ class AppsModel : public AbstractModel
 
     protected:
         void refreshInternal();
+
+        bool m_paginate;
+        int m_pageSize;
 
         QList<AbstractEntry *> m_entryList;
         bool m_deleteEntriesOnDestruction;

applets/kicker/plugin/wheelinterceptor.cpp
@@ -48,6 +48,8 @@ void WheelInterceptor::wheelEvent(QWheelEvent* event)
     if (m_destination) {
         QCoreApplication::sendEvent(m_destination, event);
     }
+
+    emit wheelMoved(event->angleDelta());
 }
 
 QQuickItem *WheelInterceptor::findWheelArea(QQuickItem *parent) const

commit b7976f0c2628db05ee11841aa455afb426f946d7

Fix race condition causing empty favorites.
Summary:
This puts the frontend in charge of calling refresh() after the
favorites have been initially set by setting auto population at
completion to false. Also preserves original behavior by keeping
auto population default-on.

Fixes a regression from 3e88ac6.
BUG:374156

This is an alternative to D3910.

Reviewers: #plasma, broulik

Subscribers: plasma-devel

Tags: #plasma

Differential Revision: https://phabricator.kde.org/D3979

applets/kicker/package/contents/ui/main.qml
@@ -76,6 +76,8 @@ Item {
     Kicker.RootModel {
         id: rootModel
 
+        autoPopulate: false
+
         appNameFormat: plasmoid.configuration.appNameFormat
         flat: isDash ? true : plasmoid.configuration.limitDepth
         sorted: plasmoid.configuration.alphaSort
@@ -101,6 +103,7 @@ Item {
 
         Component.onCompleted: {
             favoritesModel.favorites = plasmoid.configuration.favoriteApps;
+            rootModel.refresh();
         }
     }
 

applets/kicker/plugin/rootmodel.cpp
@@ -66,6 +66,7 @@ RootModel::RootModel(QObject *parent) : AppsModel(QString(), parent)
 , m_complete(false)
 , m_favorites(new FavoritesModel(this))
 , m_systemModel(nullptr)
+, m_autoPopulate(true)
 , m_showAllApps(false)
 , m_showRecentApps(true)
 , m_showRecentDocs(true)
@@ -143,6 +144,19 @@ bool RootModel::trigger(int row, const QString& actionId, const QVariant& argume
     return AppsModel::trigger(row, actionId, argument);
 }
 
+bool RootModel::autoPopulate() const
+{
+    return m_autoPopulate;
+}
+
+void RootModel::setAutoPopulate(bool populate)
+{
+    if (m_autoPopulate != populate) {
+        m_autoPopulate = populate;
+
+        emit autoPopulateChanged();
+    }
+}
 
 bool RootModel::showAllApps() const
 {
@@ -246,7 +260,9 @@ void RootModel::componentComplete()
 {
     m_complete = true;
 
-    refresh();
+    if (m_autoPopulate) {
+        refresh();
+    }
 }
 
 void RootModel::refresh()

applets/kicker/plugin/rootmodel.h
@@ -54,6 +54,8 @@ class RootModel : public AppsModel, public QQmlParserStatus
     Q_OBJECT
     Q_INTERFACES(QQmlParserStatus)
 
+    Q_PROPERTY(bool autoPopulate READ autoPopulate WRITE setAutoPopulate NOTIFY autoPopulateChanged)
+
     Q_PROPERTY(QObject* systemFavoritesModel READ systemFavoritesModel NOTIFY systemFavoritesModelChanged)
     Q_PROPERTY(bool showAllApps READ showAllApps WRITE setShowAllApps NOTIFY showAllAppsChanged)
     Q_PROPERTY(bool showRecentApps READ showRecentApps WRITE setShowRecentApps NOTIFY showRecentAppsChanged)
@@ -68,6 +70,9 @@ class RootModel : public AppsModel, public QQmlParserStatus
         QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
 
         Q_INVOKABLE virtual bool trigger(int row, const QString &actionId, const QVariant &argument);
+
+        bool autoPopulate() const;
+        void setAutoPopulate(bool populate);
 
         bool showAllApps() const;
         void setShowAllApps(bool show);
@@ -93,6 +98,7 @@ class RootModel : public AppsModel, public QQmlParserStatus
     Q_SIGNALS:
         void refreshed() const;
         void systemFavoritesModelChanged() const;
+        void autoPopulateChanged() const;
         void showAllAppsChanged() const;
         void showRecentAppsChanged() const;
         void showRecentDocsChanged() const;
@@ -108,6 +114,8 @@ class RootModel : public AppsModel, public QQmlParserStatus
 
         FavoritesModel *m_favorites;
         SystemModel *m_systemModel;
+
+        bool m_autoPopulate;
 
         bool m_showAllApps;
         bool m_showRecentApps;