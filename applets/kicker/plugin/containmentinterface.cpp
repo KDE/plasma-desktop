7993cd433ca68a0136a990d3f1710a6360024fe9
Marco Martin <notmart@gmail.com>
2017-01-23 20:57:01 +0100

    use KPluginMetaData::readStringList
    use the correct form to read a qstringlist from kpluginmetadata

--- applets/kicker/plugin/containmentinterface.cpp
+++ applets/kicker/plugin/containmentinterface.cpp
@@ -142,7 +142,7 @@ void ContainmentInterface::addLauncher(QObject *appletInterface, ContainmentInte
                 return;
             }
 
-            const QStringList &containmentProvides = containment->pluginInfo().property(QStringLiteral("X-Plasma-Provides")).value<QStringList>();
+            const QStringList &containmentProvides = KPluginMetaData::readStringList(containment->pluginMetaData().rawData(), QStringLiteral("X-Plasma-Provides"));
 
             if (containmentProvides.contains(QLatin1String("org.kde.plasma.filemanagement"))) {
                 QQuickItem* gObj = qobject_cast<QQuickItem *>(containment->property("_plasma_graphicObject").value<QObject *>());
