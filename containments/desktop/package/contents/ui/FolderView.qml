KDE/plasma-desktop

commit 2b9e0e7cc61aa4d3811cde8db92ddb2795e4e97c

Set current index to start of selection range when performing rectangle selection.
Summary: BUG:374030

Reviewers: #plasma, broulik

Subscribers: plasma-devel

Tags: #plasma

Differential Revision: https://phabricator.kde.org/D3932

containments/desktop/package/contents/ui/FolderView.qml
@@ -483,7 +483,7 @@ Item {
                         scrollDown = false;
                     }
 
-                    // Update rubberband geomety.
+                    // Update rubberband geometry.
                     if (main.rubberBand) {
                         var rB = main.rubberBand;
 
@@ -561,8 +561,13 @@ Item {
                 }
 
                 onCachedRectangleSelectionChanged: {
-                    if (cachedRectangleSelection) {
-                        dir.updateSelection(cachedRectangleSelection, gridView.ctrlPressed);
+                    if (cachedRectangleSelection.length) {
+                        // Set current index to start of selection.
+                        // cachedRectangleSelection is pre-sorted.
+                        currentIndex = cachedRectangleSelection[0];
+
+                        dir.updateSelection(cachedRectangleSelection.map(positioner.map),
+                            gridView.ctrlPressed);
                     }
                 }
 
@@ -636,7 +641,7 @@ Item {
                                         item.iconArea.width, item.iconArea.height);
 
                                     if (main.rubberBand.intersects(iconRect)) {
-                                        indices.push(positioner.map(index));
+                                        indices.push(index);
                                         continue;
                                     }
 
@@ -644,12 +649,12 @@ Item {
                                         item.labelArea.width, item.labelArea.height);
 
                                     if (main.rubberBand.intersects(labelRect)) {
-                                        indices.push(positioner.map(index));
+                                        indices.push(index);
                                         continue;
                                     }
                                 } else {
                                     // Otherwise be content with the cell intersection.
-                                    indices.push(positioner.map(index));
+                                    indices.push(index);
                                 }
                             }
                         }

commit 3568d8e4a8a2cc4fac7fccb86132c47be37495f6

Fix clearing selection when rectangle selection contains no items.
Fixes a logic regression from 2b9e0e7c.

containments/desktop/package/contents/ui/FolderView.qml
@@ -565,10 +565,10 @@ Item {
                         // Set current index to start of selection.
                         // cachedRectangleSelection is pre-sorted.
                         currentIndex = cachedRectangleSelection[0];
-
-                        dir.updateSelection(cachedRectangleSelection.map(positioner.map),
-                            gridView.ctrlPressed);
                     }
+
+                    dir.updateSelection(cachedRectangleSelection.map(positioner.map),
+                        gridView.ctrlPressed);
                 }
 
                 function makeIconSize() {