KDE/plasma-desktop

commit 1aec30d954b71c7a5b614365e2f7b800e78117ee

Don't close popup when spring-load dragging into it.
Summary:
I'm not sure how this regression came about. It might be due to mouse
handling changes on the Qt 5.9 branch. I'd appreciate it if someone
tests spring loading with this patch and older Qts.

Reviewers: #plasma, broulik

Subscribers: plasma-devel

Tags: #plasma

Differential Revision: https://phabricator.kde.org/D5310

containments/desktop/package/contents/ui/FolderView.qml
@@ -337,7 +337,9 @@ Item {
             var leftEdge = Math.min(gridView.contentX, gridView.originX);
 
             if (!item || item.blank) {
-                gridView.hoveredItem = null;
+                if (gridView.hoveredItem && !root.containsDrag && (!dialog || !dialog.containsDrag)) {
+                    gridView.hoveredItem = null;
+                }
             } else {
                 var fPos = mapToItem(item.frame, mouse.x, mouse.y);
 

commit 7aa09d2d5f200dc9a6510219d811d7df075683e3

Fix a bunch of reference errors introduced in 1aec30d9.

containments/desktop/package/contents/ui/FolderItemDelegate.qml
@@ -438,7 +438,7 @@ Item {
                         return false;
                     }
 
-                    if (!main.GridView.view.isRootView && dialog.containsDrag) {
+                    if (!main.GridView.view.isRootView && main.GridView.view.dialog.containsDrag) {
                         return false;
                     }
 

containments/desktop/package/contents/ui/FolderView.qml
@@ -60,6 +60,7 @@ Item {
     property alias hoveredItem: gridView.hoveredItem
     property var history: []
     property Item backButton: null
+    property var dialog: null
 
     function rename()
     {
@@ -337,7 +338,7 @@ Item {
             var leftEdge = Math.min(gridView.contentX, gridView.originX);
 
             if (!item || item.blank) {
-                if (gridView.hoveredItem && !root.containsDrag && (!dialog || !dialog.containsDrag)) {
+                if (gridView.hoveredItem && !root.containsDrag && (!dialog || !dialog.containsDrag) && !gridView.hoveredItem.popupDialog) {
                     gridView.hoveredItem = null;
                 }
             } else {

containments/desktop/package/contents/ui/FolderViewDialog.qml
@@ -72,6 +72,7 @@ Folder.SubDialog {
             anchors.fill: parent
 
             isRootView: false
+            dialog: dialog
 
             locked: true
 