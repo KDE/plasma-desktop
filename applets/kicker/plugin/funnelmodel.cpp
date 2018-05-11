KDE/plasma-desktop

commit 38403268aad327f658616feeecfa78f305471e2b

FunnelModel: fix invalid model API usage.
Summary:
The new asserts added in qtbase in commit 00c09e752f (Qt 5.11) are triggered
by this invalid code, which sets up the model before the begin* methods.
So Qt already sees the new rowCount in that method, which is wrong.

Test Plan: Opening the K menu

Reviewers: hein, davidedmundson

Reviewed By: davidedmundson

Subscribers: plasma-devel

Tags: #plasma

Differential Revision: https://phabricator.kde.org/D12275

applets/kicker/plugin/funnelmodel.cpp
@@ -61,24 +61,24 @@ void FunnelModel::setSourceModel(QAbstractItemModel *model)
     int oldCount = m_sourceModel->rowCount();
     int newCount = model->rowCount();
 
-    disconnectSignals();
-
-    m_sourceModel = model;
-
-    connectSignals();
+    auto setNewModel = [this, model]() {
+        disconnectSignals();
+        m_sourceModel = model;
+        connectSignals();
+    };
 
     if (newCount > oldCount) {
         beginInsertRows(QModelIndex(), oldCount, newCount - 1);
-
+        setNewModel();
         endInsertRows();
     } else if (newCount < oldCount) {
         if (newCount == 0) {
             beginResetModel();
-
+            setNewModel();
             endResetModel();
         } else {
             beginRemoveRows(QModelIndex(), newCount, oldCount - 1);
-
+            setNewModel();
             endRemoveRows();
         }
     }

commit c0cd1f8d49e6d63f1988d4749c366a0bef177790

Fix submenus not updating when switching between categories of the same size
Summary:
This fixes a regression introduced in 38403268aad3, which forgot to
handle the branch where the counts are the same.

BUG:394013

Reviewers: #plasma

Subscribers: plasma-devel

Tags: #plasma

Differential Revision: https://phabricator.kde.org/D12774

applets/kicker/plugin/funnelmodel.cpp
@@ -81,6 +81,8 @@ void FunnelModel::setSourceModel(QAbstractItemModel *model)
             setNewModel();
             endRemoveRows();
         }
+    } else {
+        setNewModel();
     }
 
     if (newCount > 0) {