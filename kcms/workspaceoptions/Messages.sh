00ec9a991525c421aec4e480db24433bf0485c21
Roman Gilg <subdiff@gmail.com>
2018-05-18 10:21:15 +0200

    Fix Workspace KCM translation domain name
    Summary:
    TRANSLATION_DOMAIN and value in Messages.sh need to be the same.
    Use the new name kcm5_workspace for consistency.
    
    Reviewers: #plasma, ltoscano, yurchor
    
    Reviewed By: ltoscano
    
    Subscribers: plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D12956

--- kcms/workspaceoptions/Messages.sh
+++ kcms/workspaceoptions/Messages.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-$XGETTEXT `find . -name "*.cpp" -o -name "*.qml"` -o $podir/kcm_workspaceoptions.pot
+$XGETTEXT `find . -name "*.cpp" -o -name "*.qml"` -o $podir/kcm5_workspace.pot
