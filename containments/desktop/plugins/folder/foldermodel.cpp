KDE/plasma-desktop

commit 07001277a8a0893aeb54240d7f5cdd9825fb57ed

Improve detecting D&D between two screen showing the same URL
Summary:
If screen 1 & 2 was showing file://home/user/foo and file://home/user/foo/bar
was moved from one screen to another, it was not detected as a drop
between shared views and the Copy/Move/Link menu was shown.

Reviewers: #plasma, hein, mwolff

Reviewed By: #plasma, hein

Subscribers: plasma-devel

Tags: #plasma

Differential Revision: https://phabricator.kde.org/D10729

containments/desktop/plugins/folder/foldermodel.cpp
@@ -1004,7 +1004,7 @@ void FolderModel::dragSelectedInternal(int x, int y)
 static bool isDropBetweenSharedViews(const QList<QUrl> &urls, const QUrl &folderUrl)
 {
     for (const auto &url : urls) {
-        if (folderUrl != url.adjusted(QUrl::RemoveFilename)) {
+        if (folderUrl != url.adjusted(QUrl::RemoveFilename | QUrl::StripTrailingSlash)) {
             return false;
         }
     }

commit cab9078eaf21f27c034c93ac94dbbfed8c228f7c

Fix drop between shared views
Summary:
Remove trailing slash from folderUrl, too, when comparing to
the file's url (minus file name minus trailing slash). FolderUrls
like file:///home/user/Desktop/ may appear and fail comparing.

Fixes regression introduced in 07001277a8a0893aeb54240d7f5cdd9825fb57ed
(see https://phabricator.kde.org/D10729) which fixes urls like
file:///home/user/foo (if using a different folder than Desktop)
but breaks moving files from one screen to another using normal
Desktop folder.

Test Plan:
1. Use folder view, Desktop folder, more than one screen.
Create files and folders in Desktop folder and move them from one
screen to another. The Copy/Move/Link menu must not appear
and files/folders must be dropped onto the other screen.

2. Use a different folder than Desktop (the same on both screens).
Moving files/folders to another screen must work like in 1.

Reviewers: mart

Reviewed By: mart

Subscribers: amantia, plasma-devel

Tags: #plasma

Differential Revision: https://phabricator.kde.org/D13612

containments/desktop/plugins/folder/foldermodel.cpp
@@ -998,7 +998,8 @@ void FolderModel::dragSelectedInternal(int x, int y)
 static bool isDropBetweenSharedViews(const QList<QUrl> &urls, const QUrl &folderUrl)
 {
     for (const auto &url : urls) {
-        if (folderUrl != url.adjusted(QUrl::RemoveFilename | QUrl::StripTrailingSlash)) {
+        if (folderUrl.adjusted(QUrl::StripTrailingSlash)
+               != url.adjusted(QUrl::RemoveFilename | QUrl::StripTrailingSlash)) {
             return false;
         }
     }