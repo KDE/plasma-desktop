5ff5d1c3ca9c0ec10ec2681de23a3f10f28f92a4
Yuri Chornoivan <yurchor@ukr.net>
2019-12-19 09:20:58 +0200

    Sync the name of the translation catalog with the module definition (patch by Victor Ryzhykh

--- kcms/style/Messages.sh
+++ kcms/style/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC *.ui >> rc.cpp
-$XGETTEXT `find . -name "*.cpp" -o -name "*.qml"` -o $podir/kcmstyle.pot
+$XGETTEXT `find . -name "*.cpp" -o -name "*.qml"` -o $podir/kcm_style.pot
 rm -f rc.cpp
