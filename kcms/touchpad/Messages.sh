197d3076dd120b464998ad20d3bf0ae7c9c76664
Luigi Toscano <luigi.toscano@tiscali.it>
2020-04-16 10:21:42 +0200

    [kcm/touchpad] Fix i18n extraction with the new dirs
    The previous change refactored the directories of the kcm/touchpad,
    but the message extraction script needs to be adapted too.

--- kcms/touchpad/Messages.sh
+++ kcms/touchpad/Messages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-$EXTRACTRC `find src -path src/applet -prune -o \( -name \*.rc -o -name \*.ui -o -name \*.kcfg \) -print` >> rc.cpp
-$XGETTEXT rc.cpp `find src/kcm -name \*.qml` `find src -path src/applet -prune -o \( -name \*.cpp -o -name \*.h \) -print` -o $podir/kcm_touchpad.pot
+$EXTRACTRC `find . -path applet -prune -o \( -name \*.rc -o -name \*.ui -o -name \*.kcfg \) -print` >> rc.cpp
+$XGETTEXT rc.cpp `find kcm -name \*.qml` `find . -path applet -prune -o \( -name \*.cpp -o -name \*.h \) -print` -o $podir/kcm_touchpad.pot
 
-$XGETTEXT `find src/applet -name \*.qml -o -name \*.js` -o $podir/plasma_applet_touchpad.pot
+$XGETTEXT `find applet -name \*.qml -o -name \*.js` -o $podir/plasma_applet_touchpad.pot
