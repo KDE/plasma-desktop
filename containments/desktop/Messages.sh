c355ef271aded161853b5561f2bd98175aa3a38e
Yuri Chornoivan <yurchor@ukr.net>
2020-07-03 09:42:10 +0300

    Fix message extraction a bit

--- containments/desktop/Messages.sh
+++ containments/desktop/Messages.sh
@@ -1,6 +1,2 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-pofile=$podir/plasma_applet_org.kde.desktopcontainment.pot
-$XGETTEXT `find . -name \*.qml` -L Java -o $pofile
-$XGETTEXT `find . -name \*.cpp` -j -o $pofile
-rm -f rc.cpp
+$XGETTEXT `find . -name \*.cpp -o -name \*.h -o -name \*.qml` -o $podir/plasma_applet_org.kde.desktopcontainment.pot
--- kcms/desktoptheme/Messages.sh
+++ kcms/desktoptheme/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name "*.ui"` >> rc.cpp || exit 11
+$EXTRACTRC `find . -name "*.kcfg"` >> rc.cpp || exit 11
 $XGETTEXT `find . -name "*.cpp" -o -name "*.qml"` -o $podir/kcm_desktoptheme.pot
 rm -f rc.cpp
