e3f15bf05956f4e3b16ad009986fd769d8119d39
Yuri Chornoivan <yurchor@ukr.net>
2020-07-03 09:42:10 +0300

    Fix message extraction a bit

--- applets/kickoff/Messages.sh
+++ applets/kickoff/Messages.sh
@@ -1,4 +1,2 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
 $XGETTEXT `find . -name \*.qml -o -name \*.js -o -name \*.cpp` -o $podir/plasma_applet_org.kde.plasma.kickoff.pot
-rm -f rc.cpp
--- applets/kimpanel/backend/ibus/emojier/Messages.sh
+++ applets/kimpanel/backend/ibus/emojier/Messages.sh
@@ -1,4 +1,2 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name "*.ui"` >> rc.cpp || exit 11
 $XGETTEXT `find . -name "*.cpp" -o -name "*.qml"` -o $podir/org.kde.plasma.emojier.pot
-rm -f rc.cpp
