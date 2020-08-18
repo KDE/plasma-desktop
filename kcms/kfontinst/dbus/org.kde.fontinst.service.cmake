896f55c94b75757b515ba2ed5730dd2991c03471
Bhushan Shah <bhush94@gmail.com>
2020-08-18 16:14:54 +0530

    kcms: remove from plasma-desktop
    They are moved to plasma-workspace

--- kcms/kfontinst/dbus/org.kde.fontinst.service.cmake
+++
@@ -1,4 +0,0 @@
-[D-BUS Service]
-Name=org.kde.fontinst
-Exec=@KAUTH_HELPER_INSTALL_ABSOLUTE_DIR@/fontinst
-
--- kcms/lookandfeel/config-kcm.h.cmake
+++
@@ -1,3 +0,0 @@
-#cmakedefine HAVE_XFIXES 1
-#cmakedefine HAVE_XCURSOR 1
-#cmakedefine HAVE_BREEZE_DECO 1
