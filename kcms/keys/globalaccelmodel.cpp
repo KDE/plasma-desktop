f3427911b069b54ff19e3b08480603657a8f614b
David Redondo <kde@david-redondo.de>
2020-09-16 14:31:19 +0200

    Remove duplicated license header

--- kcms/keys/globalaccelmodel.cpp
+++ kcms/keys/globalaccelmodel.cpp
@@ -26,27 +26,6 @@
 #include <kglobalaccel_interface.h>
 #include <kglobalaccel_component_interface.h>
 #include <KGlobalShortcutInfo>
-
-/*
- * Copyright 2020 David Redondo <kde@david-redondo.de>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License or (at your option) version 3 or any later version
- * accepted by the membership of KDE e.V. (or its successor approved
- * by the membership of KDE e.V.), which shall act as a proxy
- * defined in Section 14 of version 3 of the license.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include <KLocalizedString>
 #include <KService>
 #include <KServiceTypeTrader>
