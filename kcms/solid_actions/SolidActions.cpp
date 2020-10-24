b21c8e0b8dd01a93679f57469ed4564ccbf682c8
Alexander Lohnau <alexander.lohnau@gmx.de>
2020-10-24 14:22:16 +0200

    Fix build

--- kcms/solid_actions/SolidActions.cpp
+++ kcms/solid_actions/SolidActions.cpp
@@ -17,6 +17,7 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA          *
  ***************************************************************************/
 
+#include "SolidActions.h"
 #include "ActionItem.h"
 
 #include <KAboutData>
