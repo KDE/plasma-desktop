64546c81d43f8b10487d9c6c5bb0fab4de457d13
Jan Blackquill <uhhadd@gmail.com>
2021-02-17 12:34:15 -0500

    kcms/keyboard: fix migration
    TriState::STATE_ON is 0, while TriState::STATE_OFF is 1.
    We're currently migrating 0 -> disabled, when it should be
    1 -> disabled, * -> enabled.
    
    BUG: 431923
    FIXED-IN: 5.21.1
    (cherry picked from commit 5550af1fd10dccda80be4586f19e3aa0995be2bc)

--- kcms/keyboard/kcminputrc_migrate_repeat_value.py
+++ kcms/keyboard/kcminputrc_migrate_repeat_value.py
@@ -4,7 +4,7 @@ import sys
 for line in sys.stdin:
     line = line.rstrip()
     print(line, file=sys.stderr)
-    if line.startswith("KeyboardRepeating=0"):
+    if line.startswith("KeyboardRepeating=1"):
         print("KeyRepeat=nothing")
     elif line.startswith("KeyboardRepeating="):
         print("KeyRepeat=repeat")
