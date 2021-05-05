c2e1f4897d34fe0f53606a807dc82c81d2fd97e2
Bharadwaj Raju <bharadwaj.raju777@protonmail.com>
2021-05-05 14:35:14 +0000

    Add update script for new klipper clear dialog config
    Previously, Klipper "clear history" dialog used to not ask again even
    if user answered No. This was changed so that only a Yes answer will
    lead to no more asking, by using `warningContinueCancel` instead of
    `questionYesNo`.
    
    Now the behaviour of previous config value `really_clear_history` has
    inverted: true/undefined => ask again; false => don't ask. This update
    script migrates old configs to use the new config value, renamed to
    `klipperClearHistoryAskAgain`.
    
    Depends on plasma-workspace!816.
    
    CCBUG: 336053

---
+++ desktoppackage/contents/updates/klipper_clear_config.js
@@ -0,0 +1,14 @@
+/*
+ * Previously, Klipper "clear history" dialog used to not ask again even if user answered No.
+ * This was changed so that only a Yes answer will lead to no more asking, by using warningContinueCancel instead of questionYesNo.
+ * Now the behaviour of previous config value really_clear_history has inverted: true/undefined => ask again; false => don't ask.
+ * This update script migrates old configs to use the new config value, renamed to klipperClearHistoryAskAgain.
+*/
+
+config = ConfigFile("plasmashellrc", "Notification Messages");
+oldVal = config.readEntry("really_clear_history");
+if (oldVal === "true") {
+    // Clear and don't ask again -- preserve this choice
+    config.writeEntry("klipperClearHistoryAskAgain", false)
+}
+config.deleteEntry("really_clear_history");
