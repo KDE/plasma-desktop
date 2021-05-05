38c55b4c264af6ad2bbb660fa8b4311cd7383a70
Bhushan Shah <bhush94@gmail.com>
2021-05-05 13:34:54 +0530

    kcms: remove the KCMS moved in plasma-workspace
    They are moved to plasma-workspace with bc113eb43805439202369d72fcb6d690768135e2

--- kcms/formats/Messages.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-$EXTRACTRC *.ui >> rc.cpp
-$XGETTEXT *.cpp -o $podir/kcmformats.pot
-rm -f rc.cpp
-
--- kcms/notifications/Messages.sh
+++
@@ -1,2 +0,0 @@
-#! /usr/bin/env bash
-$XGETTEXT `find . -name "*.cpp" -o -name "*.qml"` -o $podir/kcm_notifications.pot
