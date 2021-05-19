adfb2a6800f1338fb52fc1852b77f76e0b8d19b3
Albert Astals Cid <aacid@kde.org>
2021-05-19 22:31:30 +0200

    Fix touchpad kcm Messages.sh
    CCMAIL: nate@kde.org

--- kcms/touchpad/Messages.sh
+++ kcms/touchpad/Messages.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-$EXTRACTRC `find . -path applet -prune -o \( -name \*.rc -o -name \*.ui -o -name \*.kcfg \) -print` >> rc.cpp
-$XGETTEXT rc.cpp `find kcm -name \*.qml` `find . -path applet -prune -o \( -name \*.cpp -o -name \*.h \) -print` -o $podir/kcm_touchpad.pot
-
-$XGETTEXT `find applet -name \*.qml -o -name \*.js` -o $podir/plasma_applet_touchpad.pot
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
+$XGETTEXT rc.cpp `find kcm -name \*.qml` `find . -name \*.cpp -o -name \*.h` -o $podir/kcm_touchpad.pot
