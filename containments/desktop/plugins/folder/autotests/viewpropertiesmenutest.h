f5f4251cb43089d36f19cc875d1b67073933e88a
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-05-21 08:00:49 +0200

    Adjust copyright texts to be spec conform

--- containments/desktop/plugins/folder/autotests/viewpropertiesmenutest.h
+++ containments/desktop/plugins/folder/autotests/viewpropertiesmenutest.h
@@ -1,6 +1,5 @@
 /*
-    SPDX-FileCopyrightText: 2017 Klarälvdalens Datakonsult AB a KDAB Group company
-    <info@kdab.com>
+    SPDX-FileCopyrightText: 2017 Klarälvdalens Datakonsult AB a KDAB Group company <info@kdab.com>
     Author: Laurent Montel <laurent.montel@kdab.com>
 
     SPDX-License-Identifier: GPL-2.0-or-later
--- containments/desktop/plugins/folder/screenmapper.cpp
+++ containments/desktop/plugins/folder/screenmapper.cpp
@@ -1,7 +1,6 @@
 /*
-    SPDX-FileCopyrightText: 2017 Klarälvdalens Datakonsult AB a KDAB Group company
-    <info@kdab.com>
-    Author: Andras Mantia <andras.mantia@kdab.com>
+    SPDX-FileCopyrightText: 2017 Klarälvdalens Datakonsult AB a KDAB Group company <info@kdab.com>
+    SPDX-FileCopyrightText: Andras Mantia <andras.mantia@kdab.com>
     Work sponsored by the LiMux project of the city of Munich.
     SPDX-License-Identifier: GPL-2.0-or-later
 */
