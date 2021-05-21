37e77d81e69cded7283c048fb88864f52aa5ab4b
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-05-21 07:56:10 +0200

    Remove obsolete copyright texts

--- applets/kimpanel/backend/ibus/ibus15/gdkkeysyms_p.h
+++ applets/kimpanel/backend/ibus/ibus15/gdkkeysyms_p.h
@@ -3,14 +3,6 @@
     SPDX-FileCopyrightText: 2005, 2006, 2007, 2009 GNOME Foundation
 
     SPDX-License-Identifier: LGPL-2.0-or-later
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library. If not, see <http://www.gnu.org/licenses/>.
 */
 
 /*
