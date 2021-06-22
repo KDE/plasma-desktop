b7c8eb80eec12af27b5c2a4d37a9387bce540c0f
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-06-22 16:49:39 +0200

    Add license ref for synapticstouchpad

--- kcms/touchpad/backends/x11/synapticstouchpad.cpp
+++ kcms/touchpad/backends/x11/synapticstouchpad.cpp
@@ -1,36 +1,9 @@
 /*
     SPDX-FileCopyrightText: 2013 Alexander Mezin <mezin.alexander@gmail.com>
-    SPDX-FileContributor: Peter Osterlund (petero2@telia.com)
+    SPDX-FileContributor: 2002-2005, 2007 Peter Osterlund <petero2@telia.com>
 
-    SPDX-License-Identifier: GPL-2.0-or-later
+    SPDX-License-Identifier: GPL-2.0-or-later AND LicenseRef-synaptics
 */
-
-/*
-//krazy:excludeall=copyright
- * This file incorporates work covered by the following copyright and
- * permission notice:
- *
- * SPDX-FileCopyrightText: 2002-2005, 2007 Peter Osterlund
- *
- * Permission to use, copy, modify, distribute, and sell this software
- * and its documentation for any purpose is hereby granted without
- * fee, provided that the above copyright notice appear in all copies
- * and that both that copyright notice and this permission notice
- * appear in supporting documentation, and that the name of Red Hat
- * not be used in advertising or publicity pertaining to distribution
- * of the software without specific, written prior permission.  Red
- * Hat makes no representations about the suitability of this software
- * for any purpose.  It is provided "as is" without express or implied
- * warranty.
- *
- * THE AUTHORS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
- * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN
- * NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY SPECIAL, INDIRECT OR
- * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
- * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
- * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
- * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
 
 #include <QDebug>
 #include <cmath>
