Sonic-DE/sonic-desktop-interface

Provide a KCM for Drawing Tablets
Otherwise called Wacom tablets, that is.
This is using the same InputDevice dbus interfaces we use for the mouse
and touchpad kcms.

kcms/tablet/Messages.sh
@@ -1,0 +1,4 @@
+#! /usr/bin/env bash
+$EXTRACTRC `find -name \*.ui` >> rc.cpp || exit 11
+$XGETTEXT `find . -name "*.cpp" -o -name "*.qml"` -o $podir/kcmmouse.pot
+rm -f rc.cpp

KDE/plasma-desktop

kcms/tablet/Messages.sh
@@ -1,0 +1,4 @@
+#! /usr/bin/env bash
+$EXTRACTRC `find -name \*.ui` >> rc.cpp || exit 11
+$XGETTEXT `find . -name "*.cpp" -o -name "*.qml"` -o $podir/kcm_tablet.pot
+rm -f rc.cpp