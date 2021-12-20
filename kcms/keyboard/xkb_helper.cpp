59406f88eb76e1ba319b54efcb07102b16d84961
Fabian Vogt <fabian@ritter-vogt.de>
2021-12-20 19:46:34 +0100

    [kcms/keyboard] Fix setting options on X11
    It was missing the empty parameter for "-option ''" to trigger the reset,
    resulting in an invalid commandline for setxkbmap, e.g.
    setxkbmap -option -option compose:caps.
    
    BUG: 443609

--- kcms/keyboard/xkb_helper.cpp
+++ kcms/keyboard/xkb_helper.cpp
@@ -152,7 +152,9 @@ bool XkbHelper::initializeKeyboardLayouts(KeyboardConfig &config)
         }
     }
     if (config.resetOldXkbOptions()) {
+        // Pass -option "" to clear previously set options
         setxkbmapCommandArguments.append(QStringLiteral("-option"));
+        setxkbmapCommandArguments.append(QStringLiteral(""));
     }
     if (!config.xkbOptions().isEmpty()) {
         setxkbmapCommandArguments.append(QStringLiteral("-option"));
