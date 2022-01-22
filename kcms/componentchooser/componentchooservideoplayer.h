760b1867f7191298e0bdf26dd071a0ecb8e78901
Thiago Sueto <herzenschein@gmail.com>
2022-01-22 10:08:42 -0300

    Change copyright year to 2022

--- kcms/componentchooser/componentchooservideoplayer.h
+++ kcms/componentchooser/componentchooservideoplayer.h
@@ -1,5 +1,5 @@
 /*
-    SPDX-FileCopyrightText: 2021 Thiago Sueto <herzenschein@gmail.com>
+    SPDX-FileCopyrightText: 2022 Thiago Sueto <herzenschein@gmail.com>
 
     SPDX-License-Identifier: GPL-2.0-or-later
 */
