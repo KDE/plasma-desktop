e4dbd9af00c67fe6d0e824cf67609192f96ee6c6
Thiago Sueto <herzenschein@gmail.com>
2022-01-23 18:56:27 -0300

    Complete video mimetypes list

--- kcms/componentchooser/componentchooservideoplayer.cpp
+++ kcms/componentchooser/componentchooservideoplayer.cpp
@@ -17,13 +17,48 @@ ComponentChooserVideoPlayer::ComponentChooserVideoPlayer(QObject *parent)
 
 void ComponentChooserVideoPlayer::save()
 {
+    saveMimeTypeAssociation(QStringLiteral("video/3gp"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/3gpp"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/3gpp2"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/avi"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/divx"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/dv"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/fli"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/flv"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/mp2t"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
     saveMimeTypeAssociation(QStringLiteral("video/mp4"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/mp4v-es"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
     saveMimeTypeAssociation(QStringLiteral("video/mpeg"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
     saveMimeTypeAssociation(QStringLiteral("video/msvideo"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
     saveMimeTypeAssociation(QStringLiteral("video/ogg"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
-    saveMimeTypeAssociation(QStringLiteral("video/avi"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
-    saveMimeTypeAssociation(QStringLiteral("video/flv"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
-    saveMimeTypeAssociation(QStringLiteral("video/mkv"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/quicktime"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/vnd.divx"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/vnd.mpegurl"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/vnd.rn-realvideo"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
     saveMimeTypeAssociation(QStringLiteral("video/webm"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
-    saveMimeTypeAssociation(QStringLiteral("video/3gp"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/x-avi"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/x-flc"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/x-flv"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/x-m4v"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/x-matroska"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/x-mpeg2"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/x-ms-asf"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/x-msvideo"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/x-ms-wmv"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/x-ms-wmx"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/x-ogm"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/x-ogm+ogg"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/x-theora"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("video/x-theora+ogg"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("application/mxf"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("application/ogg"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("application/sdp"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("application/vnd.apple.mpegurl"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("application/vnd.ms-asf"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("application/vnd.rn-realmedia"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("application/vnd.rn-realmedia-vbr"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("application/x-extension-m4a"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("application/x-extension-mp4"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("application/x-matroska"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
+    saveMimeTypeAssociation(QStringLiteral("application/x-ogg"), m_applications[m_index].toMap()[QStringLiteral("storageId")].toString());
 }
