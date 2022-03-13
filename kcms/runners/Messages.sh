052cfe333900a0a07ee93324c904d004b0db7f07
Fushan Wen <qydwhotmail@gmail.com>
2022-03-13 11:45:55 +0800

    kcms/runners: Update Message.sh

--- kcms/runners/Messages.sh
+++ kcms/runners/Messages.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-$XGETTEXT `find . -name "*.cpp" -o -name "*.h"` -o $podir/kcm_search.pot
+$XGETTEXT `find . -name "*.cpp" -o -name "*.h" -o -name "*.qml"` -o $podir/kcm_search.pot
