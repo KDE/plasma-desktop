8a17d50a076d76202a077810e9c597b36e34476d
Volker Krause <vkrause@kde.org>
2022-03-30 18:12:09 +0200

    Adapt to QWidget::enterEvent changes in Qt6

--- kcms/touchpad/kcm/xlib/testarea.h
+++ kcms/touchpad/kcm/xlib/testarea.h
@@ -20,7 +20,11 @@ Q_SIGNALS:
     void leave();
 
 protected:
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     void enterEvent(QEvent *) override;
+#else
+    void enterEvent(QEnterEvent *) override;
+#endif
     void leaveEvent(QEvent *) override;
 
 private:
