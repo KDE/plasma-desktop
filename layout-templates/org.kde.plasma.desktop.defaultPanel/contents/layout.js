b581b490af87dd0dfc98bc0ea526c027f0d5bb2d
Fushan Wen <qydwhotmail@gmail.com>
2022-06-05 16:07:45 +0800

    layout-templates: let ShellCorona choose the available edge
    No need to set panel.location as ShellCorona::addPanel will automatically
    pick one available edge.

--- layout-templates/org.kde.plasma.desktop.defaultPanel/contents/layout.js
+++ layout-templates/org.kde.plasma.desktop.defaultPanel/contents/layout.js
@@ -1,29 +1,8 @@
 var panel = new Panel
 var panelScreen = panel.screen
-var freeEdges = {"bottom": true, "top": true, "left": true, "right": true}
 
-for (i = 0; i < panelIds.length; ++i) {
-    var tmpPanel = panelById(panelIds[i])
-    if (tmpPanel.screen == panelScreen) {
-        // Ignore the new panel
-        if (tmpPanel.id != panel.id) {
-            freeEdges[tmpPanel.location] = false;
-        }
-    }
-}
+// No need to set panel.location as ShellCorona::addPanel will automatically pick one available edge
 
-if (freeEdges["bottom"] == true) {
-    panel.location = "bottom";
-} else if (freeEdges["top"] == true) {
-    panel.location = "top";
-} else if (freeEdges["left"] == true) {
-    panel.location = "left";
-} else if (freeEdges["right"] == true) {
-    panel.location = "right";
-} else {
-    // There is no free edge, so leave the default value
-    panel.location = "top";
-}
 // For an Icons-Only Task Manager on the bottom, *3 is too much, *2 is too little
 // Round down to next highest even number since the Panel size widget only displays
 // even numbers
