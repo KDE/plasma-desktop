24e92862c85cdfee7a0fbe66336b7d259acd86a9
Fabian Vogt <fabian@ritter-vogt.de>
2022-08-10 19:18:44 +0200

    taskmanager: Use proper QUrls for recent document actions
    The resource can either be a URL or an absolute path, the url() method
    converts the latter into a proper URL. Without this, a local path might end
    up getting parsed (badly) as URL, causing weird behaviour later, like resorting
    to kioexec/kio-fuse.
    
    BUG: 457685
    
    
    (cherry picked from commit bf37558f36494d13f0fe0f00862937d859aa20f1)

--- applets/taskmanager/plugin/backend.cpp
+++ applets/taskmanager/plugin/backend.cpp
@@ -368,12 +368,8 @@ QVariantList Backend::recentDocumentActions(const QUrl &launcherUrl, QObject *pa
     while (actionCount < 5 && resultIt != results.end()) {
         const QString resource = (*resultIt).resource();
         const QString mimetype = (*resultIt).mimetype();
+        const QUrl url = (*resultIt).url();
         ++resultIt;
-
-        QUrl url(resource);
-        if (url.scheme().isEmpty()) {
-            url = QUrl::fromLocalFile(resource);
-        }
 
         if (!url.isValid()) {
             continue;
@@ -413,7 +409,7 @@ QVariantList Backend::recentDocumentActions(const QUrl &launcherUrl, QObject *pa
         action->setProperty("agent", storageId);
         action->setProperty("entryPath", desktopEntryUrl);
         action->setProperty("mimeType", mimetype);
-        action->setData(resource);
+        action->setData(url);
         connect(action, &QAction::triggered, this, &Backend::handleRecentDocumentAction);
 
         actions << QVariant::fromValue<QAction *>(action);
@@ -469,9 +465,9 @@ void Backend::handleRecentDocumentAction() const
     }
 
     const QString desktopPath = action->property("entryPath").toUrl().toLocalFile();
-    const QString resource = action->data().toString();
+    const QUrl url = action->data().toUrl();
 
-    if (desktopPath.isEmpty() || resource.isEmpty()) {
+    if (desktopPath.isEmpty() || url.isEmpty()) {
         auto query = UsedResources | Agent(agent) | Type::any() | Activity::current() | Url::file();
 
         KAStats::forgetResources(query);
@@ -506,8 +502,7 @@ void Backend::handleRecentDocumentAction() const
     auto *delegate = new KNotificationJobUiDelegate;
     delegate->setAutoErrorHandlingEnabled(true);
     job->setUiDelegate(delegate);
-
-    job->setUrls({QUrl(resource)});
+    job->setUrls({url});
     job->start();
 }
 
