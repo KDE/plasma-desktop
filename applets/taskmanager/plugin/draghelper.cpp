79fb4df147c444a6a2a25281a71e6f7357ccd348
Fushan Wen <qydwhotmail@gmail.com>
2022-11-03 21:46:59 +0800

    applets/taskmanager: remove workaround for Qt 6.4.1
    The fix will be picked to 6.4.1

--- applets/taskmanager/plugin/draghelper.cpp
+++ applets/taskmanager/plugin/draghelper.cpp
@@ -28,6 +28,7 @@ QVariantMap DragHelper::generateMimeData(const QString &mimeType, const QVariant
     const QString &taskUrlData = Backend::tryDecodeApplicationsUrl(url).toString();
     mimedata.insert(QStringLiteral("text/x-orgkdeplasmataskmanager_taskurl"), taskUrlData);
 
+#if QT_VERSION < QT_VERSION_CHECK(6, 4, 1)
     // Workaround for https://bugreports.qt.io/browse/QTBUG-71922
     QString idString;
     if (KWindowSystem::isPlatformX11()) {
@@ -38,6 +39,9 @@ QVariantMap DragHelper::generateMimeData(const QString &mimeType, const QVariant
     }
     mimedata.insert(mimeType, idString);
     mimedata.insert(QStringLiteral("application/x-orgkdeplasmataskmanager_taskbuttonitem"), idString);
-
+#else
+    mimedata.insert(mimeType, mimeData);
+    mimedata.insert(QStringLiteral("application/x-orgkdeplasmataskmanager_taskbuttonitem"), mimeData);
+#endif
     return mimedata;
 }
