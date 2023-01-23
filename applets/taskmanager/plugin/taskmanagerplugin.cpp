bce6b2d1abcaefc7e0c3e79d798efb189c0f627d
Fushan Wen <qydwhotmail@gmail.com>
2023-01-23 13:06:41 +0800

    applets/taskmanager: remove `DragHelper`

--- applets/taskmanager/plugin/taskmanagerplugin.cpp
+++ applets/taskmanager/plugin/taskmanagerplugin.cpp
@@ -7,7 +7,6 @@
 
 #include "taskmanagerplugin.h"
 #include "backend.h"
-#include "draghelper.h"
 
 #include "smartlaunchers/smartlauncheritem.h"
 
@@ -17,7 +16,6 @@ void TaskManagerPlugin::registerTypes(const char *uri)
 {
     Q_ASSERT(QLatin1String(uri) == QLatin1String("org.kde.plasma.private.taskmanager"));
     qmlRegisterType<Backend>(uri, 0, 1, "Backend");
-    qmlRegisterType<DragHelper>(uri, 0, 1, "DragHelper");
 
     qmlRegisterType<SmartLauncher::Item>(uri, 0, 1, "SmartLauncherItem");
 }
