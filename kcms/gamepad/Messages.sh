Sonic-DE/sonic-desktop-interface

Add new gamepad KCM

kcms/joystick2/Messages.sh
@@ -1,0 +1,3 @@
+#! /usr/bin/env bash
+$EXTRACTRC `find . -name \*.kcfg` >> rc.cpp
+$XGETTEXT `find . -name "*.cpp" -o -name "*.qml"` -o $podir/kcm_splashscreen.pot

KDE/plasma-desktop

kcms/gamepad/Messages.sh
@@ -1,0 +1,3 @@
+#! /usr/bin/env bash
+$EXTRACTRC `find . -name \*.kcfg` >> rc.cpp
+$XGETTEXT `find . -name "*.cpp" -o -name "*.qml"` -o $podir/kcm_gamepad.pot