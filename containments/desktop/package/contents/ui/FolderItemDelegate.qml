KDE/plasma-desktop

commit d5abf12d6661f2a8f8449d9abe88468114a73dca

Folder View: with menu representation, always open with a single click
The single-click/double click setting applies to file and folder views.
Folder View is definitely a file and folder view. However, when Folder
view is in its list/menu representation, it's not *presenting* as a file
or folder view, it's presenting as a menu. And menus always trigger
their items with a single click.

We already do a similar thing by always activating with a single
touchscreen tap, bypassing the user's single/double-click preference. So
there's precedent for having a hardcoded behavior to support certain UX
paradigms.

Hence, in list/menu mode, don't respect the user's setting for
single/double click and always activate items with a single click.

BUG: 420261
FIXED-IN: 6.0

containments/desktop/package/contents/ui/FolderItemDelegate.qml
@@ -116,7 +116,8 @@ Item {
 
             onHoveredChanged: {
                 if (hovered) {
-                    if (plasmoid.configuration.selectionMarkers && Qt.styleHints.singleClickActivation) {
+                    // In list view, it behaves more like a menu, and menus always activate their items on a single click
+                    if (plasmoid.configuration.selectionMarkers && (Qt.styleHints.singleClickActivation || main.GridView.view.isRootView)) {
                         selectionButton = selectionButtonComponent.createObject(actions);
                     }
 

containments/desktop/package/contents/ui/FolderView.qml
@@ -421,10 +421,10 @@ FocusScope {
                     return;
                 }
 
-                // Single-click mode and single-clicked on the item or
+                // Single-click mode or list view and single-clicked on the item or
                 // double-click mode and double-clicked on the item: open it
-                if (Qt.styleHints.singleClickActivation || doubleClickInProgress || mouse.source === Qt.MouseEventSynthesizedByQt) {
-                    var func = root.useListViewMode && (mouse.button === Qt.LeftButton) && hoveredItem.isDir ? doCd : dir.run;
+                if (Qt.styleHints.singleClickActivation || root.useListViewMode || doubleClickInProgress || mouse.source === Qt.MouseEventSynthesizedByQt) {
+                    var func = mouse.button === Qt.LeftButton && hoveredItem.isDir ? doCd : dir.run;
                     func(positioner.map(gridView.currentIndex));
                     previouslySelectedItemIndex = gridView.currentIndex;
                     hoveredItem = null;

commit 968f4043ae4a53e83ab41d502d69e37a759af230

Folder View: fix selection Marker visibility criteria
Broken in d5abf12d6661f2a8f8449d9abe88468114a73dca, which was checking
for main.GridView.view.isRootView when it should have been
root.useListViewMode (as the code comment suggests).

BUG: 471096

containments/desktop/package/contents/ui/FolderItemDelegate.qml
@@ -119,7 +119,7 @@ Item {
             onHoveredChanged: {
                 if (hovered) {
                     // In list view, it behaves more like a menu, and menus always activate their items on a single click
-                    if (plasmoid.configuration.selectionMarkers && (Qt.styleHints.singleClickActivation || main.GridView.view.isRootView)) {
+                    if (plasmoid.configuration.selectionMarkers && (Qt.styleHints.singleClickActivation || root.useListViewMode)) {
                         selectionButton = selectionButtonComponent.createObject(actions);
                     }
 