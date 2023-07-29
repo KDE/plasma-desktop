Sonic-DE/sonic-desktop-interface

applets/kickoff: add blocker to prevent focus change in search view
This adds an mouse position intercepter to record the mouse position
after entering a new search term, and if there is no mouse movement,
block focus on child items.

BUG: 466192
FIXED-IN: 5.27.7

applets/kickoff/package/contents/ui/AbstractKickoffItemDelegate.qml
@@ -138,7 +138,7 @@ T.ItemDelegate {
         hoverEnabled: root.view && !root.view.movedWithKeyboard
             // Fix VerticalStackView animation causing view currentIndex
             // to change while delegates are moving under the mouse cursor
-            && plasmoid.fullRepresentationItem && !plasmoid.fullRepresentationItem.contentItem.busy
+            && plasmoid.fullRepresentationItem && !plasmoid.fullRepresentationItem.contentItem.busy && !plasmoid.fullRepresentationItem.blockingFocus
         acceptedButtons: Qt.LeftButton | Qt.RightButton
         drag {
             axis: Drag.XAndYAxis

applets/kickoff/package/contents/ui/FullRepresentation.qml
@@ -36,6 +36,7 @@ EmptyPage {
     Layout.preferredHeight: Math.max(implicitHeight, height)
 
     property alias normalPage: normalPage
+    property bool blockingFocus: false
 
     /* NOTE: Important things to know about keyboard input handling:
      *
@@ -96,11 +97,36 @@ EmptyPage {
                     isSearchResult: true
                 }
                 activeFocusOnTab: true
+                property var interceptedPosition: null
                 // always focus the first item in the header focus chain
                 KeyNavigation.tab: root.header.nextItemInFocusChain()
                 T.StackView.onActivated: {
                     plasmoid.rootItem.sideBar = null
                     plasmoid.rootItem.contentArea = searchView
+                }
+
+                Connections {
+                    target: blockFocusHandler
+                    enabled: blockFocusHandler.enabled && !searchView.interceptedPosition
+                    function onPointChanged() {
+                        searchView.interceptedPosition = blockFocusHandler.point.position
+                    }
+                }
+
+                Connections {
+                    target: blockFocusHandler
+                    enabled: blockFocusHandler.enabled && searchView.interceptedPosition && root.blockingFocus
+                    function onPointChanged() {
+                        if (blockFocusHandler.point.position === searchView.interceptedPosition) {
+                            return;
+                        }
+                        root.blockingFocus = false
+                    }
+                }
+
+                HoverHandler {
+                    id: blockFocusHandler
+                    enabled: !contentItemStackView.busy && (!searchView.interceptedPosition || root.blockingFocus)
                 }
 
                 Loader {
@@ -147,11 +173,18 @@ EmptyPage {
             target: root.header
             function onSearchTextChanged() {
                 if (root.header.searchText.length === 0 && contentItemStackView.currentItem.objectName !== "normalPage") {
+                    root.blockingFocus = false
                     contentItemStackView.reverseTransitions = true
                     contentItemStackView.replace(normalPage)
-                } else if (root.header.searchText.length > 0 && contentItemStackView.currentItem.objectName !== "searchView") {
-                    contentItemStackView.reverseTransitions = false
-                    contentItemStackView.replace(searchViewComponent)
+                } else if (root.header.searchText.length > 0) {
+                    if (contentItemStackView.currentItem.objectName !== "searchView") {
+                        contentItemStackView.reverseTransitions = false
+                        contentItemStackView.replace(searchViewComponent)
+                    } else {
+                        root.blockingFocus = true
+                        contentItemStackView.contentItem.interceptedPosition = null
+                        contentItemStackView.contentItem.currentIndex = 0
+                    }
                 }
             }
         }

KDE/plasma-desktop

This adds a mouse position interceptor to record the mouse position
(cherry picked from commit fc85def499318caae360462568e239a1eeb7d61e)

applets/kickoff/package/contents/ui/AbstractKickoffItemDelegate.qml
@@ -138,7 +138,7 @@ T.ItemDelegate {
         hoverEnabled: root.view && !root.view.movedWithKeyboard
             // Fix VerticalStackView animation causing view currentIndex
             // to change while delegates are moving under the mouse cursor
-            && plasmoid.fullRepresentationItem && !plasmoid.fullRepresentationItem.contentItem.busy
+            && plasmoid.fullRepresentationItem && !plasmoid.fullRepresentationItem.contentItem.busy && !plasmoid.fullRepresentationItem.blockingHoverFocus
         acceptedButtons: Qt.LeftButton | Qt.RightButton
         drag {
             axis: Drag.XAndYAxis

applets/kickoff/package/contents/ui/FullRepresentation.qml
@@ -36,6 +36,7 @@ EmptyPage {
     Layout.preferredHeight: Math.max(implicitHeight, height)
 
     property alias normalPage: normalPage
+    property bool blockingHoverFocus: false
 
     /* NOTE: Important things to know about keyboard input handling:
      *
@@ -96,11 +97,36 @@ EmptyPage {
                     isSearchResult: true
                 }
                 activeFocusOnTab: true
+                property var interceptedPosition: null
                 // always focus the first item in the header focus chain
                 KeyNavigation.tab: root.header.nextItemInFocusChain()
                 T.StackView.onActivated: {
                     plasmoid.rootItem.sideBar = null
                     plasmoid.rootItem.contentArea = searchView
+                }
+
+                Connections {
+                    target: blockHoverFocusHandler
+                    enabled: blockHoverFocusHandler.enabled && !searchView.interceptedPosition
+                    function onPointChanged() {
+                        searchView.interceptedPosition = blockHoverFocusHandler.point.position
+                    }
+                }
+
+                Connections {
+                    target: blockHoverFocusHandler
+                    enabled: blockHoverFocusHandler.enabled && searchView.interceptedPosition && root.blockingHoverFocus
+                    function onPointChanged() {
+                        if (blockHoverFocusHandler.point.position === searchView.interceptedPosition) {
+                            return;
+                        }
+                        root.blockingHoverFocus = false
+                    }
+                }
+
+                HoverHandler {
+                    id: blockHoverFocusHandler
+                    enabled: !contentItemStackView.busy && (!searchView.interceptedPosition || root.blockingHoverFocus)
                 }
 
                 Loader {
@@ -147,11 +173,18 @@ EmptyPage {
             target: root.header
             function onSearchTextChanged() {
                 if (root.header.searchText.length === 0 && contentItemStackView.currentItem.objectName !== "normalPage") {
+                    root.blockingHoverFocus = false
                     contentItemStackView.reverseTransitions = true
                     contentItemStackView.replace(normalPage)
-                } else if (root.header.searchText.length > 0 && contentItemStackView.currentItem.objectName !== "searchView") {
-                    contentItemStackView.reverseTransitions = false
-                    contentItemStackView.replace(searchViewComponent)
+                } else if (root.header.searchText.length > 0) {
+                    if (contentItemStackView.currentItem.objectName !== "searchView") {
+                        contentItemStackView.reverseTransitions = false
+                        contentItemStackView.replace(searchViewComponent)
+                    } else {
+                        root.blockingHoverFocus = true
+                        contentItemStackView.contentItem.interceptedPosition = null
+                        contentItemStackView.contentItem.currentIndex = 0
+                    }
                 }
             }
         }