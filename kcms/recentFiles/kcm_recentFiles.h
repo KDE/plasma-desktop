9f524a6e5dd01f42a3f82bc15b95807ab31046f6
Méven Car <meven@kde.org>
2023-09-13 10:37:57 +0200

    Revert "RecentFiles kcm: whattoremember: Make the value visible in the UI match the value saved"
    This implied a version bump of KDE Frameworks for kcmutils.
    
    This reverts commit b57bf61d8d18cc4b7319b1af5514a8466761d321.

--- kcms/recentFiles/kcm_recentFiles.h
+++ kcms/recentFiles/kcm_recentFiles.h
@@ -39,7 +39,6 @@ private Q_SLOTS:
     void forgetAll();
 
     void spinKeepHistoryValueChanged(int value);
-    void whatToRememberWidgetChanged(bool checked);
 
 Q_SIGNALS:
     void blackListModelChanged(bool changed);
