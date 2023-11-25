0bb2e54d885ed5b73a50f2f1dfe3e5243151a21d
Alexander Lohnau <alexander.lohnau@gmx.de>
2023-11-25 12:55:53 +0100

    Remove old kconf_update files
    Most of those files are from 2020 or 2021, meaning consumers should have run them, since we had an LTS in between and the Plasma6 work caused a delay in the release cycle

--- kcms/ksmserver/kconf_update/ksmserver_update_loginMode_value.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import fileinput
-
-for line in fileinput.input():
-    if line.startswith("loginMode="):
-        print ("# DELETE loginMode")
-        line = line.replace("default", "emptySession")
-        print (line.strip())
