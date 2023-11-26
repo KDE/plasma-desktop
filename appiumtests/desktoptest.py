6de63a98566935f1294f32e2c60e3b232220c422
Fushan Wen <qydwhotmail@gmail.com>
2023-11-26 12:52:17 +0800

    appiumtests/desktoptest: test return code

--- appiumtests/desktoptest.py
+++ appiumtests/desktoptest.py
@@ -98,7 +98,11 @@ class DesktopTest(unittest.TestCase):
         """
         Make sure to terminate the driver again, lest it dangles.
         """
-        subprocess.check_output(["kquitapp6", "plasmashell"], stderr=sys.stderr)
+        if cls.plasmashell is not None:
+            subprocess.check_output(["kquitapp6", "plasmashell"], stderr=sys.stderr)
+            WebDriverWait(cls.driver, 30).until_not(lambda _: cls.plasmashell.poll() is None)
+            assert cls.plasmashell.returncode == 0, cls.plasmashell.returncode
+
         if cls.kded:
             cls.kded.kill()
         if cls.kactivitymanagerd:
