064e72647a01080947f58dcc7b96181d807552e3
Prajna Sariputra <putr4.s@gmail.com>
2023-11-30 16:09:02 +0000

    ComponentChooser: Add WebM to the list of video formats
    Notably Spectacle's screen recordings are in that format by default.
    
    I also ran `clang-format`, which is what switched the list over to three columns.

--- kcms/componentchooser/components/componentchooservideoplayer.cpp
+++ kcms/componentchooser/components/componentchooservideoplayer.cpp
@@ -16,22 +16,17 @@ ComponentChooserVideoPlayer::ComponentChooserVideoPlayer(QObject *parent)
 {
 }
 
-static const QStringList videoMimetypes{"video/3gp",          "video/3gpp",
-                                        "video/3gpp2",        "video/avi",
-                                        "video/divx",         "video/dv",
-                                        "video/fli",          "video/flv",
-                                        "video/mp2t",         "video/mp4",
-                                        "video/mp4v-es",      "video/mpeg",
-                                        "video/msvideo",      "video/ogg",
-                                        "video/quicktime",    "video/vnd.divx",
-                                        "video/vnd.mpegurl",  "video/vnd.rn-realvideo",
-                                        "video/x-avi",        "video/x-flv",
-                                        "video/x-m4v",        "video/x-matroska",
-                                        "video/x-mpeg2",      "video/x-ms-asf",
-                                        "video/x-msvideo",    "video/x-ms-wmv",
-                                        "video/x-ms-wmx",     "video/x-ogm",
-                                        "video/x-ogm+ogg",    "video/x-theora",
-                                        "video/x-theora+ogg", "application/x-matroska"};
+static const QStringList videoMimetypes{"video/3gp",      "video/3gpp",         "video/3gpp2",
+                                        "video/avi",      "video/divx",         "video/dv",
+                                        "video/fli",      "video/flv",          "video/mp2t",
+                                        "video/mp4",      "video/mp4v-es",      "video/mpeg",
+                                        "video/msvideo",  "video/ogg",          "video/quicktime",
+                                        "video/vnd.divx", "video/vnd.mpegurl",  "video/vnd.rn-realvideo",
+                                        "video/webm",     "video/x-avi",        "video/x-flv",
+                                        "video/x-m4v",    "video/x-matroska",   "video/x-mpeg2",
+                                        "video/x-ms-asf", "video/x-msvideo",    "video/x-ms-wmv",
+                                        "video/x-ms-wmx", "video/x-ogm",        "video/x-ogm+ogg",
+                                        "video/x-theora", "video/x-theora+ogg", "application/x-matroska"};
 
 QStringList ComponentChooserVideoPlayer::mimeTypes() const
 {
