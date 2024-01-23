9857d8e523309df88105bf6dfd8a8f8e4f41c075
Eugene Popov <popov895@ukr.net>
2024-01-23 17:58:06 +0000

    Folder View: Fix handling of numpad keys
    On X11, all events from numpad keys come with the Qt::KeypadModifier
    keyboard modifier. We should take this into account when handling
    keyboard shortcuts.
    
    BUG: 480169
    FIXED-IN: 6.0

--- containments/desktop/plugins/folder/shortcut.cpp
+++ containments/desktop/plugins/folder/shortcut.cpp
@@ -27,7 +27,7 @@ bool ShortCut::eventFilter(QObject *obj, QEvent *e)
 {
     if (e->type() == QEvent::KeyPress) {
         QKeyEvent *keyEvent = static_cast<QKeyEvent *>(e);
-        int keyInt = keyEvent->modifiers() | keyEvent->key();
+        const int keyInt = keyEvent->modifiers() & ~Qt::KeypadModifier | keyEvent->key();
         if (KStandardShortcut::deleteFile().contains(QKeySequence(keyInt))) {
             Q_EMIT deleteFile();
             return true;
