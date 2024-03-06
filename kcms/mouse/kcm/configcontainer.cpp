130e3bb51f7cd7db00e5f45a5137af8fa8e77b45
Jakob Petsovits <jpetso@petsovits.com>
2024-03-06 10:48:49 -0500

    kcms/mouse: Don't crash when no supported backend is found
    If InputBackend::implementation() and ConfigPlugin::implementation()
    can return a nullptr, we need to have null pointer checks where they
    get used.
    
    Also, remove some dead code without these checks to avoid confusion.
    
    BUG: 482048

--- kcms/mouse/kcm/configcontainer.cpp
+++ kcms/mouse/kcm/configcontainer.cpp
@@ -17,7 +17,9 @@ extern "C" {
 Q_DECL_EXPORT void kcminit()
 {
     std::unique_ptr<InputBackend> backend(InputBackend::implementation());
-    backend->kcmInit();
+    if (backend) {
+        backend->kcmInit();
+    }
 }
 }
 
@@ -27,40 +29,23 @@ ConfigContainer::ConfigContainer(QObject *parent, const KPluginMetaData &data)
     m_plugin = ConfigPlugin::implementation(this);
 }
 
-/*QSize ConfigContainer::minimumSizeHint() const
-{
-    return m_plugin->minimumSizeHint();
-}
-QSize ConfigContainer::sizeHint() const
-{
-    return m_plugin->sizeHint();
-}
-void ConfigContainer::resizeEvent(QResizeEvent *event)
-{
-    Q_UNUSED(event);
-    m_plugin->resize(this->size());
-}
-*/
-
 void ConfigContainer::load()
 {
-    m_plugin->load();
+    if (m_plugin) {
+        m_plugin->load();
+    }
 }
 
 void ConfigContainer::save()
 {
-    m_plugin->save();
+    if (m_plugin) {
+        m_plugin->save();
+    }
 }
 
 void ConfigContainer::defaults()
 {
-    m_plugin->defaults();
+    if (m_plugin) {
+        m_plugin->defaults();
+    }
 }
-
-/*void ConfigContainer::hideEvent(QHideEvent *e)
-{
-    m_plugin->hideEvent(e);
-    KCModule::hideEvent(e);
-}
-
-*/
