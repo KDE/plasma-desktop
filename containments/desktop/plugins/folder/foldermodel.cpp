0264726f8720d3093bd3ba10f6107197b4f90be3
Kai Uwe Broulik <kde@privat.broulik.de>
2024-03-08 17:36:18 +0100

    Folder Model: Handle invalid URL in desktop file
    There's no point in handling an invalid URL, and
    KProtocolInfo asserts that the scheme is not empty.
    
    BUG: 482889
    
    
    (cherry picked from commit 0504f0e0ef2b0ebc4f1b1faa9ee8c74d2191931b)

--- containments/desktop/plugins/folder/foldermodel.cpp
+++ containments/desktop/plugins/folder/foldermodel.cpp
@@ -1412,6 +1412,9 @@ bool FolderModel::isDir(const QModelIndex &index, const KDirModel *dirModel) con
         }
 
         const QUrl url(file.readUrl());
+        if (!url.isValid()) {
+            return false;
+        }
 
         // Check if we already have a running StatJob for this URL.
         if (m_isDirJobs.contains(item.url())) {
@@ -1425,7 +1428,7 @@ bool FolderModel::isDir(const QModelIndex &index, const KDirModel *dirModel) con
             return true;
         }
 
-        if (KProtocolInfo::protocolClass(url.scheme()) != QLatin1String(":local")) {
+        if (!url.scheme().isEmpty() && KProtocolInfo::protocolClass(url.scheme()) != QLatin1String(":local")) {
             return false;
         }
 
