Sonic-DE/sonic-desktop-interface

kcms/mouse: Make QML more declarative
A lot of that imperative logic can be replaced with bindings,
which simplifies the code quite a bit. The "Reset" button is now
also properly disabled once settings are reverted to the last
saved state.

The C++ changes primarily ensure that change notification signals
are emitted when the value changes. On the QML side, the "deviceless"
variant is not needed anymore and the regular main.qml covers both
Wayland as well as "deviceless" X11 with visibility conditions for
form controls.

kcms/mouse/inputbackend.h
@@ -28,6 +28,9 @@ class InputBackend : public QObject
 {
     Q_OBJECT
 
+    Q_PROPERTY(QList<QObject *> mouseDevices READ getDevices NOTIFY devicesChanged)
+    Q_PROPERTY(bool isAnonymousDevice READ isAnonymousDevice CONSTANT)
+
 protected:
     explicit InputBackend(QObject *parent)
         : QObject(parent)
@@ -54,15 +57,6 @@ public:
 
     virtual void load()
     {
-    }
-
-    virtual bool applyConfig(const QVariantHash &)
-    {
-        return false;
-    }
-    virtual bool getConfig(QVariantHash &)
-    {
-        return false;
     }
 
     virtual bool applyConfig()
@@ -92,12 +86,17 @@ public:
     {
         return 0;
     }
+    virtual bool isAnonymousDevice() const
+    {
+        return false;
+    }
     virtual QList<QObject *> getDevices() const
     {
         return QList<QObject *>();
     }
 
 Q_SIGNALS:
+    void devicesChanged();
     void deviceAdded(bool success);
     void deviceRemoved(int index);
 };

KDE/plasma-desktop

kcms/mouse/inputbackend.h
@@ -28,6 +28,9 @@ class InputBackend : public QObject
 {
     Q_OBJECT
 
+    Q_PROPERTY(QList<QObject *> inputDevices READ getDevices NOTIFY devicesChanged)
+    Q_PROPERTY(bool isAnonymousInputDevice READ isAnonymousDevice CONSTANT)
+
 protected:
     explicit InputBackend(QObject *parent)
         : QObject(parent)
@@ -54,15 +57,6 @@ public:
 
     virtual void load()
     {
-    }
-
-    virtual bool applyConfig(const QVariantHash &)
-    {
-        return false;
-    }
-    virtual bool getConfig(QVariantHash &)
-    {
-        return false;
     }
 
     virtual bool applyConfig()
@@ -92,12 +86,17 @@ public:
     {
         return 0;
     }
+    virtual bool isAnonymousDevice() const
+    {
+        return false;
+    }
     virtual QList<QObject *> getDevices() const
     {
         return QList<QObject *>();
     }
 
 Q_SIGNALS:
+    void devicesChanged();
     void deviceAdded(bool success);
     void deviceRemoved(int index);
 };