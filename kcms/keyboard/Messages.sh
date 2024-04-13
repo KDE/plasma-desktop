445ed61054ff3f46424d6e9f003aa60eef6b5f8a
Nicolas Fella <nicolas.fella@gmx.de>
2024-04-14 01:09:48 +0200

    [kcms/keyboard] Fix translations
    For QML KCMs the domain name needs to match the KCM name

--- kcms/keyboard/Messages.sh
+++ kcms/keyboard/Messages.sh
@@ -1,4 +1,2 @@
 #! /usr/bin/env bash
-$EXTRACTRC kcm*.ui >> rc.cpp
-$XGETTEXT `find . -name "*.cpp" -o -name "*.qml"` -o $podir/kcmkeyboard.pot
-rm -f rc.cpp
+$XGETTEXT `find . -name "*.cpp" -o -name "*.qml"` -o $podir/kcm_keyboard.pot
