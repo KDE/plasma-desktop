7aa623d4437b521a0363e54dde82eee22c85434a
ivan tkachenko <me@ratijas.tk>
2024-06-03 02:04:27 +0600

    kcms/mouse: Drop unused name member of X11 device's properties
    Property name has no uses.

--- kcms/mouse/backends/x11/x11_libinput_dummydevice.h
+++ kcms/mouse/backends/x11/x11_libinput_dummydevice.h
@@ -197,9 +197,8 @@ private:
     struct Prop {
         using ChangedSignal = void (X11LibinputDummyDevice::*)();
 
-        explicit Prop(X11LibinputDummyDevice *device, const QString &name, ChangedSignal changedSignal = nullptr, const QString &cfgName = u""_s)
-            : name(name)
-            , cfgName(cfgName)
+        explicit Prop(X11LibinputDummyDevice *device, ChangedSignal changedSignal = nullptr, const QString &cfgName = u""_s)
+            : cfgName(cfgName)
             , changedSignalFunction(changedSignal)
             , device(device)
         {
@@ -234,7 +233,6 @@ private:
             set(std::move(newVal));
         }
 
-        QString name;
         QString cfgName;
 
         const ChangedSignal changedSignalFunction;
@@ -250,47 +248,42 @@ private:
 
     //
     // general
-    Prop<QString> m_name{this, u"name"_s};
-    Prop<QString> m_sysName{this, u"sysName"_s};
-    Prop<bool> m_supportsDisableEvents{this, u"supportsDisableEvents"_s};
-    Prop<bool> m_enabled{this, u"enabled"_s, &X11LibinputDummyDevice::enabledChanged};
+    Prop<QString> m_name{this};
+    Prop<QString> m_sysName{this};
+    Prop<bool> m_supportsDisableEvents{this};
+    Prop<bool> m_enabled{this, &X11LibinputDummyDevice::enabledChanged};
 
     //
     // advanced
-    Prop<Qt::MouseButtons> m_supportedButtons{this, u"supportedButtons"_s};
+    Prop<Qt::MouseButtons> m_supportedButtons{this};
 
-    Prop<bool> m_supportsLeftHanded{this, u"supportsLeftHanded"_s};
-    Prop<bool> m_leftHandedEnabledByDefault{this, u"leftHandedEnabledByDefault"_s};
-    Prop<bool> m_leftHanded{this, u"leftHanded"_s, &X11LibinputDummyDevice::leftHandedChanged, u"XLbInptLeftHanded"_s};
+    Prop<bool> m_supportsLeftHanded{this};
+    Prop<bool> m_leftHandedEnabledByDefault{this};
+    Prop<bool> m_leftHanded{this, &X11LibinputDummyDevice::leftHandedChanged, u"XLbInptLeftHanded"_s};
 
-    Prop<bool> m_supportsMiddleEmulation{this, u"supportsMiddleEmulation"_s};
-    Prop<bool> m_middleEmulationEnabledByDefault{this, u"middleEmulationEnabledByDefault"_s};
-    Prop<bool> m_middleEmulation{this, u"middleEmulation"_s, &X11LibinputDummyDevice::middleEmulationChanged, u"XLbInptMiddleEmulation"_s};
+    Prop<bool> m_supportsMiddleEmulation{this};
+    Prop<bool> m_middleEmulationEnabledByDefault{this};
+    Prop<bool> m_middleEmulation{this, &X11LibinputDummyDevice::middleEmulationChanged, u"XLbInptMiddleEmulation"_s};
 
     //
     // acceleration speed and profile
-    Prop<bool> m_supportsPointerAcceleration{this, u"supportsPointerAcceleration"_s};
-    Prop<qreal> m_defaultPointerAcceleration{this, u"defaultPointerAcceleration"_s};
-    Prop<qreal> m_pointerAcceleration{this, u"pointerAcceleration"_s, &X11LibinputDummyDevice::pointerAccelerationChanged, u"XLbInptPointerAcceleration"_s};
+    Prop<bool> m_supportsPointerAcceleration{this};
+    Prop<qreal> m_defaultPointerAcceleration{this};
+    Prop<qreal> m_pointerAcceleration{this, &X11LibinputDummyDevice::pointerAccelerationChanged, u"XLbInptPointerAcceleration"_s};
 
-    Prop<bool> m_supportsPointerAccelerationProfileFlat{this, u"supportsPointerAccelerationProfileFlat"_s};
-    Prop<bool> m_defaultPointerAccelerationProfileFlat{this, u"defaultPointerAccelerationProfileFlat"_s};
-    Prop<bool> m_pointerAccelerationProfileFlat{this,
-                                                u"pointerAccelerationProfileFlat"_s,
-                                                &X11LibinputDummyDevice::pointerAccelerationProfileFlatChanged,
-                                                u"XLbInptAccelProfileFlat"_s};
+    Prop<bool> m_supportsPointerAccelerationProfileFlat{this};
+    Prop<bool> m_defaultPointerAccelerationProfileFlat{this};
+    Prop<bool> m_pointerAccelerationProfileFlat{this, &X11LibinputDummyDevice::pointerAccelerationProfileFlatChanged, u"XLbInptAccelProfileFlat"_s};
 
-    Prop<bool> m_supportsPointerAccelerationProfileAdaptive{this, u"supportsPointerAccelerationProfileAdaptive"_s};
-    Prop<bool> m_defaultPointerAccelerationProfileAdaptive{this, u"defaultPointerAccelerationProfileAdaptive"_s};
-    Prop<bool> m_pointerAccelerationProfileAdaptive{this,
-                                                    u"pointerAccelerationProfileAdaptive"_s,
-                                                    &X11LibinputDummyDevice::pointerAccelerationProfileAdaptiveChanged};
+    Prop<bool> m_supportsPointerAccelerationProfileAdaptive{this};
+    Prop<bool> m_defaultPointerAccelerationProfileAdaptive{this};
+    Prop<bool> m_pointerAccelerationProfileAdaptive{this, &X11LibinputDummyDevice::pointerAccelerationProfileAdaptiveChanged};
 
     //
     // scrolling
-    Prop<bool> m_supportsNaturalScroll{this, u"supportsNaturalScroll"_s};
-    Prop<bool> m_naturalScrollEnabledByDefault{this, u"naturalScrollEnabledByDefault"_s};
-    Prop<bool> m_naturalScroll{this, u"naturalScroll"_s, &X11LibinputDummyDevice::naturalScrollChanged, u"XLbInptNaturalScroll"_s};
+    Prop<bool> m_supportsNaturalScroll{this};
+    Prop<bool> m_naturalScrollEnabledByDefault{this};
+    Prop<bool> m_naturalScroll{this, &X11LibinputDummyDevice::naturalScrollChanged, u"XLbInptNaturalScroll"_s};
 
     std::unique_ptr<LibinputSettings> m_settings;
     Display *m_dpy = nullptr;
