ca0884b6d06775e013152112ae53e55c01a597ae
Fushan Wen <qydwhotmail@gmail.com>
2024-06-02 15:37:45 +0800

    kcms/keyboard: fix nonexistent header in dev build

--- kcms/keyboard/qml/plugin.cpp
+++ kcms/keyboard/qml/plugin.cpp
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 // SPDX-FileCopyrightText: 2023 Harald Sitter <sitter@kde.org>
 
+#include <QQmlEngine>
 #include <QQmlExtensionPlugin>
-#include <QmlTypeAndRevisionsRegistration>
 
 #include <flags.h>
 
