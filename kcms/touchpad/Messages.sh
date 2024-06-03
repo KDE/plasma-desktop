f51176836ed5d2285c3b9961efe5c50b89696e12
Albert Astals Cid <aacid@kde.org>
2024-06-04 00:32:56 +0200

    Messages.sh: There's no kcm folder anymore

--- kcms/touchpad/Messages.sh
+++ kcms/touchpad/Messages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT rc.cpp `find kcm -name \*.qml` `find . -name \*.cpp -o -name \*.h` -o $podir/kcm_touchpad.pot
+$XGETTEXT rc.cpp `find . -name \*.qml -o -name \*.cpp -o -name \*.h` -o $podir/kcm_touchpad.pot
