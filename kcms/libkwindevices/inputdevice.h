Sonic-DE/sonic-desktop-interface

[kcms/tablet] Add combined tablet model
KWin/libinput splits tablet into separate tool and pad devices for technical reasons,
and we expose that verbatim in the UI.

That leads to poor UX since for the user it is one physical device

This adds a model that combines devices that belong together into one logical device

This is done using the libinput device group property, which was created specifically
for this purpose

kcms/libkwindevices/inputdevice.h
@@ -98,6 +98,21 @@ public:
     }
     void setMapToWorkspace(bool mapToWorkspace);
 
+    quint64 deviceGroup() const
+    {
+        return m_deviceGroup.value();
+    }
+
+    bool tabletPad() const
+    {
+        return m_tabletPad.value();
+    }
+
+    bool tabletTool() const
+    {
+        return m_tabletTool.value();
+    }
+
 Q_SIGNALS:
     void needsSaveChanged();
 
@@ -225,6 +240,9 @@ private:
 
     Prop<bool> m_mapToWorkspace =
         Prop<bool>(this, "mapToWorkspace", &OrgKdeKWinInputDeviceInterface::defaultMapToWorkspace, nullptr, &InputDevice::mapToWorkspaceChanged);
+    Prop<quint64> m_deviceGroup = Prop<quint64>(this, "deviceGroup");
+    Prop<bool> m_tabletPad = Prop<bool>(this, "tabletPad");
+    Prop<bool> m_tabletTool = Prop<bool>(this, "tabletTool");
 
     std::unique_ptr<OrgKdeKWinInputDeviceInterface> m_iface;
 };

kcms/tablet/tabletsmodel.cpp
@@ -1,0 +1,166 @@
+/*
+    SPDX-FileCopyrightText: 2018 Roman Gilg <subdiff@gmail.com>
+    SPDX-FileCopyrightText: 2021 Aleix Pol Gonzalez <aleixpol@kde.org>
+
+    Work sponsored by Technische Universität Dresden:
+    SPDX-FileCopyrightText: 2022 Klarälvdalens Datakonsult AB a KDAB Group company <info@kdab.com>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "tabletsmodel.h"
+
+#include <utility>
+
+#include <QDBusInterface>
+
+#include "inputdevice.h"
+#include "logging.h"
+
+TabletsModel::TabletsModel(QObject *parent)
+    : QAbstractListModel(parent)
+{
+    m_deviceManager = new QDBusInterface(QStringLiteral("org.kde.KWin"),
+                                         QStringLiteral("/org/kde/KWin/InputDevice"),
+                                         QStringLiteral("org.kde.KWin.InputDeviceManager"),
+                                         QDBusConnection::sessionBus(),
+                                         this);
+
+    resetModel();
+
+    m_deviceManager->connection().connect(QStringLiteral("org.kde.KWin"),
+                                          QStringLiteral("/org/kde/KWin/InputDevice"),
+                                          QStringLiteral("org.kde.KWin.InputDeviceManager"),
+                                          QStringLiteral("deviceAdded"),
+                                          this,
+                                          SLOT(onDeviceAdded(QString)));
+    m_deviceManager->connection().connect(QStringLiteral("org.kde.KWin"),
+                                          QStringLiteral("/org/kde/KWin/InputDevice"),
+                                          QStringLiteral("org.kde.KWin.InputDeviceManager"),
+                                          QStringLiteral("deviceRemoved"),
+                                          this,
+                                          SLOT(onDeviceRemoved(QString)));
+}
+
+void TabletsModel::resetModel()
+{
+    beginResetModel();
+    m_devices.clear();
+
+    QStringList devicesSysNames;
+    const QVariant reply = m_deviceManager->property("devicesSysNames");
+    if (reply.isValid()) {
+        devicesSysNames = reply.toStringList();
+    } else {
+        qWarning() << "Error on receiving device list from KWin.";
+        return;
+    }
+
+    for (const QString &sysname : std::as_const(devicesSysNames)) {
+        addDevice(sysname, false);
+    }
+    endResetModel();
+}
+
+QVariant TabletsModel::data(const QModelIndex &index, int role) const
+{
+    if (!checkIndex(index, CheckIndexOption::IndexIsValid) || index.column() != 0) {
+        return {};
+    }
+
+    switch (role) {
+    case Qt::DisplayRole:
+
+        qWarning() << m_devices.at(index.row()).padDevice.get() << m_devices.at(index.row()).penDevice.get();
+
+        return QString(m_devices.at(index.row()).padDevice->name() + QLatin1String(" and ") + m_devices.at(index.row()).penDevice->name());
+    }
+    return {};
+}
+
+int TabletsModel::rowCount(const QModelIndex &parent) const
+{
+    return parent.isValid() ? 0 : m_devices.size();
+}
+
+void TabletsModel::onDeviceAdded(const QString &sysName)
+{
+    if (std::any_of(m_devices.cbegin(), m_devices.cend(), [sysName](auto &t) {
+            return t.penDevice->sysName() == sysName || t.padDevice->sysName() == sysName;
+        })) {
+        return;
+    }
+
+    addDevice(sysName, true);
+}
+
+void TabletsModel::addDevice(const QString &sysName, bool tellModel)
+{
+    QDBusInterface deviceIface(QStringLiteral("org.kde.KWin"),
+                               QStringLiteral("/org/kde/KWin/InputDevice/") + sysName,
+                               QStringLiteral("org.kde.KWin.InputDevice"),
+                               QDBusConnection::sessionBus(),
+                               this);
+    QVariant reply = deviceIface.property("tabletTool");
+    QVariant reply2 = deviceIface.property("tabletPad");
+    if (reply.isValid() && (reply.toBool() || reply2.toBool())) {
+        auto dev = std::make_unique<InputDevice>(sysName, this);
+        connect(dev.get(), &InputDevice::needsSaveChanged, this, &TabletsModel::needsSaveChanged);
+
+        qDebug().nospace() << "Device connected: " << dev->name() << " (" << dev->sysName() << ")";
+
+        quint64 deviceGroup = dev->deviceGroup();
+
+        auto it = std::find_if(m_devices.begin(), m_devices.end(), [deviceGroup](const TabletDevice &device) {
+            return device.deviceGroup == deviceGroup;
+        });
+
+        if (it != m_devices.end()) {
+            if (dev->tabletTool()) {
+                it->penDevice = std::move(dev);
+            } else if (dev->tabletPad()) {
+                it->padDevice = std::move(dev);
+            }
+        } else {
+            TabletDevice tablet;
+            tablet.deviceGroup = deviceGroup;
+
+            if (dev->tabletTool()) {
+                tablet.penDevice = std::move(dev);
+            } else if (dev->tabletPad()) {
+                tablet.padDevice = std::move(dev);
+            }
+
+            if (tellModel) {
+                beginInsertRows({}, m_devices.size(), m_devices.size());
+            }
+            m_devices.push_back(std::move(tablet));
+            if (tellModel) {
+                endInsertRows();
+            }
+        }
+    }
+}
+
+void TabletsModel::onDeviceRemoved(const QString &sysName)
+{
+    auto it = std::find_if(m_devices.cbegin(), m_devices.cend(), [sysName](auto &t) {
+        return t.penDevice->sysName() == sysName || t.padDevice->sysName() == sysName;
+    });
+    if (it == m_devices.cend()) {
+        return;
+    }
+
+    int index = std::distance(m_devices.cbegin(), it);
+
+    beginRemoveRows({}, index, index);
+    m_devices.erase(it);
+    endRemoveRows();
+}
+
+QHash<int, QByteArray> TabletsModel::roleNames() const
+{
+    return {
+        {Qt::DisplayRole, "display"},
+    };
+}

kcms/tablet/tabletsmodel.h
@@ -1,0 +1,55 @@
+/*
+    SPDX-FileCopyrightText: 2021 Aleix Pol Gonzalez <aleixpol@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QAbstractListModel>
+
+#include <memory>
+#include <vector>
+
+#include "inputdevice.h"
+
+class QDBusInterface;
+// class InputDevice;
+
+struct TabletDevice {
+    quint64 deviceGroup;
+    std::unique_ptr<InputDevice> padDevice;
+    std::unique_ptr<InputDevice> penDevice;
+};
+
+class TabletsModel : public QAbstractListModel
+{
+    Q_OBJECT
+public:
+    explicit TabletsModel(QObject *parent = nullptr);
+
+    QHash<int, QByteArray> roleNames() const override;
+    QVariant data(const QModelIndex &index, int role) const override;
+    int rowCount(const QModelIndex &parent) const override;
+    // Q_SCRIPTABLE InputDevice *deviceAt(int row) const;
+
+    // void load();
+    // void save();
+    // void defaults();
+    // bool isSaveNeeded() const;
+    // bool isDefaults() const;
+
+private Q_SLOTS:
+    void onDeviceAdded(const QString &sysName);
+    void onDeviceRemoved(const QString &sysName);
+
+Q_SIGNALS:
+    void needsSaveChanged();
+
+private:
+    void addDevice(const QString &sysname, bool tellModel);
+    void resetModel();
+
+    std::vector<TabletDevice> m_devices;
+    QDBusInterface *m_deviceManager;
+};

KDE/plasma-desktop

kcms/libkwindevices/inputdevice.h
@@ -117,6 +117,21 @@ public:
     }
     void setMapToWorkspace(bool mapToWorkspace);
 
+    QString deviceGroup() const
+    {
+        return m_deviceGroup.value();
+    }
+
+    bool tabletPad() const
+    {
+        return m_tabletPad.value();
+    }
+
+    bool tabletTool() const
+    {
+        return m_tabletTool.value();
+    }
+
 Q_SIGNALS:
     void needsSaveChanged();
 
@@ -244,6 +259,9 @@ private:
 
     Prop<bool> m_mapToWorkspace =
         Prop<bool>(this, "mapToWorkspace", &OrgKdeKWinInputDeviceInterface::defaultMapToWorkspace, nullptr, &InputDevice::mapToWorkspaceChanged);
+    Prop<QString> m_deviceGroup = Prop<QString>(this, "deviceGroupId");
+    Prop<bool> m_tabletPad = Prop<bool>(this, "tabletPad");
+    Prop<bool> m_tabletTool = Prop<bool>(this, "tabletTool");
 
     Prop<bool> m_supportsCalibrationMatrix =
         Prop<bool>(this, "supportsCalibrationMatrix", nullptr, &OrgKdeKWinInputDeviceInterface::supportsCalibrationMatrix, nullptr);

kcms/tablet/tabletsmodel.cpp
@@ -1,0 +1,178 @@
+/*
+    SPDX-FileCopyrightText: 2018 Roman Gilg <subdiff@gmail.com>
+    SPDX-FileCopyrightText: 2021 Aleix Pol Gonzalez <aleixpol@kde.org>
+
+    Work sponsored by Technische Universität Dresden:
+    SPDX-FileCopyrightText: 2022 Klarälvdalens Datakonsult AB a KDAB Group company <info@kdab.com>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "tabletsmodel.h"
+
+#include <utility>
+
+#include <QDBusInterface>
+
+#include "inputdevice.h"
+#include "logging.h"
+
+extern "C" {
+#include <libwacom/libwacom.h>
+}
+
+TabletsModel::TabletsModel(QObject *parent)
+    : QAbstractListModel(parent)
+{
+    m_deviceManager = new QDBusInterface(QStringLiteral("org.kde.KWin"),
+                                         QStringLiteral("/org/kde/KWin/InputDevice"),
+                                         QStringLiteral("org.kde.KWin.InputDeviceManager"),
+                                         QDBusConnection::sessionBus(),
+                                         this);
+
+    resetModel();
+
+    m_deviceManager->connection().connect(QStringLiteral("org.kde.KWin"),
+                                          QStringLiteral("/org/kde/KWin/InputDevice"),
+                                          QStringLiteral("org.kde.KWin.InputDeviceManager"),
+                                          QStringLiteral("deviceAdded"),
+                                          this,
+                                          SLOT(onDeviceAdded(QString)));
+    m_deviceManager->connection().connect(QStringLiteral("org.kde.KWin"),
+                                          QStringLiteral("/org/kde/KWin/InputDevice"),
+                                          QStringLiteral("org.kde.KWin.InputDeviceManager"),
+                                          QStringLiteral("deviceRemoved"),
+                                          this,
+                                          SLOT(onDeviceRemoved(QString)));
+}
+
+void TabletsModel::resetModel()
+{
+    beginResetModel();
+    m_devices.clear();
+
+    QStringList devicesSysNames;
+    const QVariant reply = m_deviceManager->property("devicesSysNames");
+    if (reply.isValid()) {
+        devicesSysNames = reply.toStringList();
+    } else {
+        qWarning() << "Error on receiving device list from KWin.";
+        return;
+    }
+
+    for (const QString &sysname : std::as_const(devicesSysNames)) {
+        addDevice(sysname, false);
+    }
+    endResetModel();
+}
+
+QVariant TabletsModel::data(const QModelIndex &index, int role) const
+{
+    if (!checkIndex(index, CheckIndexOption::IndexIsValid) || index.column() != 0) {
+        return {};
+    }
+
+    switch (role) {
+    case Qt::DisplayRole:
+        auto db = libwacom_database_new();
+
+        const QString sysPath = QStringLiteral("/dev/input/%1").arg(m_devices.at(index.row()).penDevice->sysName());
+
+        WacomError *error = libwacom_error_new();
+        auto device = libwacom_new_from_path(db, sysPath.toLatin1().constData(), WFALLBACK_NONE, error);
+
+        if (device) {
+            return libwacom_get_name(device);
+        } else {
+            return QString(m_devices.at(index.row()).padDevice->name() + QLatin1String(" and ") + m_devices.at(index.row()).penDevice->name());
+        }
+    }
+    return {};
+}
+
+int TabletsModel::rowCount(const QModelIndex &parent) const
+{
+    return parent.isValid() ? 0 : m_devices.size();
+}
+
+void TabletsModel::onDeviceAdded(const QString &sysName)
+{
+    if (std::any_of(m_devices.cbegin(), m_devices.cend(), [sysName](auto &t) {
+            return t.penDevice->sysName() == sysName || t.padDevice->sysName() == sysName;
+        })) {
+        return;
+    }
+
+    addDevice(sysName, true);
+}
+
+void TabletsModel::addDevice(const QString &sysName, bool tellModel)
+{
+    QDBusInterface deviceIface(QStringLiteral("org.kde.KWin"),
+                               QStringLiteral("/org/kde/KWin/InputDevice/") + sysName,
+                               QStringLiteral("org.kde.KWin.InputDevice"),
+                               QDBusConnection::sessionBus(),
+                               this);
+    QVariant reply = deviceIface.property("tabletTool");
+    QVariant reply2 = deviceIface.property("tabletPad");
+    if (reply.isValid() && (reply.toBool() || reply2.toBool())) {
+        auto dev = std::make_unique<InputDevice>(sysName, this);
+        connect(dev.get(), &InputDevice::needsSaveChanged, this, &TabletsModel::needsSaveChanged);
+
+        qDebug().nospace() << "Device connected: " << dev->name() << " (" << dev->sysName() << ")";
+
+        QString deviceGroup = dev->deviceGroup();
+
+        auto it = std::find_if(m_devices.begin(), m_devices.end(), [deviceGroup](const TabletDevice &device) {
+            return device.deviceGroup == deviceGroup;
+        });
+
+        if (it != m_devices.end()) {
+            if (dev->tabletTool()) {
+                it->penDevice = std::move(dev);
+            } else if (dev->tabletPad()) {
+                it->padDevice = std::move(dev);
+            }
+        } else {
+            TabletDevice tablet;
+            tablet.deviceGroup = deviceGroup;
+
+            if (dev->tabletTool()) {
+                tablet.penDevice = std::move(dev);
+            } else if (dev->tabletPad()) {
+                tablet.padDevice = std::move(dev);
+            }
+
+            if (tellModel) {
+                beginInsertRows({}, m_devices.size(), m_devices.size());
+            }
+            m_devices.push_back(std::move(tablet));
+            if (tellModel) {
+                endInsertRows();
+            }
+        }
+    }
+}
+
+void TabletsModel::onDeviceRemoved(const QString &sysName)
+{
+    auto it = std::find_if(m_devices.cbegin(), m_devices.cend(), [sysName](auto &t) {
+        return t.penDevice->sysName() == sysName || t.padDevice->sysName() == sysName;
+    });
+    if (it == m_devices.cend()) {
+        return;
+    }
+
+    int index = std::distance(m_devices.cbegin(), it);
+
+    beginRemoveRows({}, index, index);
+    m_devices.erase(it);
+    endRemoveRows();
+}
+
+QHash<int, QByteArray> TabletsModel::roleNames() const
+{
+    return {
+        {Qt::DisplayRole, "display"},
+    };
+}

kcms/tablet/tabletsmodel.h
@@ -1,0 +1,55 @@
+/*
+    SPDX-FileCopyrightText: 2021 Aleix Pol Gonzalez <aleixpol@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QAbstractListModel>
+
+#include <memory>
+#include <vector>
+
+#include "inputdevice.h"
+
+class QDBusInterface;
+// class InputDevice;
+
+struct TabletDevice {
+    QString deviceGroup;
+    std::unique_ptr<InputDevice> padDevice;
+    std::unique_ptr<InputDevice> penDevice;
+};
+
+class TabletsModel : public QAbstractListModel
+{
+    Q_OBJECT
+public:
+    explicit TabletsModel(QObject *parent = nullptr);
+
+    QHash<int, QByteArray> roleNames() const override;
+    QVariant data(const QModelIndex &index, int role) const override;
+    int rowCount(const QModelIndex &parent) const override;
+    // Q_SCRIPTABLE InputDevice *deviceAt(int row) const;
+
+    // void load();
+    // void save();
+    // void defaults();
+    // bool isSaveNeeded() const;
+    // bool isDefaults() const;
+
+private Q_SLOTS:
+    void onDeviceAdded(const QString &sysName);
+    void onDeviceRemoved(const QString &sysName);
+
+Q_SIGNALS:
+    void needsSaveChanged();
+
+private:
+    void addDevice(const QString &sysname, bool tellModel);
+    void resetModel();
+
+    std::vector<TabletDevice> m_devices;
+    QDBusInterface *m_deviceManager;
+};