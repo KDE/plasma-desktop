3548c2a3935c4a94fc7903e6eb36c3e26e283b3b
Marco Martin <notmart@gmail.com>
2025-01-23 13:53:25 +0100

    remove debugs

--- containments/desktop/plugins/folder/shortcut.cpp
+++ containments/desktop/plugins/folder/shortcut.cpp
@@ -59,12 +59,10 @@ bool ShortCut::eventFilter(QObject *obj, QEvent *e)
             return true;
         }
         if (KStandardShortcut::copy().contains(QKeySequence(keyInt))) {
-            qWarning() << "COPY";
             Q_EMIT copyFile();
             return true;
         }
         if (KStandardShortcut::paste().contains(QKeySequence(keyInt))) {
-            qWarning() << "PASTE";
             Q_EMIT pasteFile();
             return true;
         }
