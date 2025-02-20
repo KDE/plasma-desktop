KDE/plasma-desktop

commit 70f5b7668ff75cd9f9c7d4fd8f0725176d7e0e44

applets/kicker: focus correct item on arrow key
When pressing arrow down from the search field, kicker moves focus to
the second entry in the menu/results list. This makes sense when the
first item is selected, as it is e.g. when searching. But the user can
manually remove that selection (e.g. by pressing up > down first), and
then arrow key down skips the first list entry.

Instead, select currentIndex+1 on arrow down from the search field.

applets/kicker/package/contents/ui/MenuRepresentation.qml
@@ -379,7 +379,7 @@ FocusScope {
             } else if (event.key === Qt.Key_Down) {
                 if (rootList.visible) {
                     rootList.showChildDialogs = false;
-                    rootList.currentIndex = Math.min(1, rootList.count);
+                    rootList.currentIndex = Math.min(rootList.currentIndex + 1, rootList.count);
                     rootList.forceActiveFocus();
                     rootList.showChildDialogs = true;
                 }
@@ -388,8 +388,8 @@ FocusScope {
                     for (let i = 0; i < runnerModel.count; ++i) {
                         if (runnerModel.modelForRow(i).count) {
                             const targetList = runnerColumnsRepeater.itemAt(i);
-                            targetList.currentIndex = Math.min(1, targetList.count);
-                            targetList.currentItem.forceActiveFocus();
+                            targetList.currentIndex = Math.min(targetList.currentIndex + 1, targetList.count);
+                            targetList.forceActiveFocus();
                             break;
                         }
                     }

commit 4fadac0f3ea12c3ce05cc2da50c9fb1265858db1

applets/kicker: focus the delegate in all cases
70f5b766 accidentally reverted the fix to focusing the delegate instead
of the column for the down arrow, leaving it only for the up arrow as
originally introduced in 8d10e42d.

This change adds it back. This is also a good chance to apply the fix to
left and right arrow keys, to make sure focusing items in other columns
also works immediately and to keep things consistent.

CCBUG: 499971

applets/kicker/package/contents/ui/MenuRepresentation.qml
@@ -389,7 +389,7 @@ FocusScope {
                         if (runnerModel.modelForRow(i).count) {
                             const targetList = runnerColumnsRepeater.itemAt(i);
                             targetList.currentIndex = Math.min(targetList.currentIndex + 1, targetList.count);
-                            targetList.forceActiveFocus();
+                            targetList.currentItem.forceActiveFocus();
                             break;
                         }
                     }
@@ -399,7 +399,7 @@ FocusScope {
                     if (runnerModel.modelForRow(i).count) {
                         const targetList = runnerColumnsRepeater.itemAt(i);
                         targetList.currentIndex = 0;
-                        targetList.forceActiveFocus();
+                        targetList.currentItem.forceActiveFocus();
                         break;
                     }
                 }
@@ -408,7 +408,7 @@ FocusScope {
                     if (runnerModel.modelForRow(i).count) {
                         const targetList = runnerColumnsRepeater.itemAt(i);
                         targetList.currentIndex = 0;
-                        targetList.forceActiveFocus();
+                        targetList.currentItem.forceActiveFocus();
                         break;
                     }
                 }