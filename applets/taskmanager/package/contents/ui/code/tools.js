KDE/plasma-desktop

commit 4af8e2c3d4165bfeac8e345223ee0ff8d0fbd950

Add new scroll mode to cycle through windows of a single task
New take on https://invent.kde.org/plasma/plasma-desktop/-/merge_requests/2434

Currently, there are two options for scrolling behavior: disable it entirely,
or scroll through all tasks. This commit adds a third option: only scroll
through the windows of the task under the cursor. When this mode is selected:

- If the task has multiple windows, scrolling will cycle only through those windows
- If the task has a single window, scrolling will simply raise it
- If the task is a launcher with no open windows, scrolling will have no effect

BUG: 461481
FIXED-IN: 6.4

applets/taskmanager/package/contents/ui/code/tools.js
@@ -13,9 +13,31 @@
 // Can't be `let`, or else QML counterpart won't be able to assign to it.
 var taskManagerInstanceCount = 0;
 
-function activateNextPrevTask(anchor, next, wheelSkipMinimized, tasks) {
+function activateNextPrevTask(anchor, next, wheelSkipMinimized, wheelEnabled, tasks) {
     // FIXME TODO: Unnecessarily convoluted and costly; optimize.
 
+    //Get the number of open windows for the application
+    const anchorModelIndex = anchor.modelIndex(anchor.index);
+    const winIdList = tasks.tasksModel.data(anchorModelIndex, TaskManager.AbstractTasksModel.WinIdList);
+    const windowCount = winIdList ? winIdList.length : 0;
+
+    if (wheelEnabled === 2) { // TaskOnly
+        // No windows
+        if (windowCount === 0) {
+            return;
+        }
+
+        // Single window
+        if (windowCount === 1) {
+            const isHidden = tasks.tasksModel.data(anchorModelIndex, TaskManager.AbstractTasksModel.IsHidden);
+            if (!wheelSkipMinimized || !isHidden) {
+                tasks.tasksModel.requestActivate(anchorModelIndex);
+            }
+            return;
+        }
+    }
+
+    // Multiple windows
     let taskIndexList = [];
     const activeTaskIndex = tasks.tasksModel.activeTask;
 
@@ -25,7 +47,7 @@ function activateNextPrevTask(anchor, next, wheelSkipMinimized, tasks) {
 
         if (!task.model.IsLauncher && !task.model.IsStartup) {
             if (task.model.IsGroupParent) {
-                if (task === anchor) { // If the anchor is a group parent, collect only windows within the group.
+                if (wheelEnabled === 2 && task === anchor) { // If TaskOnly mode and the anchor is a group parent, collect only windows within the group.
                     taskIndexList = [];
                 }
 
@@ -37,7 +59,7 @@ function activateNextPrevTask(anchor, next, wheelSkipMinimized, tasks) {
                     }
                 }
 
-                if (task === anchor) { // See above.
+                if (wheelEnabled === 2 && task === anchor) { // If TaskOnly mode, break after processing the anchor group.
                     break;
                 }
             } else {

applets/taskmanager/package/contents/config/main.xml
@@ -84,9 +84,14 @@
       </choices>
       <default>1</default>
     </entry>
-    <entry name="wheelEnabled" type="Bool">
+    <entry name="wheelEnabled" type="Enum">
       <label>Whether using the mouse wheel with the mouse pointer above the widget should switch between tasks.</label>
-      <default>false</default>
+      <choices>
+        <choice name="None"/>
+        <choice name="AllTask"/>
+        <choice name="TaskOnly"/>
+      </choices>
+      <default>0</default>
     </entry>
     <entry name="wheelSkipMinimized" type="Bool">
       <label>Whether to skip minimized tasks when switching between them using the mouse wheel.</label>

applets/taskmanager/package/contents/ui/ConfigBehavior.qml
@@ -25,7 +25,7 @@ KCMUtils.SimpleKCM {
     property alias cfg_separateLaunchers: separateLaunchers.checked
     property alias cfg_hideLauncherOnStart: hideLauncherOnStart.checked
     property alias cfg_middleClickAction: middleClickAction.currentIndex
-    property alias cfg_wheelEnabled: wheelEnabled.checked
+    property alias cfg_wheelEnabled: wheelEnabled.currentIndex
     property alias cfg_wheelSkipMinimized: wheelSkipMinimized.checked
     property alias cfg_showOnlyCurrentScreen: showOnlyCurrentScreen.checked
     property alias cfg_showOnlyCurrentDesktop: showOnlyCurrentDesktop.checked
@@ -200,10 +200,16 @@ KCMUtils.SimpleKCM {
             Kirigami.FormData.isSection: true
         }
 
-        QQC2.CheckBox {
+        QQC2.ComboBox {
             id: wheelEnabled
-            Kirigami.FormData.label: i18nc("@label for checkbox Part of a sentence: 'Mouse wheel cycles through tasks'", "Mouse wheel:")
-            text: i18nc("@option:check Part of a sentence: 'Mouse wheel cycles through tasks'", "Cycles through tasks")
+            Kirigami.FormData.label: i18nc("@label:listbox completes sentence like: … does nothing", "Scrolling on a task:")
+            Layout.fillWidth: true
+            Layout.minimumWidth: Kirigami.Units.gridUnit * 14
+            model: [
+                i18nc("@item:inlistbox Part of a sentence: 'Scrolling on any task does nothing'", "Does nothing"),
+                i18nc("@item:inlistbox Part of a sentence: 'Scrolling on any task cycles through all tasks'", "Cycles through all tasks"),
+                i18nc("@item:inlistbox Part of a sentence: 'Scrolling on any task cycles through that task's windows only'", "Cycles through that task's windows"),
+            ]
         }
 
         QQC2.CheckBox {
@@ -211,7 +217,7 @@ KCMUtils.SimpleKCM {
             leftPadding: mirrored ? 0 : (wheelEnabled.indicator.width + wheelEnabled.spacing)
             rightPadding: mirrored ? (wheelEnabled.indicator.width + wheelEnabled.spacing) : 0
             text: i18nc("@option:check mouse wheel task cycling", "Skip minimized tasks")
-            enabled: wheelEnabled.checked
+            enabled: wheelEnabled.currentIndex !== 0 // None
         }
 
         Item {

applets/taskmanager/package/contents/ui/MouseHandler.qml
@@ -164,7 +164,7 @@ DropArea {
 
         property bool handleWheelEvents: true
 
-        enabled: handleWheelEvents && Plasmoid.configuration.wheelEnabled
+        enabled: handleWheelEvents && Plasmoid.configuration.wheelEnabled !== 0
 
         onWheel: event => {
             // magic number 15 for common "one scroll"
@@ -180,7 +180,7 @@ DropArea {
             }
             const anchor = dropArea.target.childAt(event.x, event.y);
             while (increment !== 0) {
-                TaskTools.activateNextPrevTask(anchor, increment < 0, Plasmoid.configuration.wheelSkipMinimized, tasks);
+                TaskTools.activateNextPrevTask(anchor, increment < 0, Plasmoid.configuration.wheelSkipMinimized, Plasmoid.configuration.wheelEnabled, tasks);
                 increment += (increment < 0) ? 1 : -1;
             }
         }

desktoppackage/contents/updates/taskmanager_configUpdate_wheelEnabled.js
@@ -1,0 +1,17 @@
+// Update all applets with wheelEnabled=true to wheelEnabled=AllTask
+
+var containments = desktops().concat(panels());
+
+containments.forEach(function(cont) {
+
+    cont.widgetIds.forEach(function(id) {
+        var widget = cont.widgetById(id);
+
+        widget.currentConfigGroup = new Array("General");
+
+        if (widget.readConfig("wheelEnabled", false) === true) {
+            widget.writeConfig("wheelEnabled", "AllTask");
+            widget.reloadConfig();
+        }
+    });
+});

commit f04c3ddb26d63efa93f8f014c42c717ece4b926d

🍒 taskmanager: fix a regression from !2932
Amends 4af8e2c3d4165bfeac8e345223ee0ff8d0fbd950, which broke scrolling
through all tasks when scrolling on an empty area of the panel because
of `anchor` being undefined.

This commit moves those variables to inside the if statement that uses
them, and guards against anchor being undefined when scrolling on an
empty area so it doesn't spam warnings.

applets/taskmanager/package/contents/ui/code/tools.js
@@ -16,12 +16,13 @@ var taskManagerInstanceCount = 0;
 function activateNextPrevTask(anchor, next, wheelSkipMinimized, wheelEnabled, tasks) {
     // FIXME TODO: Unnecessarily convoluted and costly; optimize.
 
-    //Get the number of open windows for the application
-    const anchorModelIndex = anchor.modelIndex(anchor.index);
-    const winIdList = tasks.tasksModel.data(anchorModelIndex, TaskManager.AbstractTasksModel.WinIdList);
-    const windowCount = winIdList ? winIdList.length : 0;
-
     if (wheelEnabled === 2) { // TaskOnly
+        // anchor can be undefined if scrolling on an empty area
+        if (!anchor) return;
+        // Get the number of open windows for the application
+        const anchorModelIndex = anchor.modelIndex(anchor.index);
+        const winIdList = tasks.tasksModel.data(anchorModelIndex, TaskManager.AbstractTasksModel.WinIdList);
+        const windowCount = winIdList ? winIdList.length : 0;
         // No windows
         if (windowCount === 0) {
             return;