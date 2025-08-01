Sonic-DE/sonic-desktop-interface

kcms/landingpage: Add automatic global theme switch
This adds the corresponding setting for the new automatic global theme
mode.

With the automatic mode, the light global theme will be used during day.
The dark global theme will be used at night.

The user may want to quickly change the light or the dark global theme,
so the corresponding UI has been reworked too.

Co-authored-by: Nate Graham <nate@kde.org>

kcms/landingpage/landingpage.cpp
@@ -7,6 +7,9 @@
 */
 
 #include "landingpage.h"
+#include "lookandfeelcrawler.h"
+#include "lookandfeelmetadata.h"
+#include "splititem.h"
 
 #include <KLocalizedString>
 #include <KPackage/PackageLoader>
@@ -151,45 +154,21 @@ QVariant MostUsedModel::data(const QModelIndex &index, int role) const
     }
 }
 
-LookAndFeelGroup::LookAndFeelGroup(QObject *parent)
-    : QObject(parent)
-    , m_package(KPackage::PackageLoader::self()->loadPackage(QStringLiteral("Plasma/LookAndFeel")))
-{
-}
-
-QString LookAndFeelGroup::id() const
-{
-    return m_package.metadata().pluginId();
-}
-
-QString LookAndFeelGroup::name() const
-{
-    return m_package.metadata().name();
-}
-
-QUrl LookAndFeelGroup::thumbnail() const
-{
-    return m_package.fileUrl("preview");
-}
-
 KCMLandingPage::KCMLandingPage(QObject *parent, const KPluginMetaData &metaData)
     : KQuickManagedConfigModule(parent, metaData)
     , m_data(new LandingPageData(this))
 {
     qmlRegisterAnonymousType<LandingPageGlobalsSettings>("org.kde.plasma.landingpage.kcm", 0);
     qmlRegisterAnonymousType<MostUsedModel>("org.kde.plasma.landingpage.kcm", 0);
-    qmlRegisterAnonymousType<LookAndFeelGroup>("org.kde.plasma.landingpage.kcm", 0);
+    qmlRegisterType<SplitItem>("org.kde.plasma.landingpage.kcm", 1, 0, "SplitView");
+    qmlRegisterUncreatableMetaObject(KLookAndFeel::staticMetaObject, "org.kde.plasma.landingpage.kcm", 1, 0, "LookAndFeel", "for Variant enum");
+    qmlRegisterType<LookAndFeelCrawler>("org.kde.plasma.landingpage.kcm", 1, 0, "LookAndFeelCrawler");
+    qmlRegisterType<LookAndFeelMetaData>("org.kde.plasma.landingpage.kcm", 1, 0, "LookAndFeelMetaData");
 
     setButtons(Apply);
 
     m_mostUsedModel = new MostUsedModel(this);
     m_mostUsedModel->setResultModel(new ResultModel(AllResources | Agent(QStringLiteral("org.kde.systemsettings")) | HighScoredFirst | Limit(12), this));
-
-    m_defaultLightLookAndFeel = new LookAndFeelGroup(this);
-    m_defaultDarkLookAndFeel = new LookAndFeelGroup(this);
-
-    m_defaultLightLookAndFeel->m_package.setPath(globalsSettings()->defaultLightLookAndFeel());
-    m_defaultDarkLookAndFeel->m_package.setPath(globalsSettings()->defaultDarkLookAndFeel());
 }
 
 inline void swap(QJsonValueRef v1, QJsonValueRef v2)
@@ -211,7 +190,10 @@ LandingPageGlobalsSettings *KCMLandingPage::globalsSettings() const
 
 void KCMLandingPage::save()
 {
-    const bool applyNewLnf = globalsSettings()->findItem(QStringLiteral("lookAndFeelPackage"))->isSaveNeeded();
+    const auto lookAndFeelPackageItem = globalsSettings()->findItem(QStringLiteral("lookAndFeelPackage"));
+    const auto automaticLookAndFeelItem = globalsSettings()->findItem(QStringLiteral("automaticLookAndFeel"));
+    const bool automaticLookAndFeel = automaticLookAndFeelItem->property().toBool();
+    const bool applyNewLnf = !automaticLookAndFeel && (lookAndFeelPackageItem->isSaveNeeded() || automaticLookAndFeelItem->isSaveNeeded());
 
     KQuickManagedConfigModule::save();
 
@@ -227,14 +209,9 @@ void KCMLandingPage::save()
     }
 }
 
-LookAndFeelGroup *KCMLandingPage::defaultLightLookAndFeel() const
+QString KCMLandingPage::defaultLookAndFeelPackage() const
 {
-    return m_defaultLightLookAndFeel;
-}
-
-LookAndFeelGroup *KCMLandingPage::defaultDarkLookAndFeel() const
-{
-    return m_defaultDarkLookAndFeel;
+    return m_data->settings()->defaultLookAndFeelPackageValue();
 }
 
 Q_INVOKABLE void KCMLandingPage::openKCM(const QString &kcm)

kcms/landingpage/ui/main.qml
@@ -15,6 +15,8 @@ import QtQuick.Layouts
 import org.kde.kirigami as Kirigami
 import org.kde.kcmutils as KCMUtils
 
+import org.kde.plasma.landingpage.kcm
+
 KCMUtils.SimpleKCM {
     id: root
 
@@ -33,35 +35,93 @@ KCMUtils.SimpleKCM {
             Kirigami.FormData.label: i18n("Theme:")
 
             Layout.alignment: Qt.AlignCenter
-            spacing: Kirigami.Units.gridUnit * 2
+            spacing: Kirigami.Units.largeSpacing
 
             QQC2.ButtonGroup { id: themeGroup }
 
-            Thumbnail {
-                imageSource: kcm.defaultLightLookAndFeel.thumbnail
-                text: kcm.defaultLightLookAndFeel.name
-                checked: kcm.globalsSettings.lookAndFeelPackage === kcm.defaultLightLookAndFeel.id
-                QQC2.ButtonGroup.group: themeGroup
+            LookAndFeelBox {
+                id: lightLookAndFeelBox
+                packageId: kcm.globalsSettings.defaultLightLookAndFeel
+                checked: !kcm.globalsSettings.automaticLookAndFeel && kcm.globalsSettings.lookAndFeelPackage === kcm.globalsSettings.defaultLightLookAndFeel
+                group: themeGroup
 
-                onToggled: kcm.globalsSettings.lookAndFeelPackage = kcm.defaultLightLookAndFeel.id
+                availablePackages: LookAndFeelCrawler {
+                    variant: LookAndFeel.Variant.Light
+                }
 
-                KCMUtils.SettingStateBinding {
-                    configObject: kcm.globalsSettings
-                    settingName: "lookAndFeelPackage"
+                onToggled: {
+                    kcm.globalsSettings.automaticLookAndFeel = false;
+                    kcm.globalsSettings.lookAndFeelPackage = kcm.globalsSettings.defaultLightLookAndFeel;
+                }
+
+                onAccepted: (lnfId) => {
+                    kcm.globalsSettings.defaultLightLookAndFeel = lnfId;
+                }
+
+                KCMUtils.SettingHighlighter {
+                    highlight: kcm.globalsSettings.automaticLookAndFeel || kcm.globalsSettings.lookAndFeelPackage != kcm.defaultLookAndFeelPackage
                 }
             }
+
+            LookAndFeelBox {
+                id: darkLookAndFeelBox
+                packageId: kcm.globalsSettings.defaultDarkLookAndFeel
+                checked: !kcm.globalsSettings.automaticLookAndFeel && kcm.globalsSettings.lookAndFeelPackage === kcm.globalsSettings.defaultDarkLookAndFeel
+                group: themeGroup
+
+                availablePackages: LookAndFeelCrawler {
+                    variant: LookAndFeel.Variant.Dark
+                }
+
+                onToggled: {
+                    kcm.globalsSettings.automaticLookAndFeel = false;
+                    kcm.globalsSettings.lookAndFeelPackage = kcm.globalsSettings.defaultDarkLookAndFeel;
+                }
+
+                onAccepted: (lnfId) => {
+                    kcm.globalsSettings.defaultDarkLookAndFeel = lnfId;
+                }
+
+                KCMUtils.SettingHighlighter {
+                    highlight: kcm.globalsSettings.automaticLookAndFeel || kcm.globalsSettings.lookAndFeelPackage != kcm.defaultLookAndFeelPackage
+                }
+            }
+
             Thumbnail {
-                imageSource: kcm.defaultDarkLookAndFeel.thumbnail
-                text: kcm.defaultDarkLookAndFeel.name
-                checked: kcm.globalsSettings.lookAndFeelPackage === kcm.defaultDarkLookAndFeel.id
+                text: i18nc("Switch between dark and light look and feel packages automatically", "Automatic")
+                checked: kcm.globalsSettings.automaticLookAndFeel
+                QQC2.ToolTip.text: i18nc("@info:tooltip 1 is the name of a light global theme, 2 is the name of a dark global theme", 'Use “%1” during the day and “%2” at night', lightLookAndFeelBox.text, darkLookAndFeelBox.text)
+                QQC2.ToolTip.visible: autoLookAndFeelHoverHandler.hovered
+                QQC2.ToolTip.delay: Kirigami.Units.toolTipDelay
                 QQC2.ButtonGroup.group: themeGroup
 
-                onToggled: kcm.globalsSettings.lookAndFeelPackage = kcm.defaultDarkLookAndFeel.id
+                preview: SplitView {
+                    anchors.fill: parent
+                    first: lightLookAndFeelBox.preview
+                    second: darkLookAndFeelBox.preview
+                    shutter: 0.15
 
-                KCMUtils.SettingStateBinding {
-                    configObject: kcm.globalsSettings
-                    settingName: "lookAndFeelPackage"
+                    Kirigami.Icon {
+                        anchors.right: parent.right
+                        anchors.top: parent.top
+                        anchors.margins: Kirigami.Units.smallSpacing
+                        source: "lighttable"
+                        isMask: true
+                        color: "white"
+                        width: Kirigami.Units.iconSizes.smallMedium
+                        height: width
+                    }
                 }
+
+                onToggled: {
+                    kcm.globalsSettings.automaticLookAndFeel = true;
+                }
+
+                KCMUtils.SettingHighlighter {
+                    highlight: kcm.globalsSettings.automaticLookAndFeel || kcm.globalsSettings.lookAndFeelPackage != kcm.defaultLookAndFeelPackage
+                }
+
+                HoverHandler { id: autoLookAndFeelHoverHandler }
             }
         }
 

KDE/plasma-desktop

kcms/landingpage/landingpage.cpp
@@ -7,6 +7,9 @@
 */
 
 #include "landingpage.h"
+#include "lookandfeelmetadata.h"
+#include "lookandfeelmodel.h"
+#include "splititem.h"
 
 #include <KLocalizedString>
 #include <KPackage/PackageLoader>
@@ -151,45 +154,21 @@ QVariant MostUsedModel::data(const QModelIndex &index, int role) const
     }
 }
 
-LookAndFeelGroup::LookAndFeelGroup(QObject *parent)
-    : QObject(parent)
-    , m_package(KPackage::PackageLoader::self()->loadPackage(QStringLiteral("Plasma/LookAndFeel")))
-{
-}
-
-QString LookAndFeelGroup::id() const
-{
-    return m_package.metadata().pluginId();
-}
-
-QString LookAndFeelGroup::name() const
-{
-    return m_package.metadata().name();
-}
-
-QUrl LookAndFeelGroup::thumbnail() const
-{
-    return m_package.fileUrl("preview");
-}
-
 KCMLandingPage::KCMLandingPage(QObject *parent, const KPluginMetaData &metaData)
     : KQuickManagedConfigModule(parent, metaData)
     , m_data(new LandingPageData(this))
 {
     qmlRegisterAnonymousType<LandingPageGlobalsSettings>("org.kde.plasma.landingpage.kcm", 0);
     qmlRegisterAnonymousType<MostUsedModel>("org.kde.plasma.landingpage.kcm", 0);
-    qmlRegisterAnonymousType<LookAndFeelGroup>("org.kde.plasma.landingpage.kcm", 0);
+    qmlRegisterType<SplitItem>("org.kde.plasma.landingpage.kcm", 1, 0, "SplitView");
+    qmlRegisterUncreatableMetaObject(KLookAndFeel::staticMetaObject, "org.kde.plasma.landingpage.kcm", 1, 0, "LookAndFeel", "for Variant enum");
+    qmlRegisterType<LookAndFeelModel>("org.kde.plasma.landingpage.kcm", 1, 0, "LookAndFeelModel");
+    qmlRegisterType<LookAndFeelMetaData>("org.kde.plasma.landingpage.kcm", 1, 0, "LookAndFeelMetaData");
 
     setButtons(Apply);
 
     m_mostUsedModel = new MostUsedModel(this);
     m_mostUsedModel->setResultModel(new ResultModel(AllResources | Agent(QStringLiteral("org.kde.systemsettings")) | HighScoredFirst | Limit(12), this));
-
-    m_defaultLightLookAndFeel = new LookAndFeelGroup(this);
-    m_defaultDarkLookAndFeel = new LookAndFeelGroup(this);
-
-    m_defaultLightLookAndFeel->m_package.setPath(globalsSettings()->defaultLightLookAndFeel());
-    m_defaultDarkLookAndFeel->m_package.setPath(globalsSettings()->defaultDarkLookAndFeel());
 }
 
 inline void swap(QJsonValueRef v1, QJsonValueRef v2)
@@ -211,7 +190,10 @@ LandingPageGlobalsSettings *KCMLandingPage::globalsSettings() const
 
 void KCMLandingPage::save()
 {
-    const bool applyNewLnf = globalsSettings()->findItem(QStringLiteral("lookAndFeelPackage"))->isSaveNeeded();
+    const auto lookAndFeelPackageItem = globalsSettings()->findItem(QStringLiteral("lookAndFeelPackage"));
+    const auto automaticLookAndFeelItem = globalsSettings()->findItem(QStringLiteral("automaticLookAndFeel"));
+    const bool automaticLookAndFeel = automaticLookAndFeelItem->property().toBool();
+    const bool applyNewLnf = !automaticLookAndFeel && (lookAndFeelPackageItem->isSaveNeeded() || automaticLookAndFeelItem->isSaveNeeded());
 
     KQuickManagedConfigModule::save();
 
@@ -227,14 +209,9 @@ void KCMLandingPage::save()
     }
 }
 
-LookAndFeelGroup *KCMLandingPage::defaultLightLookAndFeel() const
+QString KCMLandingPage::defaultLookAndFeelPackage() const
 {
-    return m_defaultLightLookAndFeel;
-}
-
-LookAndFeelGroup *KCMLandingPage::defaultDarkLookAndFeel() const
-{
-    return m_defaultDarkLookAndFeel;
+    return m_data->settings()->defaultLookAndFeelPackageValue();
 }
 
 Q_INVOKABLE void KCMLandingPage::openKCM(const QString &kcm)

kcms/landingpage/ui/main.qml
@@ -15,6 +15,8 @@ import QtQuick.Layouts
 import org.kde.kirigami as Kirigami
 import org.kde.kcmutils as KCMUtils
 
+import org.kde.plasma.landingpage.kcm
+
 KCMUtils.SimpleKCM {
     id: root
 
@@ -33,35 +35,93 @@ KCMUtils.SimpleKCM {
             Kirigami.FormData.label: i18n("Theme:")
 
             Layout.alignment: Qt.AlignCenter
-            spacing: Kirigami.Units.gridUnit * 2
+            spacing: Kirigami.Units.largeSpacing
 
             QQC2.ButtonGroup { id: themeGroup }
 
-            Thumbnail {
-                imageSource: kcm.defaultLightLookAndFeel.thumbnail
-                text: kcm.defaultLightLookAndFeel.name
-                checked: kcm.globalsSettings.lookAndFeelPackage === kcm.defaultLightLookAndFeel.id
-                QQC2.ButtonGroup.group: themeGroup
+            LookAndFeelBox {
+                id: lightLookAndFeelBox
+                packageId: kcm.globalsSettings.defaultLightLookAndFeel
+                checked: !kcm.globalsSettings.automaticLookAndFeel && kcm.globalsSettings.lookAndFeelPackage === kcm.globalsSettings.defaultLightLookAndFeel
+                group: themeGroup
 
-                onToggled: kcm.globalsSettings.lookAndFeelPackage = kcm.defaultLightLookAndFeel.id
+                availablePackages: LookAndFeelModel {
+                    variant: LookAndFeel.Variant.Light
+                }
 
-                KCMUtils.SettingStateBinding {
-                    configObject: kcm.globalsSettings
-                    settingName: "lookAndFeelPackage"
+                onToggled: {
+                    kcm.globalsSettings.automaticLookAndFeel = false;
+                    kcm.globalsSettings.lookAndFeelPackage = kcm.globalsSettings.defaultLightLookAndFeel;
+                }
+
+                onAccepted: (lnfId) => {
+                    kcm.globalsSettings.defaultLightLookAndFeel = lnfId;
+                }
+
+                KCMUtils.SettingHighlighter {
+                    highlight: kcm.globalsSettings.automaticLookAndFeel || kcm.globalsSettings.lookAndFeelPackage != kcm.defaultLookAndFeelPackage
                 }
             }
+
+            LookAndFeelBox {
+                id: darkLookAndFeelBox
+                packageId: kcm.globalsSettings.defaultDarkLookAndFeel
+                checked: !kcm.globalsSettings.automaticLookAndFeel && kcm.globalsSettings.lookAndFeelPackage === kcm.globalsSettings.defaultDarkLookAndFeel
+                group: themeGroup
+
+                availablePackages: LookAndFeelModel {
+                    variant: LookAndFeel.Variant.Dark
+                }
+
+                onToggled: {
+                    kcm.globalsSettings.automaticLookAndFeel = false;
+                    kcm.globalsSettings.lookAndFeelPackage = kcm.globalsSettings.defaultDarkLookAndFeel;
+                }
+
+                onAccepted: (lnfId) => {
+                    kcm.globalsSettings.defaultDarkLookAndFeel = lnfId;
+                }
+
+                KCMUtils.SettingHighlighter {
+                    highlight: kcm.globalsSettings.automaticLookAndFeel || kcm.globalsSettings.lookAndFeelPackage != kcm.defaultLookAndFeelPackage
+                }
+            }
+
             Thumbnail {
-                imageSource: kcm.defaultDarkLookAndFeel.thumbnail
-                text: kcm.defaultDarkLookAndFeel.name
-                checked: kcm.globalsSettings.lookAndFeelPackage === kcm.defaultDarkLookAndFeel.id
+                text: i18nc("Switch between dark and light look and feel packages automatically", "Automatic")
+                checked: kcm.globalsSettings.automaticLookAndFeel
+                QQC2.ToolTip.text: i18nc("@info:tooltip 1 is the name of a light global theme, 2 is the name of a dark global theme", 'Use “%1” during the day and “%2” at night', lightLookAndFeelBox.text, darkLookAndFeelBox.text)
+                QQC2.ToolTip.visible: autoLookAndFeelHoverHandler.hovered
+                QQC2.ToolTip.delay: Kirigami.Units.toolTipDelay
                 QQC2.ButtonGroup.group: themeGroup
 
-                onToggled: kcm.globalsSettings.lookAndFeelPackage = kcm.defaultDarkLookAndFeel.id
+                preview: SplitView {
+                    anchors.fill: parent
+                    first: lightLookAndFeelBox.preview
+                    second: darkLookAndFeelBox.preview
+                    shutter: 0.15
 
-                KCMUtils.SettingStateBinding {
-                    configObject: kcm.globalsSettings
-                    settingName: "lookAndFeelPackage"
+                    Kirigami.Icon {
+                        anchors.right: parent.right
+                        anchors.top: parent.top
+                        anchors.margins: Kirigami.Units.smallSpacing
+                        source: "lighttable"
+                        isMask: true
+                        color: "white"
+                        width: Kirigami.Units.iconSizes.smallMedium
+                        height: width
+                    }
                 }
+
+                onToggled: {
+                    kcm.globalsSettings.automaticLookAndFeel = true;
+                }
+
+                KCMUtils.SettingHighlighter {
+                    highlight: kcm.globalsSettings.automaticLookAndFeel || kcm.globalsSettings.lookAndFeelPackage != kcm.defaultLookAndFeelPackage
+                }
+
+                HoverHandler { id: autoLookAndFeelHoverHandler }
             }
         }
 