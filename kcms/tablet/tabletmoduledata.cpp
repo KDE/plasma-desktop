Sonic-DE/sonic-desktop-interface

kcms/tablet: Show KCM only when a graphics tablet has been found
Otherwise it takes up space in the settings list and all the KCM
will do is show "No graphics tablets".

kcms/tablet/tabletmoduledata.cpp
@@ -1,0 +1,40 @@
+/**
+ * SPDX-FileCopyrightText: 2025 Kai Uwe Broulik <kde@broulik.de>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#include "tabletmoduledata.h"
+
+#include "tabletsmodel.h"
+
+TabletModuleData::TabletModuleData(QObject *parent)
+    : KCModuleDataV2(parent)
+{
+    m_db = libwacom_database_new();
+    if (m_db) {
+        m_tabletsModel = new TabletsModel(m_db, this);
+        connect(m_tabletsModel, &QAbstractItemModel::rowsInserted, this, &TabletModuleData::updateRelevance);
+        connect(m_tabletsModel, &QAbstractItemModel::rowsRemoved, this, &TabletModuleData::updateRelevance);
+        connect(m_tabletsModel, &QAbstractItemModel::modelReset, this, &TabletModuleData::updateRelevance);
+    }
+    updateRelevance();
+}
+
+TabletModuleData::~TabletModuleData()
+{
+    if (m_db) {
+        libwacom_database_destroy(m_db);
+    }
+}
+
+void TabletModuleData::updateRelevance()
+{
+    bool relevant = false;
+    if (m_tabletsModel) {
+        relevant = m_tabletsModel->rowCount(QModelIndex()) > 0;
+    }
+    setRelevant(relevant);
+}
+
+#include "moc_tabletmoduledata.cpp"

kcms/tablet/tabletmoduledata.h
@@ -1,0 +1,30 @@
+/**
+ * SPDX-FileCopyrightText: 2025 Kai Uwe Broulik <kde@broulik.de>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#pragma once
+
+#include <KCModuleDataV2>
+
+extern "C" {
+#include <libwacom/libwacom.h>
+}
+
+class TabletsModel;
+
+class TabletModuleData : public KCModuleDataV2
+{
+    Q_OBJECT
+
+public:
+    TabletModuleData(QObject *parent = nullptr);
+    ~TabletModuleData() override;
+
+private:
+    void updateRelevance();
+
+    WacomDeviceDatabase *m_db = nullptr;
+    TabletsModel *m_tabletsModel = nullptr;
+};

KDE/plasma-desktop

kcms/tablet/tabletmoduledata.cpp
@@ -1,0 +1,40 @@
+/**
+ * SPDX-FileCopyrightText: 2025 Kai Uwe Broulik <kde@broulik.de>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#include "tabletmoduledata.h"
+
+#include "tabletsmodel.h"
+
+TabletModuleData::TabletModuleData(QObject *parent)
+    : KCModuleData(parent)
+{
+    m_db = libwacom_database_new();
+    if (m_db) {
+        m_tabletsModel = new TabletsModel(m_db, this);
+        connect(m_tabletsModel, &QAbstractItemModel::rowsInserted, this, &TabletModuleData::updateRelevance);
+        connect(m_tabletsModel, &QAbstractItemModel::rowsRemoved, this, &TabletModuleData::updateRelevance);
+        connect(m_tabletsModel, &QAbstractItemModel::modelReset, this, &TabletModuleData::updateRelevance);
+    }
+    updateRelevance();
+}
+
+TabletModuleData::~TabletModuleData()
+{
+    if (m_db) {
+        libwacom_database_destroy(m_db);
+    }
+}
+
+void TabletModuleData::updateRelevance()
+{
+    bool relevant = false;
+    if (m_tabletsModel) {
+        relevant = m_tabletsModel->rowCount(QModelIndex()) > 0;
+    }
+    setRelevant(relevant);
+}
+
+#include "moc_tabletmoduledata.cpp"

kcms/tablet/tabletmoduledata.h
@@ -1,0 +1,30 @@
+/**
+ * SPDX-FileCopyrightText: 2025 Kai Uwe Broulik <kde@broulik.de>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#pragma once
+
+#include <KCModuleData>
+
+extern "C" {
+#include <libwacom/libwacom.h>
+}
+
+class TabletsModel;
+
+class TabletModuleData : public KCModuleData
+{
+    Q_OBJECT
+
+public:
+    TabletModuleData(QObject *parent = nullptr);
+    ~TabletModuleData() override;
+
+private:
+    void updateRelevance();
+
+    WacomDeviceDatabase *m_db = nullptr;
+    TabletsModel *m_tabletsModel = nullptr;
+};