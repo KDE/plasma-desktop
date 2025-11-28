72a6c0132924f3d5417761400a4cb36eb32fd86f
Joshua Goins <josh@redstrate.com>
2025-11-28 10:09:46 -0500

    kcms/tablet: Obtain a better number of buttons for certain pens
    For certain tablets, they can report more than one supported styli.
    Users have encountered issues with Wacom tablets that follow this
    behavior, which becomes frustrating when we happen to pick the stylus
    that has fewer buttons.
    
    While our UX could be improved here, I chose to pick the largest amount
    of buttons from every stylus. This could mean that a user sees more
    buttons than supported, but that's better than the buggy behavior.
    
    BUG: 511488
    FIXED-IN: 6.5.4

--- kcms/tablet/kcmtablet.cpp
+++ kcms/tablet/kcmtablet.cpp
@@ -244,9 +244,11 @@ private:
             int num_styli = 0;
             const int *styli = libwacom_get_supported_styli(device, &num_styli);
             if (num_styli > 0) {
-                const auto stylus = libwacom_stylus_get_for_id(m_db, styli[0]);
-                if (stylus != nullptr) {
-                    numButtons = libwacom_stylus_get_num_buttons(stylus);
+                for (int i = 0; i < num_styli; i++) {
+                    const auto stylus = libwacom_stylus_get_for_id(m_db, styli[i]);
+                    if (stylus != nullptr) {
+                        numButtons = std::max(numButtons, libwacom_stylus_get_num_buttons(stylus));
+                    }
                 }
             }
         }
