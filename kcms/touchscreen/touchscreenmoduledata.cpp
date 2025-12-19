94231dc3552b6e82e7903a7d0f98ba4b11644d3c
Nicolas Fella <nicolas.fella@gmx.de>
2025-12-19 10:30:49 +0100

    kcms/touchscreen: Hide when no touchscreen is present
    BUG: 513566

---
+++ kcms/touchscreen/touchscreenmoduledata.cpp
@@ -0,0 +1,27 @@
+/**
+ * SPDX-FileCopyrightText: 2025 Nicolas Fella <nicolas.fella@gmx.de>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#include "touchscreenmoduledata.h"
+
+#include "devicesmodel.h"
+
+TouchscreenModuleData::TouchscreenModuleData(QObject *parent)
+    : KCModuleData(parent)
+{
+    m_devices = new DevicesModel("touch", this);
+    connect(m_devices, &QAbstractItemModel::rowsInserted, this, &TouchscreenModuleData::updateRelevance);
+    connect(m_devices, &QAbstractItemModel::rowsRemoved, this, &TouchscreenModuleData::updateRelevance);
+    connect(m_devices, &QAbstractItemModel::modelReset, this, &TouchscreenModuleData::updateRelevance);
+    updateRelevance();
+}
+
+void TouchscreenModuleData::updateRelevance()
+{
+    bool relevant = m_devices->rowCount(QModelIndex()) > 0;
+    setRelevant(relevant);
+}
+
+#include "moc_touchscreenmoduledata.cpp"
---
+++ kcms/touchscreen/touchscreenmoduledata.h
@@ -0,0 +1,24 @@
+/**
+ * SPDX-FileCopyrightText: 2025 Nicolas Fella <nicolas.fella@gmx.de>
+ *
+ * SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#pragma once
+
+#include <KCModuleData>
+
+class DevicesModel;
+
+class TouchscreenModuleData : public KCModuleData
+{
+    Q_OBJECT
+
+public:
+    TouchscreenModuleData(QObject *parent = nullptr);
+
+private:
+    void updateRelevance();
+
+    DevicesModel *m_devices = nullptr;
+};
