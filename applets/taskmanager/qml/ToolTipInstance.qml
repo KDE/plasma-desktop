KDE/plasma-desktop

commit 1c4d08a087c00c05520e535bbace63f7f96d2f2b

applets/taskmanager: allow some tooltip labels to be multi-line
Some of these labels look a bit silly when they're limited to a single
line, being elided when they could easily wrap onto a second line if
needed. Let them wrap.

applets/taskmanager/qml/ToolTipInstance.qml
@@ -127,8 +127,9 @@ ColumnLayout {
                 // window title
                 PlasmaComponents3.Label {
                     id: winTitle
-                    maximumLineCount: 1
                     Layout.fillWidth: true
+                    maximumLineCount: 2
+                    wrapMode: Text.Wrap
                     elide: Text.ElideRight
                     property bool somethingVisible: (thumbnailSourceItem.visible ||
                         appNameHeading.visible || subtext.visible)
@@ -143,8 +144,9 @@ ColumnLayout {
                 // subtext
                 PlasmaComponents3.Label {
                     id: subtext
-                    maximumLineCount: 2
                     Layout.fillWidth: true
+                    maximumLineCount: 2
+                    wrapMode: Text.Wrap
                     elide: Text.ElideRight
                     text: toolTipDelegate.isWin ? root.generateSubText() : ""
                     color: (headerHoverHandler.visible && headerHoverHighlight.pressed) ? Kirigami.Theme.highlightedTextColor : Kirigami.Theme.textColor

commit 99a6e2c312b1f348fe42c9524ded6750c1af7124

applets/taskmanager: keep thumbnails in horizontal group tooltips aligned
Amends 1c4d08a0, which broke thumbnails' visual alignment for horizontal
grouped tooltips where not all windows have 2-line text.

This commit fixes that by always reserving enough space for 2-line text
for all items in grouped horizontal tooltips.

Single-window tooltips or grouped vertical tooltips are unchanged, as
they did not suffer from the issue being fixed here.

applets/taskmanager/qml/ToolTipInstance.qml
@@ -128,9 +128,14 @@ ColumnLayout {
                 PlasmaComponents3.Label {
                     id: winTitle
                     Layout.fillWidth: true
+                    // For horizontal grouped tasks, leave room for two lines so thumbnails align
+                    Layout.preferredHeight: root.orientation === ListView.Horizontal && lineCount === 1
+                        ? implicitHeight * 2
+                        : implicitHeight
                     maximumLineCount: 2
                     wrapMode: Text.Wrap
                     elide: Text.ElideRight
+                    verticalAlignment: Text.AlignVCenter
                     property bool somethingVisible: (thumbnailSourceItem.visible ||
                         appNameHeading.visible || subtext.visible)
                     text: ((root.titleIncludesTrack && playerController.active) ||
@@ -145,9 +150,14 @@ ColumnLayout {
                 PlasmaComponents3.Label {
                     id: subtext
                     Layout.fillWidth: true
+                    // For horizontal grouped tasks, leave room for two lines so thumbnails align
+                    Layout.preferredHeight: root.orientation === ListView.Horizontal && lineCount === 1
+                        ? implicitHeight * 2
+                        : implicitHeight
                     maximumLineCount: 2
                     wrapMode: Text.Wrap
                     elide: Text.ElideRight
+                    verticalAlignment: Text.AlignVCenter
                     text: toolTipDelegate.isWin ? root.generateSubText() : ""
                     color: (headerHoverHandler.visible && headerHoverHighlight.pressed) ? Kirigami.Theme.highlightedTextColor : Kirigami.Theme.textColor
                     opacity: 0.75