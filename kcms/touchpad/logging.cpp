a6bce836fc2507016d33e7956a4ec2026ea8b31c
Nicolas Fella <nicolas.fella@gmx.de>
2026-05-08 14:04:03 +0200

    kcms/touchpad: Generate logging category

--- kcms/touchpad/logging.cpp
+++
@@ -1,8 +0,0 @@
-/*
-    SPDX-FileCopyrightText: 2017 Roman Gilg <subdiff@gmail.com>
-
-    SPDX-License-Identifier: GPL-2.0-or-later
-*/
-
-#include "logging.h"
-Q_LOGGING_CATEGORY(KCM_TOUCHPAD, "kcm_touchpad")
