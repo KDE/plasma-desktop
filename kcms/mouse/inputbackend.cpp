d61bb159d1c9cd793105d0f245112eceb34460ce
Nicolas Fella <nicolas.fella@gmx.de>
2026-06-03 18:00:03 +0200

    kcms/mouse: Drop X11 backend
    Could be cleaned up further

--- kcms/mouse/inputbackend.cpp
+++ kcms/mouse/inputbackend.cpp
@@ -14,28 +14,9 @@
 #if BUILD_KCM_MOUSE_KWIN_WAYLAND
 #include "backends/kwin_wl/kwin_wl_backend.h"
 #endif
-#if BUILD_KCM_MOUSE_X11
-#include "backends/x11/x11_libinput_backend.h"
-#include <X11/Xlib.h>
-#include <libinput-properties.h>
-#include <private/qtx11extras_p.h>
-#endif
 
 std::unique_ptr<InputBackend> InputBackend::create()
 {
-    // There are multiple possible backends
-#if BUILD_KCM_MOUSE_X11
-    if (KWindowSystem::isPlatformX11()) {
-        qCDebug(KCM_MOUSE) << "Using X11 backend";
-
-        Atom testAtom = XInternAtom(QX11Info::display(), LIBINPUT_PROP_ACCEL, True);
-
-        if (testAtom) {
-            qCDebug(KCM_MOUSE) << "Using libinput driver on X11.";
-            return std::make_unique<X11LibinputBackend>();
-        }
-    }
-#endif
 #if BUILD_KCM_MOUSE_KWIN_WAYLAND
     if (KWindowSystem::isPlatformWayland()) {
         qCDebug(KCM_MOUSE) << "Using KWin+Wayland backend";
@@ -49,9 +30,6 @@ std::unique_ptr<InputBackend> InputBackend::create()
 
 void InputBackend::registerImplementationTypes(const char *uri)
 {
-#if BUILD_KCM_MOUSE_X11
-    qmlRegisterUncreatableType<X11LibinputBackend>(uri, 1, 0, "X11LibinputBackend", QString());
-#endif
 #if BUILD_KCM_MOUSE_KWIN_WAYLAND
     qmlRegisterUncreatableType<KWinWaylandBackend>(uri, 1, 0, "KWinWaylandBackend", QString());
 #endif
